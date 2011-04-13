826d496db7f18f2ae1f7e0d2f4fc381144ed04a4
Mathieu Desnoyers <mathieu.desnoyers@efficios.com>
2011-04-13 17:39:52 -0400

    Copyright notices cleanup
    Signed-off-by: Mathieu Desnoyers <mathieu.desnoyers@efficios.com>

--- liblttsessiondcomm/liblttsessiondcomm.c
+++ liblttsessiondcomm/liblttsessiondcomm.c
@@ -1,4 +1,5 @@
-/* Copyright (C)  2011 - David Goulet <david.goulet@polymtl.ca>
+/*
+ * Copyright (C)  2011 - David Goulet <david.goulet@polymtl.ca>
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
@@ -13,7 +14,6 @@
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
- * 
  */
 
 #define _GNU_SOURCE
--- lttng/lttng.c
+++ lttng/lttng.c
@@ -1,4 +1,5 @@
-/* Copyright (c)  2011 David Goulet <david.goulet@polymtl.ca>
+/*
+ * Copyright (c)  2011 David Goulet <david.goulet@polymtl.ca>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -13,7 +14,6 @@
  * You should have received a copy of the GNU General Public License along
  * with this program; if not, write to the Free Software Foundation, Inc.,
  * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
- *
  */
 
 #define _GNU_SOURCE
