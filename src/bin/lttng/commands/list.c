lttng/lttng-tools

commit 081311398b0deca05eebaac8672efbc9af272d36

Make execution path with uninitialized domain fail
Signed-off-by: Mathieu Desnoyers <mathieu.desnoyers@efficios.com>

src/bin/lttng/commands/list.c
@@ -598,6 +598,10 @@ int cmd_list(int argc, const char **argv)
 	} else if (opt_userspace) {
 		DBG2("Listing userspace global domain");
 		domain.type = LTTNG_DOMAIN_UST;
+	} else {
+		usage(stderr);
+		ret = CMD_UNDEFINED;
+		goto end;
 	}
 
 	handle = lttng_create_handle(session_name, &domain);

commit aa5de748c1150c17624abce3cc74839fea3234f6

Fix list command
Broken by commit 081311398b0deca05eebaac8672efbc9af272d36

Fix by only creating the handle if kernel or ust is specified (so it
still fixes the valgrind warning).

Signed-off-by: Mathieu Desnoyers <mathieu.desnoyers@efficios.com>

src/bin/lttng/commands/list.c
@@ -604,16 +604,14 @@ int cmd_list(int argc, const char **argv)
 	} else if (opt_userspace) {
 		DBG2("Listing userspace global domain");
 		domain.type = LTTNG_DOMAIN_UST;
-	} else {
-		usage(stderr);
-		ret = CMD_UNDEFINED;
-		goto end;
 	}
 
-	handle = lttng_create_handle(session_name, &domain);
-	if (handle == NULL) {
-		ret = CMD_FATAL;
-		goto end;
+	if (opt_kernel || opt_userspace) {
+		handle = lttng_create_handle(session_name, &domain);
+		if (handle == NULL) {
+			ret = CMD_FATAL;
+			goto end;
+		}
 	}
 
 	if (session_name == NULL) {
@@ -676,7 +674,9 @@ int cmd_list(int argc, const char **argv)
 				}
 
 				/* Clean handle before creating a new one */
-				lttng_destroy_handle(handle);
+				if (handle) {
+					lttng_destroy_handle(handle);
+				}
 
 				handle = lttng_create_handle(session_name, &domains[i]);
 				if (handle == NULL) {
@@ -696,7 +696,9 @@ end:
 	if (domains) {
 		free(domains);
 	}
-	lttng_destroy_handle(handle);
+	if (handle) {
+		lttng_destroy_handle(handle);
+	}
 
 	poptFreeContext(pc);
 	return ret;