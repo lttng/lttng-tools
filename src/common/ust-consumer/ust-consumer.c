6bf73bf53464ad309fcf7f02a4dc397d280b81f8
Mathieu Desnoyers <mathieu.desnoyers@efficios.com>
2012-02-24 14:41:49 -0500

    Fix: lttng UST and kernel consumer: fix ret vs errno mixup
    - errno should be set to -ret (not the opposite!)
      The main effect is that the perror errors were meaningless.
    
    - errno should always be compared with positive values.
      This was also causing consumerd error handling mistakes.
    
    Signed-off-by: Mathieu Desnoyers <mathieu.desnoyers@efficios.com>
    Signed-off-by: David Goulet <dgoulet@efficios.com>

--- src/common/ust-consumer/ust-consumer.c
+++ src/common/ust-consumer/ust-consumer.c
@@ -57,7 +57,7 @@ int lttng_ustconsumer_on_read_subbuffer_mmap(
 	ret = ustctl_get_mmap_read_offset(stream->chan->handle,
 		stream->buf, &mmap_offset);
 	if (ret != 0) {
-		ret = -errno;
+		errno = -ret;
 		perror("ustctl_get_mmap_read_offset");
 		goto end;
 	}
@@ -66,7 +66,7 @@ int lttng_ustconsumer_on_read_subbuffer_mmap(
 		if (ret >= len) {
 			len = 0;
 		} else if (ret < 0) {
-			ret = -errno;
+			errno = -ret;
 			perror("Error in file write");
 			goto end;
 		}
@@ -108,7 +108,7 @@ int lttng_ustconsumer_take_snapshot(struct lttng_consumer_local_data *ctx,
 
 	ret = ustctl_snapshot(stream->chan->handle, stream->buf);
 	if (ret != 0) {
-		ret = errno;
+		errno = -ret;
 		perror("Getting sub-buffer snapshot.");
 	}
 
@@ -130,7 +130,7 @@ int lttng_ustconsumer_get_produced_snapshot(
 	ret = ustctl_snapshot_get_produced(stream->chan->handle,
 			stream->buf, pos);
 	if (ret != 0) {
-		ret = errno;
+		errno = -ret;
 		perror("kernctl_snapshot_get_produced");
 	}
 
@@ -346,7 +346,7 @@ int lttng_ustconsumer_read_subbuffer(struct lttng_consumer_stream *stream,
 	if (!stream->hangup_flush_done) {
 		do {
 			readlen = read(stream->wait_fd, &dummy, 1);
-		} while (readlen == -1 && errno == -EINTR);
+		} while (readlen == -1 && errno == EINTR);
 		if (readlen == -1) {
 			ret = readlen;
 			goto end;
