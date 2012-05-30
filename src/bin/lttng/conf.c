28eee19b0d323ae86bc1ec6bb0ddc7485893119c
Francis Giraldeau <francis.giraldeau@gmail.com>
2012-05-31 01:50:07 +0200

    Fix comments formating in conf.c
    Comments should be indented with one space only.
    
    Signed-off-by: Francis Giraldeau <francis.giraldeau@gmail.com>
    Signed-off-by: David Goulet <dgoulet@efficios.com>

--- src/bin/lttng/conf.c
+++ src/bin/lttng/conf.c
@@ -29,10 +29,8 @@
 #include "conf.h"
 
 /*
- *  config_get_file_path
- *
- *  Returns the path with '/CONFIG_FILENAME' added to it;
- *  path will be NULL if an error occurs.
+ * Returns the path with '/CONFIG_FILENAME' added to it;
+ * path will be NULL if an error occurs.
  */
 char *config_get_file_path(char *path)
 {
@@ -48,10 +46,8 @@ char *config_get_file_path(char *path)
 }
 
 /*
- *  open_config
- *
- *  Returns an open FILE pointer to the config file;
- *  on error, NULL is returned.
+ * Returns an open FILE pointer to the config file;
+ * on error, NULL is returned.
  */
 static FILE *open_config(char *path, const char *mode)
 {
@@ -76,11 +72,9 @@ error:
 }
 
 /*
- *  create_config_file
- *
- *  Creates the empty config file at the path.
- *  On success, returns 0;
- *  on error, returns -1.
+ * Creates the empty config file at the path.
+ * On success, returns 0;
+ * on error, returns -1.
  */
 static int create_config_file(char *path)
 {
@@ -101,11 +95,9 @@ error:
 }
 
 /*
- *  write_config
- *
- *  Append data to the config file in file_path
- *  On success, returns 0;
- *  on error, returns -1.
+ * Append data to the config file in file_path
+ * On success, returns 0;
+ * on error, returns -1.
  */
 static int write_config(char *file_path, size_t size, char *data)
 {
@@ -130,9 +122,7 @@ end:
 }
 
 /*
- *  config_get_default_path
- *
- *  Returns the HOME directory path. Caller MUST NOT free(3) the return pointer.
+ * Returns the HOME directory path. Caller MUST NOT free(3) the return pointer.
  */
 char *config_get_default_path(void)
 {
@@ -140,9 +130,7 @@ char *config_get_default_path(void)
 }
 
 /*
- *  config_destroy
- *
- *  Destroys directory config and file config.
+ * Destroys directory config and file config.
  */
 void config_destroy(char *path)
 {
@@ -168,11 +156,8 @@ end:
 }
 
 /*
- *  config_destroy_default
- *
- *  Destroys the default config
+ * Destroys the default config
  */
-
 void config_destroy_default(void)
 {
 	char *path = config_get_default_path();
@@ -183,9 +168,7 @@ void config_destroy_default(void)
 }
 
 /*
- *  config_exists
- *
- *  Returns 1 if config exists, 0 otherwise
+ * Returns 1 if config exists, 0 otherwise
  */
 int config_exists(const char *path)
 {
@@ -200,11 +183,9 @@ int config_exists(const char *path)
 }
 
 /*
- *  config_read_session_name
- *
- *  Returns the session name from the config file.
- *  The caller is responsible for freeing the returned string.
- *  On error, NULL is returned.
+ * Returns the session name from the config file.
+ * The caller is responsible for freeing the returned string.
+ * On error, NULL is returned.
  */
 char *config_read_session_name(char *path)
 {
@@ -252,11 +233,9 @@ found:
 }
 
 /*
- *  config_add_session_name
- *
- *  Write session name option to the config file.
- *  On success, returns 0;
- *  on error, returns -1.
+ * Write session name option to the config file.
+ * On success, returns 0;
+ * on error, returns -1.
  */
 int config_add_session_name(char *path, char *name)
 {
@@ -278,11 +257,9 @@ error:
 }
 
 /*
- *  config_init
- *
- *  Init configuration directory and file.
- *  On success, returns 0;
- *  on error, returns -1.
+ * Init configuration directory and file.
+ * On success, returns 0;
+ * on error, returns -1.
  */
 int config_init(char *session_name)
 {
