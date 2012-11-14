94e60b1f03ae2c3c9450ffc3eca8e052e2c37fb4
David Goulet <dgoulet@efficios.com>
2012-11-14 13:42:44 -0500

    Fix: Warn if session is running with lttng view
    Fixes #156
    
    Signed-off-by: David Goulet <dgoulet@efficios.com>

--- src/bin/lttng/commands/view.c
+++ src/bin/lttng/commands/view.c
@@ -343,6 +343,13 @@ static int view_trace(void)
 		trace_path = opt_trace_path;
 	}
 
+	if (sessions[i].enabled) {
+		WARN("Session %s is running. Please stop it before reading it.",
+				session_name);
+		ret = CMD_ERROR;
+		goto free_sessions;
+	}
+
 	MSG("Trace directory: %s\n", trace_path);
 
 	ret = spawn_viewer(trace_path);
