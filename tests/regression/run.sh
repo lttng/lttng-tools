f134f7178a3d522b6e5946c19c1e209b4e0a7ab4
Christian Babeux <christian.babeux@efficios.com>
2013-02-21 23:29:02 -0500

    Tests: Add runner script for regression testsuite
    Signed-off-by: Christian Babeux <christian.babeux@efficios.com>

--- tests/regression/run.sh
+++ tests/regression/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# Copyright (C) 2011 - David Goulet <david.goulet@polymtl.ca>
+# Copyright (C) 2013 - Christian Babeux <christian.babeux@efficios.com>
 #
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
@@ -16,34 +16,8 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #
+DIR=$(dirname $0)
 
-#### ADD TESTS HERE ####
-
-tests=( kernel/runall.sh ust/runall.sh tools/runall.sh )
-
-#### END TESTS HERE ####
-
-TESTDIR=$(dirname $0)/..
-
-source $TESTDIR/utils/utils.sh
-
-## lttng-tools unit tests ##
-# No session daemon needed
-for bin in ${tests[@]};
-do
-	if [ ! -e $bin ]; then
-		echo -e "$bin not found, passing"
-		continue
-	fi
-
-	./$bin
-	# Test must return 0 to pass.
-	if [ $? -ne 0 ]; then
-		echo -e '\e[1;31mFAIL\e[0m'
-		echo ""
-		exit 1
-	fi
-done
-
-# All passed
-exit 0
+$DIR/kernel/run.sh $@
+$DIR/ust/run.sh $@
+$DIR/tools/run.sh $@
