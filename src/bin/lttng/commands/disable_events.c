c138a39bf483662d3aa07bf4740f265a1f1c0f13
Mathieu Desnoyers <mathieu.desnoyers@efficios.com>
2013-07-18 10:19:38 -0400

    Manpage: cleanup layout of disable-event
    Signed-off-by: Mathieu Desnoyers <mathieu.desnoyers@efficios.com>

--- src/bin/lttng/commands/disable_events.c
+++ src/bin/lttng/commands/disable_events.c
@@ -69,7 +69,7 @@ static struct poptOption long_options[] = {
  */
 static void usage(FILE *ofp)
 {
-	fprintf(ofp, "usage: lttng disable-event NAME[,NAME2,...] [-k|-u] [OPTIONS]\n");
+	fprintf(ofp, "usage: lttng disable-event NAME[,NAME2,...] (-k | -u) [OPTIONS]\n");
 	fprintf(ofp, "\n");
 	fprintf(ofp, "Options:\n");
 	fprintf(ofp, "  -h, --help               Show this help\n");
