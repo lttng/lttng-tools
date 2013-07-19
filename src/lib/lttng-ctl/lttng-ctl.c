b7a81c24b6356fedf2b4ea08a0a2c25a91a35d82
Mathieu Desnoyers <mathieu.desnoyers@efficios.com>
2013-07-19 10:36:52 -0400

    Fix: print dots while waiting for data availability
    Signed-off-by: Mathieu Desnoyers <mathieu.desnoyers@efficios.com>

--- src/lib/lttng-ctl/lttng-ctl.c
+++ src/lib/lttng-ctl/lttng-ctl.c
@@ -574,6 +574,7 @@ static int _lttng_stop_tracing(const char *session_name, int wait)
 	}
 
 	_MSG("Waiting for data availability");
+	fflush(stdout);
 
 	/* Check for data availability */
 	do {
@@ -591,6 +592,7 @@ static int _lttng_stop_tracing(const char *session_name, int wait)
 		if (data_ret) {
 			usleep(DEFAULT_DATA_AVAILABILITY_WAIT_TIME);
 			_MSG(".");
+			fflush(stdout);
 		}
 	} while (data_ret != 0);
 
