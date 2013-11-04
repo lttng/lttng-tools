4031e53e78e673c7bf84fc5cc5f844b2a037b6f6
JP Ikaheimonen <jp_ikaheimonen@mentor.com>
2013-11-04 15:27:07 +0200

    Copy exclusion data in add_unique_ust_event.

--- src/bin/lttng-sessiond/event.c
+++ src/bin/lttng-sessiond/event.c
@@ -48,6 +48,7 @@ static void add_unique_ust_event(struct lttng_ht *ht,
 	key.name = event->attr.name;
 	key.filter = (struct lttng_filter_bytecode *) event->filter;
 	key.loglevel = event->attr.loglevel;
+	key.exclusion = event->exclusion;
 
 	node_ptr = cds_lfht_add_unique(ht->ht,
 			ht->hash_fct(event->node.key, lttng_ht_seed),
