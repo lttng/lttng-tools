c7b19256dd3612f5c6acb02f7d98d61b9b3a7b7b
Julien Desfossez <jdesfossez@efficios.com>
2014-01-13 11:28:07 -0500

    Fix: wrong check before destroying the viewer metadata stream
    Signed-off-by: Julien Desfossez <jdesfossez@efficios.com>
    Signed-off-by: David Goulet <dgoulet@efficios.com>

--- src/bin/lttng-relayd/live.c
+++ src/bin/lttng-relayd/live.c
@@ -1120,7 +1120,7 @@ void destroy_viewer_stream(struct relay_viewer_stream *vstream)
 	 * we need to remove it because we won't detect a EOF for this
 	 * stream.
 	 */
-	if (ret_ref == 1 && vstream->ctf_trace->metadata_stream) {
+	if (ret_ref == 1 && vstream->ctf_trace->viewer_metadata_stream) {
 		destroy_viewer_stream(vstream->ctf_trace->viewer_metadata_stream);
 		vstream->ctf_trace->metadata_stream = NULL;
 		DBG("Freeing ctf_trace %" PRIu64, vstream->ctf_trace->id);
