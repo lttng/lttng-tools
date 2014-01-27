fef84dd50816b13d967a9dbc3221af590f163f5f
Mathieu Desnoyers <mathieu.desnoyers@efficios.com>
2014-01-26 22:32:35 -0500

    tests: use --background for session daemon
    Will now show errors when they happen.
    
    Signed-off-by: Mathieu Desnoyers <mathieu.desnoyers@efficios.com>

--- tests/utils/utils.sh
+++ tests/utils/utils.sh
@@ -157,7 +157,7 @@ function start_lttng_sessiond()
 	DIR=$(readlink -f $TESTDIR)
 
 	if [ -z $(pidof lt-$SESSIOND_BIN) ]; then
-		$DIR/../src/bin/lttng-sessiond/$SESSIOND_BIN --daemonize --consumerd32-path="$DIR/../src/bin/lttng-consumerd/lttng-consumerd" --consumerd64-path="$DIR/../src/bin/lttng-consumerd/lttng-consumerd"
+		$DIR/../src/bin/lttng-sessiond/$SESSIOND_BIN --background --consumerd32-path="$DIR/../src/bin/lttng-consumerd/lttng-consumerd" --consumerd64-path="$DIR/../src/bin/lttng-consumerd/lttng-consumerd"
 		#$DIR/../src/bin/lttng-sessiond/$SESSIOND_BIN --consumerd32-path="$DIR/../src/bin/lttng-consumerd/lttng-consumerd" --consumerd64-path="$DIR/../src/bin/lttng-consumerd/lttng-consumerd" --verbose-consumer >>/tmp/sessiond.log 2>&1 &
 		status=$?
 		ok $status "Start session daemon"
