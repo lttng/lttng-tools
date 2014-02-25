0841762cf475e71336a4f8df1b9a468e3a606dde
David Goulet <dgoulet@efficios.com>
2014-02-25 14:45:08 -0500

    Fix: forbid session name creation if contains /
    This adds a validation function for session name which for now denies
    any session name containing '/'.
    
    This is in response of bug #721 that actually uses a path as a session
    name such as "test/../session1" which would then be concatenated to the
    session path adding a relative path to it making this a serious security
    issue.
    
    Because of this issue, this is backported from master up to stable-2.3.
    
    Fixes #721
    
    Signed-off-by: David Goulet <dgoulet@efficios.com>

--- src/common/error.c
+++ src/common/error.c
@@ -116,6 +116,7 @@ static const char *error_string_array[] = {
 	[ ERROR_INDEX(LTTNG_ERR_CHAN_EXIST) ] = "Channel already exists",
 	[ ERROR_INDEX(LTTNG_ERR_SNAPSHOT_NODATA) ] = "No data available in snapshot",
 	[ ERROR_INDEX(LTTNG_ERR_NO_CHANNEL) ] = "No channel found in the session",
+	[ ERROR_INDEX(LTTNG_ERR_SESSION_INVALID_CHAR) ] = "Invalid character found in session name",
 
 	/* Last element */
 	[ ERROR_INDEX(LTTNG_ERR_NR) ] = "Unknown error code"
