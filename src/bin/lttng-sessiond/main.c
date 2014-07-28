385b881bb1ff2ddc5ff2657e038a0c54712e19a4
Mathieu Desnoyers <mathieu.desnoyers@efficios.com>
2014-07-27 20:38:50 -0400

    Fix: unbalanced ustconsumer32_data.pid_mutex lock
    It is never locked in this function, but should be. This is triggering
    spurious runtime failures on my system, where it seems that sessiond was
    sometimes breaking the communication pipe with liblttng-ctl when the
    unbalanced unlock is reached.
    
    This should be backported to stable-2.4 and stable-2.5.
    
    Signed-off-by: Mathieu Desnoyers <mathieu.desnoyers@efficios.com>
    Signed-off-by: David Goulet <dgoulet@efficios.com>

--- src/bin/lttng-sessiond/main.c
+++ src/bin/lttng-sessiond/main.c
@@ -3016,6 +3016,7 @@ static int process_client_msg(struct command_ctx *cmd_ctx, int sock,
 			}
 
 			/* 32-bit */
+			pthread_mutex_lock(&ustconsumer32_data.pid_mutex);
 			if (consumerd32_bin[0] != '\0' &&
 					ustconsumer32_data.pid == 0 &&
 					cmd_ctx->lsm->cmd_type != LTTNG_REGISTER_CONSUMER) {
