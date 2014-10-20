90d13b3d56b65b0895251c8bf2fc22d8b8da289e
Mathieu Desnoyers <mathieu.desnoyers@efficios.com>
2014-10-20 18:50:32 -0400

    Fix: syscall list ioctl number conflict
    Signed-off-by: Mathieu Desnoyers <mathieu.desnoyers@efficios.com>
    Signed-off-by: David Goulet <dgoulet@efficios.com>

--- src/common/kernel-ctl/kernel-ioctl.h
+++ src/common/kernel-ctl/kernel-ioctl.h
@@ -103,7 +103,7 @@
 #define LTTNG_KERNEL_WAIT_QUIESCENT		_IO(0xF6, 0x48)
 #define LTTNG_KERNEL_CALIBRATE			\
 	_IOWR(0xF6, 0x49, struct lttng_kernel_calibrate)
-#define LTTNG_KERNEL_SYSCALL_LIST		_IO(0xF6, 0x49)
+#define LTTNG_KERNEL_SYSCALL_LIST		_IO(0xF6, 0x4A)
 
 /* Session FD ioctl */
 #define LTTNG_KERNEL_METADATA			\
