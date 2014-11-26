4ea184d1d43d64a2ccfc0dc181904f27b4ad1f88
Mathieu Desnoyers <mathieu.desnoyers@efficios.com>
2014-11-26 12:18:07 -0500

    Fix: add missing str*dup oom check in sessiond main.c
    Signed-off-by: Mathieu Desnoyers <mathieu.desnoyers@efficios.com>
    Signed-off-by: Jérémie Galarneau <jeremie.galarneau@efficios.com>

--- src/bin/lttng-sessiond/main.c
+++ src/bin/lttng-sessiond/main.c
@@ -5110,6 +5110,10 @@ int main(int argc, char **argv)
 
 	if (is_root) {
 		rundir = strdup(DEFAULT_LTTNG_RUNDIR);
+		if (!rundir) {
+			ret = -ENOMEM;
+			goto error;
+		}
 
 		/* Create global run dir with root access */
 		ret = create_lttng_rundir(rundir);
