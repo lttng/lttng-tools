e205d79b4dc22178644fa6f3d0a4eae86df36e73
Mathieu Desnoyers <mathieu.desnoyers@efficios.com>
2014-12-17 20:45:17 -0500

    Misleading error handling: utils_create_pid_file() should return 0 on success
    fprintf returns a positive value on success, which is used as return
    value for utils_create_pid_file(), which is odd. This is in preparation
    for main() refactoring.
    
    Signed-off-by: Mathieu Desnoyers <mathieu.desnoyers@efficios.com>
    Signed-off-by: Jérémie Galarneau <jeremie.galarneau@efficios.com>

--- src/common/utils.c
+++ src/common/utils.c
@@ -477,10 +477,14 @@ int utils_create_pid_file(pid_t pid, const char *filepath)
 	ret = fprintf(fp, "%d\n", pid);
 	if (ret < 0) {
 		PERROR("fprintf pid file");
+		goto error;
 	}
 
-	fclose(fp);
+	if (fclose(fp)) {
+		PERROR("fclose");
+	}
 	DBG("Pid %d written in file %s", pid, filepath);
+	ret = 0;
 error:
 	return ret;
 }
