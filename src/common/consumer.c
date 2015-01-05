d9607cd79bbe69ba8224cd56134e0e8f6fe8d51a
Mathieu Desnoyers <mathieu.desnoyers@efficios.com>
2015-01-05 16:43:08 -0500

    Fix: exit threads not only on goto restart
    Exit threads as soon as number of FD is 0, on every loop (no need for
    goto restart special case). Number of FD being 0 is a sufficient
    condition for exiting the thread: it means the quit pipe has been
    removed from the poll set.
    
    Signed-off-by: Mathieu Desnoyers <mathieu.desnoyers@efficios.com>
    Signed-off-by: Jérémie Galarneau <jeremie.galarneau@efficios.com>

--- src/common/consumer.c
+++ src/common/consumer.c
@@ -2181,14 +2181,6 @@ void *consumer_thread_metadata_poll(void *data)
 	DBG("Metadata main loop started");
 
 	while (1) {
-		health_code_update();
-
-		/* Only the metadata pipe is set */
-		if (LTTNG_POLL_GETNB(&events) == 0 && consumer_quit == 1) {
-			err = 0;	/* All is OK */
-			goto end;
-		}
-
 restart:
 		health_code_update();
 		health_poll_entry();
@@ -2203,7 +2195,10 @@ restart:
 				ERR("Poll EINTR catched");
 				goto restart;
 			}
-			goto error;
+			if (LTTNG_POLL_GETNB(&events) == 0) {
+				err = 0;	/* All is OK */
+			}
+			goto end;
 		}
 
 		nb_fd = ret;
@@ -2759,14 +2754,6 @@ void *consumer_thread_channel_poll(void *data)
 	DBG("Channel main loop started");
 
 	while (1) {
-		health_code_update();
-
-		/* Only the channel pipe is set */
-		if (LTTNG_POLL_GETNB(&events) == 0 && consumer_quit == 1) {
-			err = 0;	/* All is OK */
-			goto end;
-		}
-
 restart:
 		health_code_update();
 		DBG("Channel poll wait");
@@ -2780,6 +2767,9 @@ restart:
 			if (errno == EINTR) {
 				ERR("Poll EINTR catched");
 				goto restart;
+			}
+			if (LTTNG_POLL_GETNB(&events) == 0) {
+				err = 0;	/* All is OK */
 			}
 			goto end;
 		}
