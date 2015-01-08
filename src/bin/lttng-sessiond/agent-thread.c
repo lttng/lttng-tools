546f19b598d3282adb7159c2ae4e0592ffff4598
Jérémie Galarneau <jeremie.galarneau@efficios.com>
2015-01-08 15:43:24 -0500

    Fix: Remove unused argument in debug statement
    Signed-off-by: Jérémie Galarneau <jeremie.galarneau@efficios.com>

--- src/bin/lttng-sessiond/agent-thread.c
+++ src/bin/lttng-sessiond/agent-thread.c
@@ -307,8 +307,7 @@ void *agent_thread_manage_registration(void *data)
 	}
 
 	while (1) {
-		DBG3("[agent-thread] Manage agent polling",
-				LTTNG_POLL_GETNB(&events));
+		DBG3("[agent-thread] Manage agent polling");
 
 		/* Inifinite blocking call, waiting for transmission */
 restart:
