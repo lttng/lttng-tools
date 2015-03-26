e04aa23e052089bfd9aaa577f6a6972fb058b650
Jérémie Galarneau <jeremie.galarneau@efficios.com>
2015-03-26 14:49:52 -0400

    Fix: Possible call to strtoul() with NULL argument
    Signed-off-by: Jérémie Galarneau <jeremie.galarneau@efficios.com>

--- src/bin/lttng-sessiond/main.c
+++ src/bin/lttng-sessiond/main.c
@@ -4612,6 +4612,10 @@ static int set_option(int opt, const char *arg, const char *optname)
 		} else {
 			unsigned long v;
 
+			if (!arg) {
+				ret = -EINVAL;
+				goto end;
+			}
 			errno = 0;
 			v = strtoul(arg, NULL, 0);
 			if (errno != 0 || !isdigit(arg[0])) {
