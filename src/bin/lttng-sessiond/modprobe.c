44603c80f2e1ce55b6237a02810de8e7fc1687d4
Philippe Proulx <eeppeliteloop@gmail.com>
2015-04-28 17:09:16 -0400

    Fix: modprobe.c: fix tmp_list memory leak
    Reported-by: Hannes Weisbach <hannes.weisbach@mailbox.tu-dresden.de>
    Signed-off-by: Philippe Proulx <eeppeliteloop@gmail.com>
    Signed-off-by: Jérémie Galarneau <jeremie.galarneau@efficios.com>

--- src/bin/lttng-sessiond/modprobe.c
+++ src/bin/lttng-sessiond/modprobe.c
@@ -360,11 +360,11 @@ static int append_list_to_probes(const char *list)
 {
 	char *next;
 	int index = nr_probes, ret;
-	char *tmp_list;
+	char *tmp_list, *cur_list;
 
 	assert(list);
 
-	tmp_list = strdup(list);
+	cur_list = tmp_list = strdup(list);
 	if (!tmp_list) {
 		PERROR("strdup temp list");
 		return -ENOMEM;
@@ -374,11 +374,11 @@ static int append_list_to_probes(const char *list)
 		size_t name_len;
 		struct kern_modules_param *cur_mod;
 
-		next = strtok(tmp_list, ",");
+		next = strtok(cur_list, ",");
 		if (!next) {
 			break;
 		}
-		tmp_list = NULL;
+		cur_list = NULL;
 
 		/* filter leading spaces */
 		while (*next == ' ') {
