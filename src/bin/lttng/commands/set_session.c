f74306392c0fb8b155ae281247e16f0448476a83
Partha Pratim Mukherjee <ppm.floss@gmail.com>
2015-07-25 13:25:19 +0530

    Fix: set session should not set non-existent session
    set-session does not check the existence of a session before setting
    it as the current session. Fix it so that it gives error for a
    non-existent session.
    
    Fixes #885
    
    Signed-off-by: Partha Pratim Mukherjee <ppm.floss@gmail.com>
    Signed-off-by: Jérémie Galarneau <jeremie.galarneau@efficios.com>

--- src/bin/lttng/commands/set_session.c
+++ src/bin/lttng/commands/set_session.c
@@ -105,10 +105,33 @@ end:
 static int set_session(void)
 {
 	int ret = CMD_SUCCESS;
+	int count, i;
+	unsigned int session_found = 0;
+	struct lttng_session *sessions;
 
 	if (opt_session_name && strlen(opt_session_name) > NAME_MAX) {
 		ERR("Session name too long. Length must be lower or equal to %d",
 			NAME_MAX);
+		ret = CMD_ERROR;
+		goto end;
+	}
+
+	count = lttng_list_sessions(&sessions);
+	if (count < 0) {
+		ret = CMD_ERROR;
+		ERR("%s", lttng_strerror(count));
+		goto end;
+	}
+
+	for (i = 0; i < count; i++) {
+		if (strncmp(sessions[i].name, opt_session_name, NAME_MAX) == 0) {
+			session_found = 1;
+			break;
+		}
+	}
+
+	if (!session_found) {
+		ERR("Session '%s' not found", opt_session_name);
 		ret = CMD_ERROR;
 		goto error;
 	}
@@ -132,6 +155,8 @@ static int set_session(void)
 	ret = CMD_SUCCESS;
 
 error:
+	free(sessions);
+end:
 	return ret;
 }
 
