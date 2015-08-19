9b7d4857ad1737701bd830f0e618e944d35e140b
Mathieu Desnoyers <mathieu.desnoyers@efficios.com>
2015-08-19 14:13:48 -0700

    Fix: streamline ret/errno of run_as()
    Signed-off-by: Mathieu Desnoyers <mathieu.desnoyers@efficios.com>

--- src/bin/lttng-sessiond/kernel-consumer.c
+++ src/bin/lttng-sessiond/kernel-consumer.c
@@ -57,7 +57,7 @@ static char *create_channel_path(struct consumer_output *consumer,
 		/* Create directory */
 		ret = run_as_mkdir_recursive(pathname, S_IRWXU | S_IRWXG, uid, gid);
 		if (ret < 0) {
-			if (ret != -EEXIST) {
+			if (errno != EEXIST) {
 				ERR("Trace directory creation error");
 				goto error;
 			}
