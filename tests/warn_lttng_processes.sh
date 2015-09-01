e3820b59121d0ed44d15d1b9c4f84a9570d93bab
Philippe Proulx <eeppeliteloop@gmail.com>
2015-09-01 11:57:39 -0400

    warn_lttng_processes.sh: use $0 for self name
    Signed-off-by: Philippe Proulx <eeppeliteloop@gmail.com>

--- tests/warn_lttng_processes.sh
+++ tests/warn_lttng_processes.sh
@@ -8,7 +8,7 @@ color_warn='\E[1;33m'
 color_reset='\E[0m'
 color_bold='\E[1m'
 
-lttng_processes="$("$1" 'lttng|gen-ust-.+' -a | grep -v warn_lttng_processes)"
+lttng_processes="$("$1" 'lttng|gen-ust-.+' -a | grep -v "$(basename "$0")")"
 
 if [ $? -eq 0 ]; then
 	pids="$(cut -d ' ' -f 1 <<< "$lttng_processes" | tr '\n' ' ')"
