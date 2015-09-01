c462e7074cd59247b5eda021d20c7add3b9c67d5
Philippe Proulx <eeppeliteloop@gmail.com>
2015-09-01 11:51:23 -0400

    configure.ac: check for pgrep for warn_lttng_processes.sh

--- tests/warn_lttng_processes.sh
+++ tests/warn_lttng_processes.sh
@@ -8,20 +8,16 @@ color_warn='\E[1;33m'
 color_reset='\E[0m'
 color_bold='\E[1m'
 
-which pgrep &>/dev/null
+lttng_processes="$("$1" 'lttng|gen-ust-.+' -a | grep -v warn_lttng_processes)"
 
 if [ $? -eq 0 ]; then
-	lttng_processes="$(pgrep 'lttng|gen-ust-.+' -a | grep -v warn_lttng_processes)"
+	pids="$(cut -d ' ' -f 1 <<< "$lttng_processes" | tr '\n' ' ')"
 
-	if [ $? -eq 0 ]; then
-		pids="$(cut -d ' ' -f 1 <<< "$lttng_processes" | tr '\n' ' ')"
-
-		echo -e "${color_warn}Warning: the following LTTng processes were detected running on the system:$color_reset"
-		echo
-		echo "$lttng_processes"
-		echo
-		echo -e "Here's how to kill them: ${color_bold}kill -9 $pids$color_reset"
-		echo -e "${color_warn}If you leave them alive, some tests could fail.$color_reset"
-		echo
-	fi
+	echo -e "${color_warn}Warning: the following LTTng processes were detected running on the system:$color_reset"
+	echo
+	echo "$lttng_processes"
+	echo
+	echo -e "Here's how to kill them: ${color_bold}kill -9 $pids$color_reset"
+	echo -e "${color_warn}If you leave them alive, some tests could fail.$color_reset"
+	echo
 fi
