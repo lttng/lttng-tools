8d9eb6d8a3b60f0b20abe470ccede407c44cf6cb
Jérémie Galarneau <jeremie.galarneau@efficios.com>
2015-09-17 13:49:22 -0400

    Fix: Hide sessiond-comm functions
    Signed-off-by: Jérémie Galarneau <jeremie.galarneau@efficios.com>

--- src/common/sessiond-comm/unix.h
+++ src/common/sessiond-comm/unix.h
@@ -23,28 +23,42 @@
 #include <sys/un.h>
 
 #include <common/compat/socket.h>
+#include <common/macros.h>
 
 #include "sessiond-comm.h"
 
-extern int lttcomm_create_unix_sock(const char *pathname);
-extern int lttcomm_create_anon_unix_socketpair(int *fds);
-extern int lttcomm_connect_unix_sock(const char *pathname);
-extern int lttcomm_accept_unix_sock(int sock);
-extern int lttcomm_listen_unix_sock(int sock);
-extern int lttcomm_close_unix_sock(int sock);
+LTTNG_HIDDEN
+int lttcomm_create_unix_sock(const char *pathname);
+LTTNG_HIDDEN
+int lttcomm_create_anon_unix_socketpair(int *fds);
+LTTNG_HIDDEN
+int lttcomm_connect_unix_sock(const char *pathname);
+LTTNG_HIDDEN
+int lttcomm_accept_unix_sock(int sock);
+LTTNG_HIDDEN
+int lttcomm_listen_unix_sock(int sock);
+LTTNG_HIDDEN
+int lttcomm_close_unix_sock(int sock);
 
 /* Send a message accompanied by fd(s) over a unix socket. */
-extern ssize_t lttcomm_send_fds_unix_sock(int sock, int *fds, size_t nb_fd);
+LTTNG_HIDDEN
+ssize_t lttcomm_send_fds_unix_sock(int sock, int *fds, size_t nb_fd);
 /* Recv a message accompanied by fd(s) from a unix socket */
-extern ssize_t lttcomm_recv_fds_unix_sock(int sock, int *fds, size_t nb_fd);
+LTTNG_HIDDEN
+ssize_t lttcomm_recv_fds_unix_sock(int sock, int *fds, size_t nb_fd);
 
-extern ssize_t lttcomm_recv_unix_sock(int sock, void *buf, size_t len);
-extern ssize_t lttcomm_send_unix_sock(int sock, void *buf, size_t len);
+LTTNG_HIDDEN
+ssize_t lttcomm_recv_unix_sock(int sock, void *buf, size_t len);
+LTTNG_HIDDEN
+ssize_t lttcomm_send_unix_sock(int sock, void *buf, size_t len);
 
-extern ssize_t lttcomm_send_creds_unix_sock(int sock, void *buf, size_t len);
-extern ssize_t lttcomm_recv_creds_unix_sock(int sock, void *buf, size_t len,
+LTTNG_HIDDEN
+ssize_t lttcomm_send_creds_unix_sock(int sock, void *buf, size_t len);
+LTTNG_HIDDEN
+ssize_t lttcomm_recv_creds_unix_sock(int sock, void *buf, size_t len,
 		lttng_sock_cred *creds);
 
-extern int lttcomm_setsockopt_creds_unix_sock(int sock);
+LTTNG_HIDDEN
+int lttcomm_setsockopt_creds_unix_sock(int sock);
 
 #endif	/* _LTTCOMM_UNIX_H */
