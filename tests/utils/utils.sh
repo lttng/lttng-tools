4f0cb1f148e3800caddb1676ad5b005570be6457
Jonathan Rajotte <jonathan.rajotte-julien@efficios.com>
2015-10-01 15:15:05 -0400

    Use create session from utils.sh
    Signed-off-by: Jonathan Rajotte <jonathan.rajotte-julien@efficios.com>

--- tests/utils/utils.sh
+++ tests/utils/utils.sh
@@ -673,9 +673,13 @@ function create_lttng_session ()
 	local expected_to_fail=$1
 	local sess_name=$2
 	local trace_path=$3
-	local opt=$4
+	local opt="$4"
 
-	$TESTDIR/$LTTNG_PATH_REL/$LTTNG_BIN create $sess_name -o $trace_path $opt > $OUTPUT_DEST
+	if [[ ! -z $trace_path ]]; then
+		trace_path="-o $trace_path"
+	fi
+
+	$TESTDIR/$LTTNG_PATH_REL/$LTTNG_BIN create $sess_name $trace_path $opt > $OUTPUT_DEST
 	ret=$?
 	if [[ $expected_to_fail -eq "1" ]]; then
 		test "$ret" -ne "0"
