b1325effaf300e21bb804113cc202b0e42ea61da
Michael Jeanson <mjeanson@efficios.com>
2015-10-14 14:16:30 -0400

    Port: Add Solaris support to socket compat
    Signed-off-by: Michael Jeanson <mjeanson@efficios.com>
    Signed-off-by: Jérémie Galarneau <jeremie.galarneau@efficios.com>

--- src/common/compat/socket.h
+++ src/common/compat/socket.h
@@ -37,7 +37,7 @@ typedef struct ucred lttng_sock_cred;
 #define LTTNG_SOCK_GET_GID_CRED(c) LTTNG_REF(c)->gid
 #define LTTNG_SOCK_GET_PID_CRED(c) LTTNG_REF(c)->pid
 
-#elif (defined(__FreeBSD__) || defined(__CYGWIN__))
+#elif (defined(__FreeBSD__) || defined(__CYGWIN__) || defined(__sun__))
 
 struct lttng_sock_cred {
 	uid_t uid;
