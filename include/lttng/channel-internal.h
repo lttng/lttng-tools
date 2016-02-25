8d7f6956990e8d0e0f32a532321db44263c6903f
Jérémie Galarneau <jeremie.galarneau@efficios.com>
2016-02-25 16:28:16 -0500

    Add internal extended channel structure
    Signed-off-by: Jérémie Galarneau <jeremie.galarneau@efficios.com>

---
+++ include/lttng/channel-internal.h
@@ -0,0 +1,26 @@
+/*
+ * Copyright (C) 2015 - Jérémie Galarneau <jeremie.galarneau@efficios.com>
+ *
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License, version 2.1 only,
+ * as published by the Free Software Foundation.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ */
+
+#ifndef LTTNG_CHANNEL_INTERNAL_H
+#define LTTNG_CHANNEL_INTERNAL_H
+
+struct lttng_channel_extended {
+	uint64_t discarded_events;
+	uint64_t lost_packets;
+} LTTNG_PACKED;
+
+#endif /* LTTNG_CHANNEL_INTERNAL_H */
