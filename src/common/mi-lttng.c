654f9f5e49aae2669d8ae771650b5e2dce5d660f
Jérémie Galarneau <jeremie.galarneau@efficios.com>
2016-02-26 17:08:33 -0500

    Bump LTTng MI schema to 3.0
    The machine interface schema now allows LTTng to express filter
    expressions.
    
    It also introduces "any" clauses at various points to allow the
    extension of the schema in a non-breaking way.
    
    Signed-off-by: Jérémie Galarneau <jeremie.galarneau@efficios.com>

--- src/common/mi-lttng.c
+++ src/common/mi-lttng.c
@@ -951,22 +951,16 @@ int mi_lttng_event_common_attributes(struct mi_writer *writer,
 		goto end;
 	}
 
-	/* Event filter enabled? */
-	ret = mi_lttng_writer_write_element_bool(writer,
-			config_element_filter, event->filter);
-
 	/* Event filter expression */
 	ret = lttng_event_get_filter_string(event, &filter_expression);
-
 	if (ret) {
 		goto end;
 	}
 
 	if (filter_expression) {
 		ret = mi_lttng_writer_write_element_string(writer,
-			config_element_filter_expression,
-			filter_expression);
-
+				config_element_filter_expression,
+				filter_expression);
 		if (ret) {
 			goto end;
 		}
