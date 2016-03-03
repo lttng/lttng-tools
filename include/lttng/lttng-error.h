7d8829fd67c56102436f3e1e16d652f23076ed86
Jérémie Galarneau <jeremie.galarneau@efficios.com>
2016-03-03 16:50:42 -0500

    Add new LTTNG_ERR_OVERFLOW error code
    Signed-off-by: Jérémie Galarneau <jeremie.galarneau@efficios.com>

--- include/lttng/lttng-error.h
+++ include/lttng/lttng-error.h
@@ -137,6 +137,7 @@ enum lttng_error_code {
 	LTTNG_ERR_PID_TRACKED            = 114, /* PID already tracked */
 	LTTNG_ERR_PID_NOT_TRACKED        = 115, /* PID not tracked */
 	LTTNG_ERR_INVALID_CHANNEL_DOMAIN = 116, /* Invalid channel domain */
+	LTTNG_ERR_OVERFLOW		 = 117, /* Overflow occured. */
 
 	/* MUST be last element */
 	LTTNG_ERR_NR,                           /* Last element */
