jdesfossez/lttng-tools-dev-old

Display discarded and lost events at destroy and stop
Signed-off-by: Julien Desfossez <jdesfossez@efficios.com>
Reviewed-by: Mathieu Desnoyers <mathieu.desnoyers@efficios.com>

src/bin/lttng/commands/stop.c
@@ -156,6 +156,7 @@ static int stop_tracing(void)
 
 	ret = CMD_SUCCESS;
 
+	get_session_stats(session_name);
 	MSG("Tracing stopped for session %s", session_name);
 	if (lttng_opt_mi) {
 		ret = mi_print_session(session_name, 0);

src/bin/lttng/utils.h
@@ -59,5 +59,6 @@ int print_missing_or_multiple_domains(unsigned int sum);
 
 int spawn_relayd(const char *pathname, int port);
 int check_relayd(void);
+void get_session_stats(const char *session_name);
 
 #endif /* _LTTNG_UTILS_H */

lttng/lttng-tools

Signed-off-by: Jérémie Galarneau <jeremie.galarneau@efficios.com>

src/bin/lttng/commands/stop.c
@@ -155,6 +155,7 @@ static int stop_tracing(void)
 
 	ret = CMD_SUCCESS;
 
+	print_session_stats(session_name);
 	MSG("Tracing stopped for session %s", session_name);
 	if (lttng_opt_mi) {
 		ret = mi_print_session(session_name, 0);

src/bin/lttng/utils.h
@@ -59,5 +59,6 @@ int print_missing_or_multiple_domains(unsigned int sum);
 
 int spawn_relayd(const char *pathname, int port);
 int check_relayd(void);
+void print_session_stats(const char *session_name);
 
 #endif /* _LTTNG_UTILS_H */