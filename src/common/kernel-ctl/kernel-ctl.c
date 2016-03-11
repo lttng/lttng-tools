jdesfossez/lttng-tools-dev-old

Command metadata regenerate
Command to regenerate the metadata file when the session is running.
This allows the user to regenerate the metadata after a major NTP
correction and that way update the clock offset from epoch in the
metadata.

Works for kernel and UST per-uid session local or remote (not live).

Signed-off-by: Julien Desfossez <jdesfossez@efficios.com>

src/common/kernel-ctl/kernel-ctl.c
@@ -209,6 +209,11 @@ int kernctl_list_tracker_pids(int fd)
 	return ioctl(fd, LTTNG_KERNEL_SESSION_LIST_TRACKER_PIDS);
 }
 
+int kernctl_session_metadata_regen(int fd)
+{
+	return ioctl(fd, LTTNG_KERNEL_SESSION_METADATA_REGEN);
+}
+
 int kernctl_create_stream(int fd)
 {
 	return compat_ioctl_no_arg(fd, LTTNG_KERNEL_OLD_STREAM,
@@ -404,6 +409,12 @@ end:
 int kernctl_buffer_flush(int fd)
 {
 	return ioctl(fd, RING_BUFFER_FLUSH);
+}
+
+/* returns the version of the metadata. */
+int kernctl_get_metadata_version(int fd, uint64_t *version)
+{
+	return ioctl(fd, RING_BUFFER_GET_METADATA_VERSION, version);
 }
 
 

lttng/lttng-tools

Signed-off-by: Jérémie Galarneau <jeremie.galarneau@efficios.com>

src/common/kernel-ctl/kernel-ctl.c
@@ -209,6 +209,11 @@ int kernctl_list_tracker_pids(int fd)
 	return ioctl(fd, LTTNG_KERNEL_SESSION_LIST_TRACKER_PIDS);
 }
 
+int kernctl_session_metadata_regenerate(int fd)
+{
+	return ioctl(fd, LTTNG_KERNEL_SESSION_METADATA_REGEN);
+}
+
 int kernctl_create_stream(int fd)
 {
 	return compat_ioctl_no_arg(fd, LTTNG_KERNEL_OLD_STREAM,
@@ -404,6 +409,12 @@ end:
 int kernctl_buffer_flush(int fd)
 {
 	return ioctl(fd, RING_BUFFER_FLUSH);
+}
+
+/* returns the version of the metadata. */
+int kernctl_get_metadata_version(int fd, uint64_t *version)
+{
+	return ioctl(fd, RING_BUFFER_GET_METADATA_VERSION, version);
 }
 
 