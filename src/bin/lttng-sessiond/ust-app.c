93105672efbc65a5940b3c02da1412eec00a352d
Jérémie Galarneau <jeremie.galarneau@efficios.com>
2016-05-16 01:20:47 -0400

    Snapshot warning if there are no events in one of the domains
    Remove the "no data" check from the UST snapshot code since
    
    1) It would report that no data was recorded in the snapshot even
       if the kernel tracer had recorded events,
    2) The kernel consumer does not seem to report such a condition,
       making it hard to perform the check without modifying the
       consumer.
    
    The consumer interfaces could be modified to reliably provide
    this information in the future.
    
    However, this warning is not provided in the other tracing
    modes, making its usefulness questionable. An empty snapshot is
    still a valid trace and users should not always expect a trace
    to contain events. Moreover, "warning" that no data was produced
    implies an error may have occured and is probably as confusing
    as opening an empty trace to the average user.
    
    Fixes #1012
    
    Signed-off-by: Jérémie Galarneau <jeremie.galarneau@efficios.com>

--- src/bin/lttng-sessiond/ust-app.c
+++ src/bin/lttng-sessiond/ust-app.c
@@ -5726,7 +5726,6 @@ int ust_app_snapshot_record(struct ltt_ust_session *usess,
 		uint64_t nb_packets_per_stream)
 {
 	int ret = 0;
-	unsigned int snapshot_done = 0;
 	struct lttng_ht_iter iter;
 	struct ust_app *app;
 	char pathname[PATH_MAX];
@@ -5778,7 +5777,6 @@ int ust_app_snapshot_record(struct ltt_ust_session *usess,
 			if (ret < 0) {
 				goto error;
 			}
-			snapshot_done = 1;
 		}
 		break;
 	}
@@ -5831,22 +5829,12 @@ int ust_app_snapshot_record(struct ltt_ust_session *usess,
 			if (ret < 0) {
 				goto error;
 			}
-			snapshot_done = 1;
 		}
 		break;
 	}
 	default:
 		assert(0);
 		break;
-	}
-
-	if (!snapshot_done) {
-		/*
-		 * If no snapshot was made and we are not in the error path, this means
-		 * that there are no buffers thus no (prior) application to snapshot
-		 * data from so we have simply NO data.
-		 */
-		ret = -ENODATA;
 	}
 
 error:
