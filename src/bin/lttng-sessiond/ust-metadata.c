e85e37239d682b8127005c3f63f72999193214c1
Philippe Proulx <eeppeliteloop@gmail.com>
2016-07-04 17:48:42 -0400

    Add support for UST's autoincrementing enum entries
    Signed-off-by: Philippe Proulx <eeppeliteloop@gmail.com>
    Signed-off-by: Jérémie Galarneau <jeremie.galarneau@efficios.com>

--- src/bin/lttng-sessiond/ust-metadata.c
+++ src/bin/lttng-sessiond/ust-metadata.c
@@ -300,38 +300,52 @@ int ust_metadata_enum_statedump(struct ust_registry_session *session,
 				goto end;
 			}
 		}
-		ret = lttng_metadata_printf(session,
-				"\" = ");
+		ret = lttng_metadata_printf(session, "\"");
 		if (ret) {
 			goto end;
 		}
 
-		if (entry->start.signedness) {
-			ret = lttng_metadata_printf(session,
-				"%lld", (long long) entry->start.value);
+		if (entry->u.options.is_auto) {
+			ret = lttng_metadata_printf(session, ",\n");
+			if (ret) {
+				goto end;
+			}
 		} else {
 			ret = lttng_metadata_printf(session,
-				"%llu", entry->start.value);
-		}
-		if (ret) {
-			goto end;
-		}
+					" = ");
+			if (ret) {
+				goto end;
+			}
 
-		if (entry->start.signedness == entry->end.signedness &&
-				entry->start.value == entry->end.value) {
-			ret = lttng_metadata_printf(session,
-				",\n");
-		} else {
-			if (entry->end.signedness) {
+			if (entry->start.signedness) {
 				ret = lttng_metadata_printf(session,
-					" ... %lld,\n", (long long) entry->end.value);
+					"%lld", (long long) entry->start.value);
 			} else {
 				ret = lttng_metadata_printf(session,
-					" ... %llu,\n", entry->end.value);
+					"%llu", entry->start.value);
 			}
-		}
-		if (ret) {
-			goto end;
+			if (ret) {
+				goto end;
+			}
+
+			if (entry->start.signedness == entry->end.signedness &&
+					entry->start.value ==
+						entry->end.value) {
+				ret = lttng_metadata_printf(session, ",\n");
+			} else {
+				if (entry->end.signedness) {
+					ret = lttng_metadata_printf(session,
+						" ... %lld,\n",
+						(long long) entry->end.value);
+				} else {
+					ret = lttng_metadata_printf(session,
+						" ... %llu,\n",
+						entry->end.value);
+				}
+			}
+			if (ret) {
+				goto end;
+			}
 		}
 	}
 	nesting--;
