bac9a7eed3b7ca2cf243db6b298e0cc74ca9e424
Philippe Proulx <eeppeliteloop@gmail.com>
2016-07-04 18:49:48 -0400

    fixup! Add support for UST's autoincrementing enum entries

--- src/bin/lttng-sessiond/ust-metadata.c
+++ src/bin/lttng-sessiond/ust-metadata.c
@@ -310,41 +310,42 @@ int ust_metadata_enum_statedump(struct ust_registry_session *session,
 			if (ret) {
 				goto end;
 			}
-			continue;
-		}
-
-		ret = lttng_metadata_printf(session,
-				" = ");
-		if (ret) {
-			goto end;
-		}
-
-		if (entry->start.signedness) {
-			ret = lttng_metadata_printf(session,
-				"%lld", (long long) entry->start.value);
 		} else {
 			ret = lttng_metadata_printf(session,
-				"%llu", entry->start.value);
-		}
-		if (ret) {
-			goto end;
-		}
+					" = ");
+			if (ret) {
+				goto end;
+			}
 
-		if (entry->start.signedness == entry->end.signedness &&
-				entry->start.value == entry->end.value) {
-			ret = lttng_metadata_printf(session,
-				",\n");
-		} else {
-			if (entry->end.signedness) {
+			if (entry->start.signedness) {
 				ret = lttng_metadata_printf(session,
-					" ... %lld,\n", (long long) entry->end.value);
+					"%lld", (long long) entry->start.value);
 			} else {
 				ret = lttng_metadata_printf(session,
-					" ... %llu,\n", entry->end.value);
+					"%llu", entry->start.value);
 			}
-		}
-		if (ret) {
-			goto end;
+			if (ret) {
+				goto end;
+			}
+
+			if (entry->start.signedness == entry->end.signedness &&
+					entry->start.value ==
+						entry->end.value) {
+				ret = lttng_metadata_printf(session, ",\n");
+			} else {
+				if (entry->end.signedness) {
+					ret = lttng_metadata_printf(session,
+						" ... %lld,\n",
+						(long long) entry->end.value);
+				} else {
+					ret = lttng_metadata_printf(session,
+						" ... %llu,\n",
+						entry->end.value);
+				}
+			}
+			if (ret) {
+				goto end;
+			}
 		}
 	}
 	nesting--;
