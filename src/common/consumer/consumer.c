a837d60e6ef365c87361a4486087e40a791a4e5f
Jérémie Galarneau <jeremie.galarneau@efficios.com>
2016-12-21 17:56:24 -0500

    Fix: protect the channel's metadata stream using the metadata cache lock
    The consumer_thread_data_poll and consumer_thread_metadata_poll
    both access the channel's metadata stream.
    
    During a session destruction, consumer_thread_metadata_poll will
    destroy all metadata streams. However, the consumer_thread_data_poll
    may still invoke a consumer_metadata_cache_write() triggered
    by a "ready" subbuffer. Hence, the metadata stream must be protected
    from this action by the metadata cache lock.
    
    Signed-off-by: Jérémie Galarneau <jeremie.galarneau@efficios.com>

--- src/common/consumer/consumer.c
+++ src/common/consumer/consumer.c
@@ -47,6 +47,7 @@
 #include <common/consumer/consumer-stream.h>
 #include <common/consumer/consumer-testpoint.h>
 #include <common/align.h>
+#include <common/consumer/consumer-metadata-cache.h>
 
 struct lttng_consumer_global_data consumer_data = {
 	.stream_count = 0,
@@ -2052,6 +2053,7 @@ void consumer_del_metadata_stream(struct lttng_consumer_stream *stream,
 
 	pthread_mutex_lock(&consumer_data.lock);
 	pthread_mutex_lock(&stream->chan->lock);
+	pthread_mutex_lock(&stream->chan->metadata_cache->lock);
 	pthread_mutex_lock(&stream->lock);
 
 	/* Remove any reference to that stream. */
@@ -2077,6 +2079,7 @@ void consumer_del_metadata_stream(struct lttng_consumer_stream *stream,
 	stream->chan->metadata_stream = NULL;
 
 	pthread_mutex_unlock(&stream->lock);
+	pthread_mutex_unlock(&stream->chan->metadata_cache->lock);
 	pthread_mutex_unlock(&stream->chan->lock);
 	pthread_mutex_unlock(&consumer_data.lock);
 
