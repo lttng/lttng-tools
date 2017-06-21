f181fa5afa242b51c1a6cd8868f56a579a7e39a0
Jérémie Galarneau <jeremie.galarneau@efficios.com>
2017-06-21 09:35:12 -0400

    Hide internal string-utils symbols
    Signed-off-by: Jérémie Galarneau <jeremie.galarneau@efficios.com>

--- src/common/string-utils/string-utils.h
+++ src/common/string-utils/string-utils.h
@@ -19,13 +19,27 @@
 #define _STRING_UTILS_H
 
 #include <stdbool.h>
+#include <common/macros.h>
 
+LTTNG_HIDDEN
 void strutils_normalize_star_glob_pattern(char *pattern);
+
+LTTNG_HIDDEN
 bool strutils_is_star_glob_pattern(const char *pattern);
+
+LTTNG_HIDDEN
 bool strutils_is_star_at_the_end_only_glob_pattern(const char *pattern);
+
+LTTNG_HIDDEN
 char *strutils_unescape_string(const char *input, char only_char);
+
+LTTNG_HIDDEN
 char **strutils_split(const char *input, char delim, bool escape_delim);
+
+LTTNG_HIDDEN
 void strutils_free_null_terminated_array_of_strings(char **array);
+
+LTTNG_HIDDEN
 size_t strutils_array_of_strings_len(char * const *array);
 
 #endif /* _STRING_UTILS_H */
