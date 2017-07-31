04b03ebf1e52445551ab86bd04d7727d18f9c4df
Jérémie Galarneau <jeremie.galarneau@efficios.com>
2017-07-31 14:08:19 -0400

    Docs: document the lttng_action_notify action type
    Signed-off-by: Jérémie Galarneau <jeremie.galarneau@efficios.com>

--- include/lttng/action/notify.h
+++ include/lttng/action/notify.h
@@ -24,6 +24,17 @@ struct lttng_action;
 extern "C" {
 #endif
 
+/*
+ * Create a newly allocated notification action object.
+ *
+ * A "notify" action will emit a notification to all clients which have an
+ * open notification channel. In order to receive this notification, clients
+ * must have subscribed to a condition equivalent to the one paired to this
+ * notify action in a trigger.
+ *
+ * Returns a new action on success, NULL on failure. This action must be
+ * destroyed using lttng_action_destroy().
+ */
 extern struct lttng_action *lttng_action_notify_create(void);
 
 #ifdef __cplusplus
