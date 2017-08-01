97cf8c646983da1b0196c390b5c30a58ea5af99c
Jérémie Galarneau <jeremie.galarneau@efficios.com>
2017-08-01 14:29:02 -0400

    Docs: document the notification API
    Signed-off-by: Jérémie Galarneau <jeremie.galarneau@efficios.com>

--- include/lttng/notification/notification.h
+++ include/lttng/notification/notification.h
@@ -27,16 +27,39 @@ struct lttng_evaluation;
 struct lttng_notification;
 
 /*
- * The notification retains ownership of both the condition and evaluation.
- * Destroying the notification will also destroy the notification and evaluation
- * objects.
+ * Get a notification's condition.
+ *
+ * The notification retains the ownership of both the condition and evaluation
+ * objects. Hence, it is not valid to access those objects after the destruction
+ * of their associated notification.
+ *
+ * The caller should check the condition's type in order to use the appropriate
+ * specialized functions to access the condition's properties.
+ *
+ * Returns an lttng_condition object on success, NULL on error.
  */
 extern const struct lttng_condition *lttng_notification_get_condition(
 		struct lttng_notification *notification);
 
+/*
+ * Get a notification's condition's evaluation.
+ *
+ * The notification retains the ownership of the evaluation object. Hence, it is
+ * not valid to access that object after the destruction of its associated
+ * notification.
+ *
+ * The caller should check the evaluation's type in order to use the appropriate
+ * specialized functions to access the evaluation's properties.
+ *
+ * Returns an lttng_evaluation object on success, NULL on error.
+ */
 extern const struct lttng_evaluation *lttng_notification_get_evaluation(
 		struct lttng_notification *notification);
 
+/*
+ * Destroys (frees) a notification. The notification's condition and evaluation
+ * are destroyed as a side-effect.
+ */
 extern void lttng_notification_destroy(struct lttng_notification *notification);
 
 #ifdef __cplusplus
