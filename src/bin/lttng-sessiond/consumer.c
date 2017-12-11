a0307da8f7eccc2379a2ff64136de264b506735d
Julien Desfossez <jdesfossez@efficios.com>
2017-12-11 14:16:06 -0500

    Keep the base directory of a relay session separate
    For the session rotation feature, we need a way to modify the path of a
    channel, so we need to keep the "<hostname>/<session-name>" information
    separate in the consumer_net structure for future use.
    
    Signed-off-by: Julien Desfossez <jdesfossez@efficios.com>

--- src/bin/lttng-sessiond/consumer.c
+++ src/bin/lttng-sessiond/consumer.c
@@ -715,11 +715,12 @@ int consumer_set_network_uri(struct consumer_output *obj,
 			goto error;
 		}
 
-		if (lttng_strncpy(obj->subdir, tmp_path, sizeof(obj->subdir))) {
+		if (lttng_strncpy(obj->dst.net.base_dir, tmp_path,
+					sizeof(obj->dst.net.base_dir))) {
 			ret = -LTTNG_ERR_INVALID;
 			goto error;
 		}
-		DBG3("Consumer set network uri subdir path %s", tmp_path);
+		DBG3("Consumer set network uri base_dir path %s", tmp_path);
 	}
 
 	return 0;
@@ -1405,7 +1406,8 @@ int consumer_snapshot_channel(struct consumer_socket *socket, uint64_t key,
 		msg.u.snapshot_channel.use_relayd = 1;
 		ret = snprintf(msg.u.snapshot_channel.pathname,
 				sizeof(msg.u.snapshot_channel.pathname),
-				"%s/%s-%s-%" PRIu64 "%s", output->consumer->subdir,
+				"%s/%s/%s-%s-%" PRIu64 "%s", output->consumer->dst.net.base_dir,
+				output->consumer->subdir,
 				output->name, output->datetime, output->nb_snapshot,
 				session_path);
 		if (ret < 0) {
