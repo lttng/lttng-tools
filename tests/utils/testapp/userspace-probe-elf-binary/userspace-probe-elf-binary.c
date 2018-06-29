a9c2df2bfce7a27b53ee5d5101f259f3e1f506b1
Francis Deslauriers <francis.deslauriers@efficios.com>
2018-06-29 16:09:15 -0400

    Implement userspace-probe regression tests
    This adds regression testcases for the following userspace probe
    features and should-fail scenarios.
    
    Features:
      * ELF function location tracing works,
      * ELF C++ mangled function and method location tracing works,
      * SDT tracepoint location tracing works,
      * Implicit location type defaults to ELF location,
      * Binary found $PATH,
      * Binary found using full path,
      * Binary with colon in file name,
      * Disabling of all location types,
      * Listing of all location types,
      * Saving and loading of all location types,
      * SDT probe with one or more callsites,
      * SDT probe with arguments,
      * SDT probe in shared object,
      * SDT probe in dlopen()ed and ldpreload()ed shared object.
    Should-fail:
      * Target binary does not exist,
      * Target binary not ELF file,
      * ELF symbol not found in target binary,
      * ELF symbol not a function,
      * SDT provider name not found,
      * SDT probe name not found,
      * SDT probe guarded by a semaphore.
    
    We also make sure to disable testcases when the necessary files/tools
    for them are not found:
      * Disables C++ testcases if no C++ compiler is found.
      * Disables SDT testcases if the Systemtap header files are not found.
        This is tested by trying to compile a simple C program with a SDT
        probe. If it fails, it will only disable the testcases and not the
        feature since the SDT userspace probe feature does not use this header
        for parsing.
    
    Signed-off-by: Francis Deslauriers <francis.deslauriers@efficios.com>
    Signed-off-by: Erica Bugden <ebugden@efficios.com>
    Signed-off-by: Jérémie Galarneau <jeremie.galarneau@efficios.com>

---
+++ tests/utils/testapp/userspace-probe-elf-binary/userspace-probe-elf-binary.c
@@ -0,0 +1,28 @@
+/*
+ * Copyright (C) 2017  Francis Deslauriers <francis.deslauriers@efficios.com>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA
+ */
+
+volatile int not_a_function = 0;
+void __attribute__ ((noinline))  test_function()
+{
+	not_a_function += 1;
+}
+int main(int argc, char *argv[])
+{
+	test_function();
+	return 0;
+}
