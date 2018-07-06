a06914793e67ef1fba517e1c83ddfd015b6a4a3b
Jérémie Galarneau <jeremie.galarneau@efficios.com>
2018-07-06 11:35:29 -0400

    fd-tracker build fix: missing parameter in poll compat function signature
    Signed-off-by: Jérémie Galarneau <jeremie.galarneau@efficios.com>

--- src/common/fd-tracker/utils-poll.c
+++ src/common/fd-tracker/utils-poll.c
@@ -23,7 +23,7 @@
  * The epoll variant of the poll compat layer creates an unsuspendable fd which
  * must be tracked.
  */
-int fd_tracker_util_poll_create(struct fd_tracker *tracker,
+int fd_tracker_util_poll_create(struct fd_tracker *tracker, const char *name,
 		struct lttng_poll_event *events, int size, int flags)
 {
 	return lttng_poll_create(events, size, flags);
