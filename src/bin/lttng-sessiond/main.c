1785d7f29bf3f162f37152ee9ea8907b9bc6d9d2
Jérémie Galarneau <jeremie.galarneau@efficios.com>
2018-11-30 16:33:38 -0500

    Launch application registration thread using lttng_thread
    Signed-off-by: Jérémie Galarneau <jeremie.galarneau@efficios.com>

--- src/bin/lttng-sessiond/main.c
+++ src/bin/lttng-sessiond/main.c
@@ -84,6 +84,7 @@
 #include "thread.h"
 #include "client.h"
 #include "dispatch.h"
+#include "register.h"
 
 static const char *help_msg =
 #ifdef LTTNG_EMBED_HELP
@@ -134,7 +135,6 @@ static const struct option long_options[] = {
 
 /* Command line options to ignore from configuration file */
 static const char *config_ignore_options[] = { "help", "version", "config" };
-static int apps_sock = -1;
 
 /*
  * This pipe is used to inform the thread managing application communication
@@ -145,7 +145,6 @@ static int apps_cmd_pipe[2] = { -1, -1 };
 /* Pthread, Mutexes and Semaphores */
 static pthread_t apps_thread;
 static pthread_t apps_notify_thread;
-static pthread_t reg_apps_thread;
 static pthread_t kernel_thread;
 static pthread_t agent_reg_thread;
 static pthread_t load_session_thread;
@@ -381,31 +380,6 @@ static void sessiond_cleanup_options(void)
 	sessiond_config_fini(&config);
 
 	run_as_destroy_worker();
-}
-
-/*
- * Notify UST applications using the shm mmap futex.
- */
-static int notify_ust_apps(int active)
-{
-	char *wait_shm_mmap;
-
-	DBG("Notifying applications of session daemon state: %d", active);
-
-	/* See shm.c for this call implying mmap, shm and futex calls */
-	wait_shm_mmap = shm_ust_get_mmap(config.wait_shm_path.value, is_root);
-	if (wait_shm_mmap == NULL) {
-		goto error;
-	}
-
-	/* Wake waiting process */
-	futex_wait_update((int32_t *) wait_shm_mmap, active);
-
-	/* Apps notified successfully */
-	return 0;
-
-error:
-	return -1;
 }
 
 /*
@@ -1248,232 +1222,6 @@ error_testpoint:
 }
 
 /*
- * This thread manage application registration.
- */
-static void *thread_registration_apps(void *data)
-{
-	int sock = -1, i, ret, pollfd, err = -1;
-	uint32_t revents, nb_fd;
-	struct lttng_poll_event events;
-	/*
-	 * Get allocated in this thread, enqueued to a global queue, dequeued and
-	 * freed in the manage apps thread.
-	 */
-	struct ust_command *ust_cmd = NULL;
-
-	DBG("[thread] Manage application registration started");
-
-	health_register(health_sessiond, HEALTH_SESSIOND_TYPE_APP_REG);
-
-	if (testpoint(sessiond_thread_registration_apps)) {
-		goto error_testpoint;
-	}
-
-	ret = lttcomm_listen_unix_sock(apps_sock);
-	if (ret < 0) {
-		goto error_listen;
-	}
-
-	/*
-	 * Pass 2 as size here for the thread quit pipe and apps socket. Nothing
-	 * more will be added to this poll set.
-	 */
-	ret = sessiond_set_thread_pollset(&events, 2);
-	if (ret < 0) {
-		goto error_create_poll;
-	}
-
-	/* Add the application registration socket */
-	ret = lttng_poll_add(&events, apps_sock, LPOLLIN | LPOLLRDHUP);
-	if (ret < 0) {
-		goto error_poll_add;
-	}
-
-	/* Notify all applications to register */
-	ret = notify_ust_apps(1);
-	if (ret < 0) {
-		ERR("Failed to notify applications or create the wait shared memory.\n"
-			"Execution continues but there might be problem for already\n"
-			"running applications that wishes to register.");
-	}
-
-	while (1) {
-		DBG("Accepting application registration");
-
-		/* Inifinite blocking call, waiting for transmission */
-	restart:
-		health_poll_entry();
-		ret = lttng_poll_wait(&events, -1);
-		health_poll_exit();
-		if (ret < 0) {
-			/*
-			 * Restart interrupted system call.
-			 */
-			if (errno == EINTR) {
-				goto restart;
-			}
-			goto error;
-		}
-
-		nb_fd = ret;
-
-		for (i = 0; i < nb_fd; i++) {
-			health_code_update();
-
-			/* Fetch once the poll data */
-			revents = LTTNG_POLL_GETEV(&events, i);
-			pollfd = LTTNG_POLL_GETFD(&events, i);
-
-			if (!revents) {
-				/* No activity for this FD (poll implementation). */
-				continue;
-			}
-
-			/* Thread quit pipe has been closed. Killing thread. */
-			ret = sessiond_check_thread_quit_pipe(pollfd, revents);
-			if (ret) {
-				err = 0;
-				goto exit;
-			}
-
-			/* Event on the registration socket */
-			if (pollfd == apps_sock) {
-				if (revents & LPOLLIN) {
-					sock = lttcomm_accept_unix_sock(apps_sock);
-					if (sock < 0) {
-						goto error;
-					}
-
-					/*
-					 * Set socket timeout for both receiving and ending.
-					 * app_socket_timeout is in seconds, whereas
-					 * lttcomm_setsockopt_rcv_timeout and
-					 * lttcomm_setsockopt_snd_timeout expect msec as
-					 * parameter.
-					 */
-					if (config.app_socket_timeout >= 0) {
-						(void) lttcomm_setsockopt_rcv_timeout(sock,
-								config.app_socket_timeout * 1000);
-						(void) lttcomm_setsockopt_snd_timeout(sock,
-								config.app_socket_timeout * 1000);
-					}
-
-					/*
-					 * Set the CLOEXEC flag. Return code is useless because
-					 * either way, the show must go on.
-					 */
-					(void) utils_set_fd_cloexec(sock);
-
-					/* Create UST registration command for enqueuing */
-					ust_cmd = zmalloc(sizeof(struct ust_command));
-					if (ust_cmd == NULL) {
-						PERROR("ust command zmalloc");
-						ret = close(sock);
-						if (ret) {
-							PERROR("close");
-						}
-						goto error;
-					}
-
-					/*
-					 * Using message-based transmissions to ensure we don't
-					 * have to deal with partially received messages.
-					 */
-					ret = lttng_fd_get(LTTNG_FD_APPS, 1);
-					if (ret < 0) {
-						ERR("Exhausted file descriptors allowed for applications.");
-						free(ust_cmd);
-						ret = close(sock);
-						if (ret) {
-							PERROR("close");
-						}
-						sock = -1;
-						continue;
-					}
-
-					health_code_update();
-					ret = ust_app_recv_registration(sock, &ust_cmd->reg_msg);
-					if (ret < 0) {
-						free(ust_cmd);
-						/* Close socket of the application. */
-						ret = close(sock);
-						if (ret) {
-							PERROR("close");
-						}
-						lttng_fd_put(LTTNG_FD_APPS, 1);
-						sock = -1;
-						continue;
-					}
-					health_code_update();
-
-					ust_cmd->sock = sock;
-					sock = -1;
-
-					DBG("UST registration received with pid:%d ppid:%d uid:%d"
-							" gid:%d sock:%d name:%s (version %d.%d)",
-							ust_cmd->reg_msg.pid, ust_cmd->reg_msg.ppid,
-							ust_cmd->reg_msg.uid, ust_cmd->reg_msg.gid,
-							ust_cmd->sock, ust_cmd->reg_msg.name,
-							ust_cmd->reg_msg.major, ust_cmd->reg_msg.minor);
-
-					/*
-					 * Lock free enqueue the registration request. The red pill
-					 * has been taken! This apps will be part of the *system*.
-					 */
-					cds_wfcq_enqueue(&ust_cmd_queue.head, &ust_cmd_queue.tail, &ust_cmd->node);
-
-					/*
-					 * Wake the registration queue futex. Implicit memory
-					 * barrier with the exchange in cds_wfcq_enqueue.
-					 */
-					futex_nto1_wake(&ust_cmd_queue.futex);
-				} else if (revents & (LPOLLERR | LPOLLHUP | LPOLLRDHUP)) {
-					ERR("Register apps socket poll error");
-					goto error;
-				} else {
-					ERR("Unexpected poll events %u for sock %d", revents, pollfd);
-					goto error;
-				}
-			}
-		}
-	}
-
-exit:
-error:
-	/* Notify that the registration thread is gone */
-	notify_ust_apps(0);
-
-	if (apps_sock >= 0) {
-		ret = close(apps_sock);
-		if (ret) {
-			PERROR("close");
-		}
-	}
-	if (sock >= 0) {
-		ret = close(sock);
-		if (ret) {
-			PERROR("close");
-		}
-		lttng_fd_put(LTTNG_FD_APPS, 1);
-	}
-	unlink(config.apps_unix_sock_path.value);
-
-error_poll_add:
-	lttng_poll_clean(&events);
-error_listen:
-error_create_poll:
-error_testpoint:
-	DBG("UST Registration thread cleanup complete");
-	if (err) {
-		health_error();
-		ERR("Health error occurred in %s", __func__);
-	}
-	health_unregister(health_sessiond);
-
-	return NULL;
-}
-
-/*
  * Setup necessary data for kernel tracer action.
  */
 static int init_kernel_tracer(void)
@@ -2057,49 +1805,6 @@ static int set_options(int argc, char **argv)
 
 end:
 	free(optstring);
-	return ret;
-}
-
-/*
- * Creates the application socket.
- */
-static int init_daemon_socket(void)
-{
-	int ret = 0;
-	mode_t old_umask;
-
-	old_umask = umask(0);
-
-	/* Create the application unix socket */
-	apps_sock = lttcomm_create_unix_sock(config.apps_unix_sock_path.value);
-	if (apps_sock < 0) {
-		ERR("Create unix sock failed: %s", config.apps_unix_sock_path.value);
-		ret = -1;
-		goto end;
-	}
-
-	/* Set the cloexec flag */
-	ret = utils_set_fd_cloexec(apps_sock);
-	if (ret < 0) {
-		ERR("Unable to set CLOEXEC flag to the app Unix socket (fd: %d). "
-				"Continuing but note that the consumer daemon will have a "
-				"reference to this socket on exec()", apps_sock);
-	}
-
-	/* File permission MUST be 666 */
-	ret = chmod(config.apps_unix_sock_path.value,
-			S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH | S_IWOTH);
-	if (ret < 0) {
-		ERR("Set file permissions failed: %s", config.apps_unix_sock_path.value);
-		PERROR("chmod");
-		goto end;
-	}
-
-	DBG3("Session daemon application socket %d created",
-			apps_sock);
-
-end:
-	umask(old_umask);
 	return ret;
 }
 
@@ -2768,12 +2473,6 @@ int main(int argc, char **argv)
 		goto exit_init_data;
 	}
 
-	/* Setup the needed unix socket */
-	if (init_daemon_socket()) {
-		retval = -1;
-		goto exit_init_data;
-	}
-
 	/* Set credentials to socket */
 	if (is_root && set_permissions(config.rundir.value)) {
 		retval = -1;
@@ -2900,13 +2599,8 @@ int main(int argc, char **argv)
 	}
 
 	/* Create thread to manage application registration. */
-	ret = pthread_create(&reg_apps_thread, default_pthread_attr(),
-			thread_registration_apps, (void *) NULL);
-	if (ret) {
-		errno = ret;
-		PERROR("pthread_create registration");
+	if (!launch_application_registration_thread(&ust_cmd_queue)) {
 		retval = -1;
-		stop_threads();
 		goto exit_reg_apps;
 	}
 
@@ -3029,13 +2723,6 @@ exit_apps_notify:
 		retval = -1;
 	}
 exit_apps:
-
-	ret = pthread_join(reg_apps_thread, &status);
-	if (ret) {
-		errno = ret;
-		PERROR("pthread_join");
-		retval = -1;
-	}
 exit_reg_apps:
 exit_dispatch:
 exit_client:
