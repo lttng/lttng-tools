293d47db24588336be65fd5fb61e1376bf77619d
Jérémie Galarneau <jeremie.galarneau@efficios.com>
2018-12-04 17:46:00 -0500

    Stop sessions before destroying on teardown of session daemon
    Stopping sessions ensures that trace data is no longer produced
    for a session which will allow pending rotations to complete.
    
    It also ensure that no data is produced beyond the last rename
    of a rotated session's output folder.
    
    Signed-off-by: Jérémie Galarneau <jeremie.galarneau@efficios.com>

--- src/bin/lttng-sessiond/main.c
+++ src/bin/lttng-sessiond/main.c
@@ -1705,6 +1705,7 @@ static void destroy_all_sessions_and_wait(void)
 		if (session->destroyed) {
 			goto unlock_session;
 		}
+		(void) cmd_stop_trace(session);
 		(void) cmd_destroy_session(session,
 				notification_thread_handle);
 	unlock_session:
