9b27c72144f412ea2749a3ed18b49568881599a8
Jérémie Galarneau <jeremie.galarneau@efficios.com>
2019-04-07 14:39:08 -0400

    lttng: make the configuration file interface const correct
    The interface defined in conf.h is not const-correct thus hindering
    the use of const-correct code in the CLI client.
    
    Signed-off-by: Jérémie Galarneau <jeremie.galarneau@efficios.com>

--- src/bin/lttng/conf.h
+++ src/bin/lttng/conf.h
@@ -20,15 +20,15 @@
 
 #define CONFIG_FILENAME ".lttngrc"
 
-void config_destroy(char *path);
+void config_destroy(const char *path);
 void config_destroy_default(void);
 int config_exists(const char *path);
-int config_init(char *path);
-int config_add_session_name(char *path, char *name);
+int config_init(const char *path);
+int config_add_session_name(const char *path, const char *name);
 
 /* Must free() the return pointer */
-char *config_read_session_name(char *path);
-char *config_read_session_name_quiet(char *path);
-char *config_get_file_path(char *path);
+char *config_read_session_name(const char *path);
+char *config_read_session_name_quiet(const char *path);
+char *config_get_file_path(const char *path);
 
 #endif /* _LTTNG_CONFIG_H */
