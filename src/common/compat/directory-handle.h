a42089bd2788e4eeb14527c53d9d620fc33141df
Jérémie Galarneau <jeremie.galarneau@efficios.com>
2019-04-17 16:55:27 -0400

    Add mkdirat utils and runas wrappers
    The lttng_directory_handle allows its user to keep a handle to
    a directory and to create subdirectories relative to it.
    
    On platforms implementing POSIX.2008, a directory file descriptor
    is used to maintain a handle to an existing directory and used
    in conjunction with mkdirat() to create subdirectories.
    
    Derelict platforms (such as Solaris 10) use an alternative
    implementation which carries the location of the root directory
    and builds subdirectory paths on creation.
    
    The existing mkdir utils are re-implemented using this new
    interface (using the special AT_FDCWD file descriptor value, when
    applicable) to limit code duplication.
    
    The implementation of the directory handle and its users is
    automatically selected based on the presence of the dirfd() function,
    but can also be explicitly chosen using the --enable/disable-dirfd
    configuration option.
    
    Signed-off-by: Jérémie Galarneau <jeremie.galarneau@efficios.com>

---
+++ src/common/compat/directory-handle.h
@@ -0,0 +1,102 @@
+/*
+ * Copyright (C) 2019 - Jérémie Galarneau <jeremie.galarneau@efficios.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License, version 2 only,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+#ifndef _COMPAT_DIRECTORY_HANDLE_H
+#define _COMPAT_DIRECTORY_HANDLE_H
+
+#include <common/macros.h>
+#include <common/credentials.h>
+
+/*
+ * Some platforms, such as Solaris 10, do not support directory file descriptors
+ * and their associated functions (*at(...)), which are defined in POSIX.2008.
+ *
+ * This wrapper provides a handle that is either a copy of a directory's path
+ * or a directory file descriptors, depending on the platform's capabilities.
+ */
+#ifdef COMPAT_DIRFD
+struct lttng_directory_handle {
+	int dirfd;
+};
+#else
+struct lttng_directory_handle {
+	char *base_path;
+};
+#endif
+
+/*
+ * Initialize a directory handle to the provided path. Passing a NULL path
+ * returns a handle to the current working directory. The working directory
+ * is not sampled; it will be accessed at the time of use of the functions
+ * of this API.
+ *
+ * An initialized directory handle must be finalized using
+ * lttng_directory_handle_fini().
+ */
+LTTNG_HIDDEN
+int lttng_directory_handle_init(struct lttng_directory_handle *handle,
+		const char *path);
+
+LTTNG_HIDDEN
+int lttng_directory_handle_init_from_dirfd(
+		struct lttng_directory_handle *handle, int dirfd);
+
+/*
+ * Release the resources of a directory handle.
+ */
+LTTNG_HIDDEN
+void lttng_directory_handle_fini(struct lttng_directory_handle *handle);
+
+/*
+ * Create a subdirectory relative to a directory handle.
+ */
+LTTNG_HIDDEN
+int lttng_directory_handle_create_subdirectory(
+		const struct lttng_directory_handle *handle,
+		const char *subdirectory,
+		mode_t mode);
+
+/*
+ * Create a subdirectory relative to a directory handle
+ * as a given user.
+ */
+LTTNG_HIDDEN
+int lttng_directory_handle_create_subdirectory_as_user(
+		const struct lttng_directory_handle *handle,
+		const char *subdirectory,
+		mode_t mode, struct lttng_credentials *creds);
+
+/*
+ * Recursively create a directory relative to a directory handle.
+ */
+LTTNG_HIDDEN
+int lttng_directory_handle_create_subdirectory_recursive(
+		const struct lttng_directory_handle *handle,
+		const char *subdirectory_path,
+		mode_t mode);
+
+/*
+ * Recursively create a directory relative to a directory handle
+ * as a given user.
+ */
+LTTNG_HIDDEN
+int lttng_directory_handle_create_subdirectory_recursive_as_user(
+		const struct lttng_directory_handle *handle,
+		const char *subdirectory_path,
+		mode_t mode, struct lttng_credentials *creds);
+
+#endif /* _COMPAT_PATH_HANDLE_H */
