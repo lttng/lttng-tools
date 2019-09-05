compudj/lttng-tools-dev

Fix: honor base path for network URIs
Starting from 2.11, the following use-case stopped working as expected:

lttng create --set-url=net://localhost/my/path/custom
lttng enable-event -u -a
lttng start
[ run app ]

The output trace is expected in the following directory on the relayd
(no timestamp suffix):

$LTTNG_HOME/lttng-traces/[hostname]/my/path/custom

Add the base_path information to the session object in the session
daemon, extracted from the network URI, and pass that information to the
relay daemon through the create session (2.11) command.

It also fixes the use-case:

lttng create --snapshot --set-url=net://localhost/my/path/custom
lttng enable-event -u -a
lttng start
[ run app ]
lttng snapshot record

which is expected to record the snapshot under:
$LTTNG_HOME/lttng-traces/[hostname]/my/path/custom/snapshot-[N]-[timestamp]

Similar situation for:

lttng create --snapshot
lttng enable-event -u -a
lttng start
[ run app ]
lttng snapshot record net://localhost/my/path/custom

which is expected to record the snapshot under:
$LTTNG_HOME/lttng-traces/[hostname]/my/path/custom/snapshot-[N]-[timestamp]

Note that specifying the base_path on the snapshot record command
overrides the base path specified at create.

Signed-off-by: Mathieu Desnoyers <mathieu.desnoyers@efficios.com>

src/bin/lttng-relayd/main.c
@@ -1074,6 +1074,7 @@ static int relay_create_session(const struct lttcomm_relayd_hdr *recv_hdr,
 	uint32_t live_timer = 0;
 	bool snapshot = false;
 	/* Left nil for peers < 2.11. */
+	char base_path[PATH_MAX] = {};
 	lttng_uuid sessiond_uuid = {};
 	LTTNG_OPTIONAL(uint64_t) id_sessiond = {};
 	LTTNG_OPTIONAL(uint64_t) current_chunk_id = {};
@@ -1094,7 +1095,7 @@ static int relay_create_session(const struct lttcomm_relayd_hdr *recv_hdr,
 
 		/* From 2.11 to ... */
 		ret = cmd_create_session_2_11(payload, session_name, hostname,
-				&live_timer, &snapshot, &id_sessiond_value,
+				base_path, &live_timer, &snapshot, &id_sessiond_value,
 				sessiond_uuid, &has_current_chunk,
 				&current_chunk_id_value, &creation_time_value);
 		if (lttng_uuid_is_nil(sessiond_uuid)) {
@@ -1115,7 +1116,7 @@ static int relay_create_session(const struct lttcomm_relayd_hdr *recv_hdr,
 		goto send_reply;
 	}
 
-	session = session_create(session_name, hostname, live_timer,
+	session = session_create(session_name, hostname, base_path, live_timer,
 			snapshot, sessiond_uuid,
 			id_sessiond.is_set ? &id_sessiond.value : NULL,
 			current_chunk_id.is_set ? &current_chunk_id.value : NULL,
@@ -2261,29 +2262,49 @@ static int init_session_output_directory_handle(struct relay_session *session,
 		struct lttng_directory_handle *handle)
 {
 	int ret;
-	/* hostname/session_name */
+	/*
+	 * session_directory:
+	 *
+	 * if base_path is NULL
+	 *   hostname/session_name
+	 * else
+	 *   hostname/base_path
+	 */
 	char *session_directory = NULL;
 	/*
 	 * base path + session_directory
 	 * e.g. /home/user/lttng-traces/hostname/session_name
 	 */
 	char *full_session_path = NULL;
-	char creation_time_str[16];
-	struct tm *timeinfo;
 
-	assert(session->creation_time.is_set);
-	timeinfo = localtime(&session->creation_time.value);
-	if (!timeinfo) {
-		ret = -1;
-		goto end;
+	/*
+	 * If base path is set, it overrides the session name for the
+	 * session relative base path. No timestamp is appended if the
+	 * base path is overridden.
+	 */
+	if (session->base_path[0] == '\0') {
+		char creation_time_str[16];
+		struct tm *timeinfo;
+
+		assert(session->creation_time.is_set);
+		timeinfo = localtime(&session->creation_time.value);
+		if (!timeinfo) {
+			ret = -1;
+			goto end;
+		}
+		strftime(creation_time_str, sizeof(creation_time_str), "%Y%m%d-%H%M%S",
+				timeinfo);
+
+		pthread_mutex_lock(&session->lock);
+		ret = asprintf(&session_directory, "%s/%s-%s", session->hostname,
+				session->session_name, creation_time_str);
+		pthread_mutex_unlock(&session->lock);
+	} else {
+		pthread_mutex_lock(&session->lock);
+		ret = asprintf(&session_directory, "%s/%s", session->hostname,
+				session->base_path);
+		pthread_mutex_unlock(&session->lock);
 	}
-	strftime(creation_time_str, sizeof(creation_time_str), "%Y%m%d-%H%M%S",
-			timeinfo);
-
-	pthread_mutex_lock(&session->lock);
-	ret = asprintf(&session_directory, "%s/%s-%s", session->hostname,
-			session->session_name, creation_time_str);
-	pthread_mutex_unlock(&session->lock);
 	if (ret < 0) {
 		PERROR("Failed to format session directory name");
 		goto end;

src/bin/lttng-relayd/session.h
@@ -55,6 +55,7 @@ struct relay_session {
 	LTTNG_OPTIONAL(time_t) creation_time;
 	char session_name[LTTNG_NAME_MAX];
 	char hostname[LTTNG_HOST_NAME_MAX];
+	char base_path[PATH_MAX];
 	uint32_t live_timer;
 
 	/* Session in snapshot mode. */
@@ -125,7 +126,7 @@ struct relay_session {
 };
 
 struct relay_session *session_create(const char *session_name,
-		const char *hostname,
+		const char *hostname, const char *base_path,
 		uint32_t live_timer,
 		bool snapshot,
 		const lttng_uuid sessiond_uuid,

lttng/lttng-tools

Signed-off-by: Jérémie Galarneau <jeremie.galarneau@efficios.com>

src/bin/lttng-relayd/main.c
@@ -1074,6 +1074,7 @@ static int relay_create_session(const struct lttcomm_relayd_hdr *recv_hdr,
 	uint32_t live_timer = 0;
 	bool snapshot = false;
 	/* Left nil for peers < 2.11. */
+	char base_path[LTTNG_PATH_MAX] = {};
 	lttng_uuid sessiond_uuid = {};
 	LTTNG_OPTIONAL(uint64_t) id_sessiond = {};
 	LTTNG_OPTIONAL(uint64_t) current_chunk_id = {};
@@ -1094,7 +1095,7 @@ static int relay_create_session(const struct lttcomm_relayd_hdr *recv_hdr,
 
 		/* From 2.11 to ... */
 		ret = cmd_create_session_2_11(payload, session_name, hostname,
-				&live_timer, &snapshot, &id_sessiond_value,
+				base_path, &live_timer, &snapshot, &id_sessiond_value,
 				sessiond_uuid, &has_current_chunk,
 				&current_chunk_id_value, &creation_time_value);
 		if (lttng_uuid_is_nil(sessiond_uuid)) {
@@ -1115,7 +1116,7 @@ static int relay_create_session(const struct lttcomm_relayd_hdr *recv_hdr,
 		goto send_reply;
 	}
 
-	session = session_create(session_name, hostname, live_timer,
+	session = session_create(session_name, hostname, base_path, live_timer,
 			snapshot, sessiond_uuid,
 			id_sessiond.is_set ? &id_sessiond.value : NULL,
 			current_chunk_id.is_set ? &current_chunk_id.value : NULL,
@@ -2262,29 +2263,49 @@ static int init_session_output_directory_handle(struct relay_session *session,
 		struct lttng_directory_handle *handle)
 {
 	int ret;
-	/* hostname/session_name */
+	/*
+	 * session_directory:
+	 *
+	 * if base_path is NULL
+	 *   hostname/session_name
+	 * else
+	 *   hostname/base_path
+	 */
 	char *session_directory = NULL;
 	/*
 	 * base path + session_directory
 	 * e.g. /home/user/lttng-traces/hostname/session_name
 	 */
 	char *full_session_path = NULL;
-	char creation_time_str[16];
-	struct tm *timeinfo;
 
-	assert(session->creation_time.is_set);
-	timeinfo = localtime(&session->creation_time.value);
-	if (!timeinfo) {
-		ret = -1;
-		goto end;
+	/*
+	 * If base path is set, it overrides the session name for the
+	 * session relative base path. No timestamp is appended if the
+	 * base path is overridden.
+	 */
+	if (session->base_path[0] == '\0') {
+		char creation_time_str[16];
+		struct tm *timeinfo;
+
+		assert(session->creation_time.is_set);
+		timeinfo = localtime(&session->creation_time.value);
+		if (!timeinfo) {
+			ret = -1;
+			goto end;
+		}
+		strftime(creation_time_str, sizeof(creation_time_str), "%Y%m%d-%H%M%S",
+				timeinfo);
+
+		pthread_mutex_lock(&session->lock);
+		ret = asprintf(&session_directory, "%s/%s-%s", session->hostname,
+				session->session_name, creation_time_str);
+		pthread_mutex_unlock(&session->lock);
+	} else {
+		pthread_mutex_lock(&session->lock);
+		ret = asprintf(&session_directory, "%s/%s", session->hostname,
+				session->base_path);
+		pthread_mutex_unlock(&session->lock);
 	}
-	strftime(creation_time_str, sizeof(creation_time_str), "%Y%m%d-%H%M%S",
-			timeinfo);
-
-	pthread_mutex_lock(&session->lock);
-	ret = asprintf(&session_directory, "%s/%s-%s", session->hostname,
-			session->session_name, creation_time_str);
-	pthread_mutex_unlock(&session->lock);
 	if (ret < 0) {
 		PERROR("Failed to format session directory name");
 		goto end;

src/bin/lttng-relayd/session.h
@@ -55,6 +55,7 @@ struct relay_session {
 	LTTNG_OPTIONAL(time_t) creation_time;
 	char session_name[LTTNG_NAME_MAX];
 	char hostname[LTTNG_HOST_NAME_MAX];
+	char base_path[LTTNG_PATH_MAX];
 	uint32_t live_timer;
 
 	/* Session in snapshot mode. */
@@ -125,7 +126,7 @@ struct relay_session {
 };
 
 struct relay_session *session_create(const char *session_name,
-		const char *hostname,
+		const char *hostname, const char *base_path,
 		uint32_t live_timer,
 		bool snapshot,
 		const lttng_uuid sessiond_uuid,