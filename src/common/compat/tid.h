96415c0a3036b2764fcfb7e31e62246d9abd137f
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2019-09-05 15:08:59 +0200

    Fix: namespace our gettid wrapper
    Since glibc 2.30, a gettid wrapper was added that conflicts with the
    static declaration. Namespace the wrapper so there is no conflict.
    
    Fixes:
     - http://autobuild.buildroot.org/results/8680c5a355b226cf978397615cbe5df1c5f8c656
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>

--- src/common/compat/tid.h
+++ src/common/compat/tid.h
@@ -29,11 +29,9 @@
 #include <syscall.h>
 #endif
 
-#if defined(_syscall0)
-_syscall0(pid_t, gettid)
-#elif defined(__NR_gettid)
+#if defined(__NR_gettid)
 #include <unistd.h>
-static inline pid_t gettid(void)
+static inline pid_t lttng_gettid(void)
 {
 	return syscall(__NR_gettid);
 }
@@ -42,7 +40,7 @@ static inline pid_t gettid(void)
 #include <unistd.h>
 
 /* Fall-back on getpid for tid if not available. */
-static inline pid_t gettid(void)
+static inline pid_t lttng_gettid(void)
 {
 	return getpid();
 }
