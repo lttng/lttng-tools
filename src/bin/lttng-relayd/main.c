compudj/lttng-tools-dev

relayd: Implement get_configuration relayd protocol command
Introduce a "GET_CONFIGURATION" command to the relayd protocol.
This allows checking whether the relay daemon allows the clear
command or not. A 64-bit set of flags are returned, and it can be
extended in the future with a variable length payload.

Signed-off-by: Mathieu Desnoyers <mathieu.desnoyers@efficios.com>

src/bin/lttng-relayd/main.c
@@ -2865,6 +2865,57 @@ end_no_reply:
 	return ret;
 }
 
+/*
+ * relay_get_configuration: query whether feature is available
+ */
+static int relay_get_configuration(const struct lttcomm_relayd_hdr *recv_hdr,
+		struct relay_connection *conn,
+		const struct lttng_buffer_view *payload)
+{
+	int ret = 0;
+	ssize_t send_ret;
+	struct lttcomm_relayd_get_configuration *msg;
+	struct lttcomm_relayd_get_configuration_reply reply = {};
+	struct lttng_buffer_view header_view;
+	uint64_t query_flags = 0;
+	uint64_t result_flags = 0;
+
+	header_view = lttng_buffer_view_from_view(payload, 0, sizeof(*msg));
+	if (!header_view.data) {
+		ERR("Failed to receive payload of chunk close command");
+		ret = -1;
+		goto end_no_reply;
+	}
+
+	/* Convert to host endianness. */
+	msg = (typeof(msg)) header_view.data;
+	query_flags = be32toh(msg->query_flags);
+
+	if (query_flags) {
+		ret = LTTNG_ERR_INVALID_PROTOCOL;
+		goto reply;
+	}
+	if (opt_allow_clear) {
+		result_flags |= LTTCOMM_RELAYD_CONFIGURATION_FLAG_CLEAR_ALLOWED;
+	}
+	ret = 0;
+reply:
+	reply = (typeof(reply)){
+		.generic.ret_code = htobe32((uint32_t)
+			(ret == 0 ? LTTNG_OK : LTTNG_ERR_INVALID_PROTOCOL)),
+		.relayd_configuration_flags = htobe64(result_flags),
+	};
+	send_ret = conn->sock->ops->sendmsg(
+			conn->sock, &reply, sizeof(reply), 0);
+	if (send_ret < (ssize_t) sizeof(reply)) {
+		ERR("Failed to send \"get configuration\" command reply (ret = %zd)",
+				send_ret);
+		ret = -1;
+	}
+end_no_reply:
+	return ret;
+}
+
 #define DBG_CMD(cmd_name, conn) \
 		DBG3("Processing \"%s\" command for socket %i", cmd_name, conn->sock->fd);
 
@@ -2942,6 +2993,10 @@ static int relay_process_control_command(struct relay_connection *conn,
 	case RELAYD_TRACE_CHUNK_EXISTS:
 		DBG_CMD("RELAYD_TRACE_CHUNK_EXISTS", conn);
 		ret = relay_trace_chunk_exists(header, conn, payload);
+		break;
+	case RELAYD_GET_CONFIGURATION:
+		DBG_CMD("RELAYD_GET_CONFIGURATION", conn);
+		ret = relay_get_configuration(header, conn, payload);
 		break;
 	case RELAYD_UPDATE_SYNC_INFO:
 	default:

src/common/relayd/relayd.c
@@ -45,6 +45,17 @@ bool relayd_supports_chunks(const struct lttcomm_relayd_sock *sock)
 	return false;
 }
 
+static
+bool relayd_supports_get_configuration(const struct lttcomm_relayd_sock *sock)
+{
+	if (sock->major > 2) {
+		return true;
+	} else if (sock->major == 2 && sock->minor >= 12) {
+		return true;
+	}
+	return false;
+}
+
 /*
  * Send command. Fill up the header and append the data.
  */
@@ -1529,6 +1540,59 @@ int relayd_trace_chunk_exists(struct lttcomm_relayd_sock *sock,
 				", exists = %s", chunk_id,
 				reply.trace_chunk_exists ? "true" : "false");
 		*chunk_exists = !!reply.trace_chunk_exists;
+	}
+end:
+	return ret;
+}
+
+int relayd_get_configuration(struct lttcomm_relayd_sock *sock,
+		uint64_t query_flags,
+		uint64_t *result_flags)
+{
+	int ret = 0;
+	struct lttcomm_relayd_get_configuration msg = {};
+	struct lttcomm_relayd_get_configuration_reply reply = {};
+
+	if (!relayd_supports_get_configuration(sock)) {
+		DBG("Refusing to get relayd configuration (unsupported by relayd)");
+		if (query_flags) {
+			ret = -1;
+			goto end;
+		}
+		*result_flags = 0;
+		goto end;
+	}
+
+	msg = (typeof(msg)){
+			.query_flags = htobe64(query_flags),
+	};
+
+	ret = send_command(sock, RELAYD_GET_CONFIGURATION, &msg, sizeof(msg),
+			0);
+	if (ret < 0) {
+		ERR("Failed to send get configuration command to relay daemon");
+		goto end;
+	}
+
+	ret = recv_reply(sock, &reply, sizeof(reply));
+	if (ret < 0) {
+		ERR("Failed to receive relay daemon get configuration command reply");
+		goto end;
+	}
+
+	reply.generic.ret_code = be32toh(reply.generic.ret_code);
+	if (reply.generic.ret_code != LTTNG_OK) {
+		ret = -1;
+		ERR("Relayd get configuration replied error %d",
+				reply.generic.ret_code);
+	} else {
+		reply.relayd_configuration_flags =
+			be64toh(reply.relayd_configuration_flags);
+		ret = 0;
+		DBG("Relayd successfully get configuration : query_flags = %" PRIu64
+				", results_flags = %" PRIu64, query_flags,
+				reply.relayd_configuration_flags);
+		*result_flags = reply.relayd_configuration_flags;
 	}
 end:
 	return ret;

lttng/lttng-tools

Change-Id: I33a03d09871f1c0c248d5514451c533ffd773979
Signed-off-by: Jérémie Galarneau <jeremie.galarneau@efficios.com>

src/bin/lttng-relayd/main.c
@@ -2866,6 +2866,57 @@ end_no_reply:
 	return ret;
 }
 
+/*
+ * relay_get_configuration: query whether feature is available
+ */
+static int relay_get_configuration(const struct lttcomm_relayd_hdr *recv_hdr,
+		struct relay_connection *conn,
+		const struct lttng_buffer_view *payload)
+{
+	int ret = 0;
+	ssize_t send_ret;
+	struct lttcomm_relayd_get_configuration *msg;
+	struct lttcomm_relayd_get_configuration_reply reply = {};
+	struct lttng_buffer_view header_view;
+	uint64_t query_flags = 0;
+	uint64_t result_flags = 0;
+
+	header_view = lttng_buffer_view_from_view(payload, 0, sizeof(*msg));
+	if (!header_view.data) {
+		ERR("Failed to receive payload of chunk close command");
+		ret = -1;
+		goto end_no_reply;
+	}
+
+	/* Convert to host endianness. */
+	msg = (typeof(msg)) header_view.data;
+	query_flags = be64toh(msg->query_flags);
+
+	if (query_flags) {
+		ret = LTTNG_ERR_INVALID_PROTOCOL;
+		goto reply;
+	}
+	if (opt_allow_clear) {
+		result_flags |= LTTCOMM_RELAYD_CONFIGURATION_FLAG_CLEAR_ALLOWED;
+	}
+	ret = 0;
+reply:
+	reply = (typeof(reply)){
+		.generic.ret_code = htobe32((uint32_t)
+			(ret == 0 ? LTTNG_OK : LTTNG_ERR_INVALID_PROTOCOL)),
+		.relayd_configuration_flags = htobe64(result_flags),
+	};
+	send_ret = conn->sock->ops->sendmsg(
+			conn->sock, &reply, sizeof(reply), 0);
+	if (send_ret < (ssize_t) sizeof(reply)) {
+		ERR("Failed to send \"get configuration\" command reply (ret = %zd)",
+				send_ret);
+		ret = -1;
+	}
+end_no_reply:
+	return ret;
+}
+
 #define DBG_CMD(cmd_name, conn) \
 		DBG3("Processing \"%s\" command for socket %i", cmd_name, conn->sock->fd);
 
@@ -2943,6 +2994,10 @@ static int relay_process_control_command(struct relay_connection *conn,
 	case RELAYD_TRACE_CHUNK_EXISTS:
 		DBG_CMD("RELAYD_TRACE_CHUNK_EXISTS", conn);
 		ret = relay_trace_chunk_exists(header, conn, payload);
+		break;
+	case RELAYD_GET_CONFIGURATION:
+		DBG_CMD("RELAYD_GET_CONFIGURATION", conn);
+		ret = relay_get_configuration(header, conn, payload);
 		break;
 	case RELAYD_UPDATE_SYNC_INFO:
 	default:

src/common/relayd/relayd.c
@@ -45,6 +45,17 @@ bool relayd_supports_chunks(const struct lttcomm_relayd_sock *sock)
 	return false;
 }
 
+static
+bool relayd_supports_get_configuration(const struct lttcomm_relayd_sock *sock)
+{
+	if (sock->major > 2) {
+		return true;
+	} else if (sock->major == 2 && sock->minor >= 12) {
+		return true;
+	}
+	return false;
+}
+
 /*
  * Send command. Fill up the header and append the data.
  */
@@ -1529,6 +1540,57 @@ int relayd_trace_chunk_exists(struct lttcomm_relayd_sock *sock,
 				", exists = %s", chunk_id,
 				reply.trace_chunk_exists ? "true" : "false");
 		*chunk_exists = !!reply.trace_chunk_exists;
+	}
+end:
+	return ret;
+}
+
+int relayd_get_configuration(struct lttcomm_relayd_sock *sock,
+		uint64_t query_flags,
+		uint64_t *result_flags)
+{
+	int ret = 0;
+	struct lttcomm_relayd_get_configuration msg = (typeof(msg)) {
+		.query_flags = htobe64(query_flags),
+	};
+	struct lttcomm_relayd_get_configuration_reply reply = {};
+
+	if (!relayd_supports_get_configuration(sock)) {
+		DBG("Refusing to get relayd configuration (unsupported by relayd)");
+		if (query_flags) {
+			ret = -1;
+			goto end;
+		}
+		*result_flags = 0;
+		goto end;
+	}
+
+	ret = send_command(sock, RELAYD_GET_CONFIGURATION, &msg, sizeof(msg),
+			0);
+	if (ret < 0) {
+		ERR("Failed to send get configuration command to relay daemon");
+		goto end;
+	}
+
+	ret = recv_reply(sock, &reply, sizeof(reply));
+	if (ret < 0) {
+		ERR("Failed to receive relay daemon get configuration command reply");
+		goto end;
+	}
+
+	reply.generic.ret_code = be32toh(reply.generic.ret_code);
+	if (reply.generic.ret_code != LTTNG_OK) {
+		ret = -1;
+		ERR("Relayd get configuration replied error %d",
+				reply.generic.ret_code);
+	} else {
+		reply.relayd_configuration_flags =
+			be64toh(reply.relayd_configuration_flags);
+		ret = 0;
+		DBG("Relayd successfully got configuration: query_flags = %" PRIu64
+				", results_flags = %" PRIu64, query_flags,
+				reply.relayd_configuration_flags);
+		*result_flags = reply.relayd_configuration_flags;
 	}
 end:
 	return ret;