compudj/lttng-tools-dev

sessiond: query relayd configuration for availability of clear feature
Query the relayd configuration when connecting to check for availability
of the clear feature.

Signed-off-by: Mathieu Desnoyers <mathieu.desnoyers@efficios.com>

src/bin/lttng-sessiond/cmd.c
@@ -998,6 +998,8 @@ static enum lttng_error_code create_connect_relayd(struct lttng_uri *uri,
 
 	/* Create socket for control stream. */
 	if (uri->stype == LTTNG_STREAM_CONTROL) {
+		uint64_t result_flags;
+
 		DBG3("Creating relayd stream socket from URI");
 
 		/* Check relayd version */
@@ -1012,6 +1014,16 @@ static enum lttng_error_code create_connect_relayd(struct lttng_uri *uri,
 		}
 		consumer->relay_major_version = rsock->major;
 		consumer->relay_minor_version = rsock->minor;
+		ret = relayd_get_configuration(rsock, 0,
+				&result_flags);
+		if (ret < 0) {
+			ERR("Unable to get relayd configuration");
+			status = LTTNG_ERR_RELAYD_CONNECT_FAIL;
+			goto close_sock;
+		}
+		if (result_flags & LTTCOMM_RELAYD_CONFIGURATION_FLAG_CLEAR_ALLOWED) {
+			consumer->relay_clear_feature = true;
+		}
 	} else if (uri->stype == LTTNG_STREAM_DATA) {
 		DBG3("Creating relayd data socket from URI");
 	} else {
@@ -1225,6 +1237,8 @@ int cmd_setup_relayd(struct ltt_session *session)
 			usess->consumer->relay_major_version;
 		session->consumer->relay_minor_version =
 			usess->consumer->relay_minor_version;
+		session->consumer->relay_clear_feature =
+			usess->consumer->relay_clear_feature;
 	}
 
 	if (ksess && ksess->consumer && ksess->consumer->type == CONSUMER_DST_NET
@@ -1251,6 +1265,8 @@ int cmd_setup_relayd(struct ltt_session *session)
 			ksess->consumer->relay_major_version;
 		session->consumer->relay_minor_version =
 			ksess->consumer->relay_minor_version;
+		session->consumer->relay_clear_feature =
+			ksess->consumer->relay_clear_feature;
 	}
 
 error:

src/bin/lttng-sessiond/consumer.c
@@ -626,6 +626,7 @@ struct consumer_output *consumer_copy_output(struct consumer_output *src)
 	output->snapshot = src->snapshot;
 	output->relay_major_version = src->relay_major_version;
 	output->relay_minor_version = src->relay_minor_version;
+	output->relay_clear_feature = src->relay_clear_feature;
 	memcpy(&output->dst, &src->dst, sizeof(output->dst));
 	ret = consumer_copy_sockets(output, src);
 	if (ret < 0) {

lttng/lttng-tools

Change-Id: I5827fdc7f92bc9bc87ccf9d1c567057b2c227e2b
Signed-off-by: Jérémie Galarneau <jeremie.galarneau@efficios.com>

src/bin/lttng-sessiond/cmd.c
@@ -998,6 +998,8 @@ static enum lttng_error_code create_connect_relayd(struct lttng_uri *uri,
 
 	/* Create socket for control stream. */
 	if (uri->stype == LTTNG_STREAM_CONTROL) {
+		uint64_t result_flags;
+
 		DBG3("Creating relayd stream socket from URI");
 
 		/* Check relayd version */
@@ -1012,6 +1014,16 @@ static enum lttng_error_code create_connect_relayd(struct lttng_uri *uri,
 		}
 		consumer->relay_major_version = rsock->major;
 		consumer->relay_minor_version = rsock->minor;
+		ret = relayd_get_configuration(rsock, 0,
+				&result_flags);
+		if (ret < 0) {
+			ERR("Unable to get relayd configuration");
+			status = LTTNG_ERR_RELAYD_CONNECT_FAIL;
+			goto close_sock;
+		}
+		if (result_flags & LTTCOMM_RELAYD_CONFIGURATION_FLAG_CLEAR_ALLOWED) {
+			consumer->relay_allows_clear = true;
+		}
 	} else if (uri->stype == LTTNG_STREAM_DATA) {
 		DBG3("Creating relayd data socket from URI");
 	} else {
@@ -1224,6 +1236,8 @@ int cmd_setup_relayd(struct ltt_session *session)
 			usess->consumer->relay_major_version;
 		session->consumer->relay_minor_version =
 			usess->consumer->relay_minor_version;
+		session->consumer->relay_allows_clear =
+			usess->consumer->relay_allows_clear;
 	}
 
 	if (ksess && ksess->consumer && ksess->consumer->type == CONSUMER_DST_NET
@@ -1250,6 +1264,8 @@ int cmd_setup_relayd(struct ltt_session *session)
 			ksess->consumer->relay_major_version;
 		session->consumer->relay_minor_version =
 			ksess->consumer->relay_minor_version;
+		session->consumer->relay_allows_clear =
+			ksess->consumer->relay_allows_clear;
 	}
 
 error:

src/bin/lttng-sessiond/consumer.c
@@ -626,6 +626,7 @@ struct consumer_output *consumer_copy_output(struct consumer_output *src)
 	output->snapshot = src->snapshot;
 	output->relay_major_version = src->relay_major_version;
 	output->relay_minor_version = src->relay_minor_version;
+	output->relay_allows_clear = src->relay_allows_clear;
 	memcpy(&output->dst, &src->dst, sizeof(output->dst));
 	ret = consumer_copy_sockets(output, src);
 	if (ret < 0) {