2c5426c3d8070813f8eead7719aa7387ba6e188e
Michael Jeanson <mjeanson@efficios.com>
2020-01-30 18:33:57 -0500

    fix: add include guards to compat/path.h
    Signed-off-by: Michael Jeanson <mjeanson@efficios.com>
    Change-Id: Ifb0672dcaf9b98715742546d71e54c5f4cd8dff6
    Signed-off-by: Jérémie Galarneau <jeremie.galarneau@efficios.com>

--- src/common/compat/path.h
+++ src/common/compat/path.h
@@ -5,9 +5,14 @@
  *
  */
 
+#ifndef _COMPAT_PATH_H
+#define _COMPAT_PATH_H
+
 /* Build platform's preferred path separator. */
 #if defined(_WIN32) || defined(__CYGWIN__)
 #define LTTNG_PATH_SEPARATOR '\\'
 #else
 #define LTTNG_PATH_SEPARATOR '/'
 #endif
+
+#endif /* _COMPAT_PATH_H */
