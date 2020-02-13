d23253aed60c9aaea2adcc689a7fbefc22b43e7b
Francis Deslauriers <francis.deslauriers@efficios.com>
2020-02-13 15:44:02 -0500

    Tests: gen-syscall-events: generate 2 events of each type for filtering
    Signed-off-by: Francis Deslauriers <francis.deslauriers@efficios.com>
    Signed-off-by: Jérémie Galarneau <jeremie.galarneau@efficios.com>
    Change-Id: I4803fb2e465d1865f0130b7546ee871b6752a226
    Depends-on: lttng-ust: I5a800fc92e588c2a6a0e26282b0ad5f31c044479

--- tests/utils/testapp/gen-syscall-events/gen-syscall-events.c
+++ tests/utils/testapp/gen-syscall-events/gen-syscall-events.c
@@ -9,10 +9,76 @@
 #include <stdio.h>
 #include <sys/syscall.h>
 #include <unistd.h>
+#include <common/error.h>
+#include <common/align.h>
 
 #include "utils.h"
 
 #define MAX_LEN 16
+
+/*
+ * The LTTng system call tracing facilities can't handle page faults at the
+ * moment. If a fault would occur while reading a syscall argument, the
+ * tracer will report an empty string (""). Since the proper execution of the
+ * tests which use this generator depends on some syscall string arguments being
+ * present, this util allows us to mitigate the page-fault risk.
+ *
+ * This isn't a proper fix; it is simply the best we can do for now.
+ * See bug #1261 for more context.
+ */
+static
+void prefault_string(const char *p)
+{
+	const char * const end = p + strlen(p) + 1;
+
+	while (p < end) {
+		/*
+		 * Trigger a read attempt on *p, faulting-in the pages
+		 * for reading.
+		 */
+		asm volatile("" : : "m"(*p));
+		p += PAGE_SIZE;
+	}
+}
+
+static
+int open_read_close(const char *path)
+{
+	int fd, ret;
+	char buf[MAX_LEN];
+
+	/*
+	 * Start generating syscalls. We use syscall(2) to prevent libc from
+	 * changing the underlying syscall (e.g. calling openat(2) instead of
+	 * open(2)).
+	 */
+	prefault_string(path);
+	fd = syscall(SYS_openat, AT_FDCWD, path, O_RDONLY);
+	if (fd < 0) {
+		PERROR_NO_LOGGER("Failed to open file with openat(): path = '%s'", path);
+		ret = -1;
+		goto error;
+	}
+
+	ret = syscall(SYS_read, fd, buf, MAX_LEN);
+	if (ret < 0) {
+		PERROR_NO_LOGGER("Failed to read file: path = '%s', fd = %d, length = %d",
+				path, fd, MAX_LEN);
+		ret = -1;
+		goto error;
+	}
+
+	ret = syscall(SYS_close, fd);
+	if (ret == -1) {
+		PERROR_NO_LOGGER("Failed to close file: path = '%s', fd = %d", path, fd);
+		ret = -1;
+		goto error;
+	}
+
+error:
+	return ret;
+}
+
 /*
  * The process waits for the creation of a file passed as argument from an
  * external processes to execute a syscall and exiting. This is useful for tests
@@ -21,8 +87,7 @@
  */
 int main(int argc, char **argv)
 {
-	int fd, ret;
-	char buf[MAX_LEN];
+	int ret;
 	char *start_file;
 
 	if (argc != 2) {
@@ -47,23 +112,14 @@ int main(int argc, char **argv)
 	 * Start generating syscalls. We use syscall(2) to prevent libc to change
 	 * the underlying syscall. e.g. calling openat(2) instead of open(2).
 	 */
-	fd = syscall(SYS_openat, AT_FDCWD, "/proc/cpuinfo", O_RDONLY);
-	if (fd < 0) {
-		perror("open");
-		ret = -1;
-		goto error;
-	}
-
-	ret = syscall(SYS_read, fd, buf, MAX_LEN);
-	if (ret < 0) {
-		perror("read");
-		ret = -1;
-		goto error;
-	}
-
-	ret = syscall(SYS_close, fd);
+	ret = open_read_close("/proc/cpuinfo");
 	if (ret == -1) {
-		perror("close");
+		ret = -1;
+		goto error;
+	}
+
+	ret = open_read_close("/proc/cmdline");
+	if (ret == -1) {
 		ret = -1;
 		goto error;
 	}
--- tests/utils/utils.h
+++ tests/utils/utils.h
@@ -8,6 +8,30 @@
 #ifndef TEST_UTILS_H
 #define TEST_UTILS_H
 
+#if !defined(__GLIBC__) || ((_POSIX_C_SOURCE >= 200112L || _XOPEN_SOURCE >= 600) && !defined(_GNU_SOURCE))
+
+/*
+ * Version using XSI strerror_r.
+ */
+#define PERROR_NO_LOGGER(msg, args...)                      \
+	do {                                                \
+		char buf[200];                              \
+		strerror_r(errno, buf, sizeof(buf));        \
+		fprintf(stderr, msg ": %s\n", ##args, buf); \
+	} while (0);
+#else
+/*
+ * Version using GNU strerror_r, for linux with appropriate defines.
+ */
+#define PERROR_NO_LOGGER(msg, args...)                      \
+	do {                                                \
+		char *buf;                                  \
+		char tmp[200];                              \
+		buf = strerror_r(errno, tmp, sizeof(tmp));  \
+		fprintf(stderr, msg ": %s\n", ##args, buf); \
+	} while (0);
+#endif
+
 int usleep_safe(useconds_t usec);
 int create_file(const char *path);
 int wait_on_file(const char *path);
