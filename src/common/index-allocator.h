246611b0dffa58fbc0e2329ddf6f9dc9d9eff7ce
Francis Deslauriers <francis.deslauriers@efficios.com>
2020-08-24 17:02:05 -0400

    common: Add index allocator for error counters
    Signed-off-by: Francis Deslauriers <francis.deslauriers@efficios.com>
    Signed-off-by: Jérémie Galarneau <jeremie.galarneau@efficios.com>
    Change-Id: I677a8f5b774315eadb8e3b2c2f2a5b1aae02133f

---
+++ src/common/index-allocator.h
@@ -0,0 +1,52 @@
+/*
+ * Copyright (C) 2020 Francis Deslauriers <francis.deslauriers@efficios.com>
+ *
+ * SPDX-License-Identifier: GPL-2.0-only
+ *
+ */
+
+#ifndef _COMMON_INDEX_ALLOCATOR_H
+#define _COMMON_INDEX_ALLOCATOR_H
+
+#include <inttypes.h>
+
+struct lttng_index_allocator;
+
+enum lttng_index_allocator_status {
+	LTTNG_INDEX_ALLOCATOR_STATUS_OK,
+	LTTNG_INDEX_ALLOCATOR_STATUS_EMPTY,
+	LTTNG_INDEX_ALLOCATOR_STATUS_ERROR,
+};
+
+/*
+ * Create an index allocator of `index_count` slots.
+ */
+struct lttng_index_allocator *lttng_index_allocator_create(
+		uint64_t index_count);
+
+/*
+ * Get the number of indexes currently in use.
+ */
+uint64_t lttng_index_allocator_get_index_count(
+	struct lttng_index_allocator *allocator);
+
+/*
+ * Allocate (i.e. reserve) a slot.
+ */
+enum lttng_index_allocator_status lttng_index_allocator_alloc(
+		struct lttng_index_allocator *allocator,
+		uint64_t *index);
+
+/*
+ * Release a slot by index. The slot will be re-used by the index allocator
+ * in future 'alloc' calls.
+ */
+enum lttng_index_allocator_status lttng_index_allocator_release(
+		struct lttng_index_allocator *allocator, uint64_t index);
+
+/*
+ * Destroy an index allocator.
+ */
+void lttng_index_allocator_destroy(struct lttng_index_allocator *allocator);
+
+#endif /* _COMMON_INDEX_ALLOCATOR_H */
