0060607b4f1947339819316b63f00e42aab2a341
Michael Jeanson <mjeanson@efficios.com>
2020-08-26 11:39:15 -0400

    Cleanup: simplify 'poll' wrapper build
    Remove the AM conditionnal and merge the sources in single files like the
    other wrappers. This removes a special case from the build system.
    
    Signed-off-by: Michael Jeanson <mjeanson@efficios.com>
    Signed-off-by: Jérémie Galarneau <jeremie.galarneau@efficios.com>
    Change-Id: I6078f6013e52c3bc7c74cb8937f3741453c65874

--- src/common/compat/poll.c
+++ src/common/compat/poll.c
@@ -9,8 +9,6 @@
 #define _LGPL_SOURCE
 #include <assert.h>
 #include <stdlib.h>
-#include <sys/resource.h>
-#include <sys/time.h>
 #include <stdbool.h>
 
 #include <common/defaults.h>
@@ -20,6 +18,343 @@
 
 #include "poll.h"
 
+#if HAVE_EPOLL
+
+#include <fcntl.h>
+#include <limits.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
+/*
+ * Maximum number of fd we can monitor.
+ *
+ * For epoll(7), /proc/sys/fs/epoll/max_user_watches (since Linux 2.6.28) will
+ * be used for the maximum size of the poll set. If this interface is not
+ * available, according to the manpage, the max_user_watches value is 1/25 (4%)
+ * of the available low memory divided by the registration cost in bytes which
+ * is 90 bytes on a 32-bit kernel and 160 bytes on a 64-bit kernel.
+ *
+ */
+static unsigned int poll_max_size;
+
+/*
+ * Resize the epoll events structure of the new size.
+ *
+ * Return 0 on success or else -1 with the current events pointer untouched.
+ */
+static int resize_poll_event(struct lttng_poll_event *events,
+		uint32_t new_size)
+{
+	struct epoll_event *ptr;
+
+	assert(events);
+
+	ptr = realloc(events->events, new_size * sizeof(*ptr));
+	if (ptr == NULL) {
+		PERROR("realloc epoll add");
+		goto error;
+	}
+	if (new_size > events->alloc_size) {
+		/* Zero newly allocated memory */
+		memset(ptr + events->alloc_size, 0,
+			(new_size - events->alloc_size) * sizeof(*ptr));
+	}
+	events->events = ptr;
+	events->alloc_size = new_size;
+
+	return 0;
+
+error:
+	return -1;
+}
+
+/*
+ * Create epoll set and allocate returned events structure.
+ */
+LTTNG_HIDDEN
+int compat_epoll_create(struct lttng_poll_event *events, int size, int flags)
+{
+	int ret;
+
+	if (events == NULL || size <= 0) {
+		goto error;
+	}
+
+	if (!poll_max_size) {
+		if (lttng_poll_set_max_size()) {
+			goto error;
+		}
+	}
+
+	/* Don't bust the limit here */
+	if (size > poll_max_size) {
+		size = poll_max_size;
+	}
+
+	ret = compat_glibc_epoll_create(size, flags);
+	if (ret < 0) {
+		/* At this point, every error is fatal */
+		PERROR("epoll_create1");
+		goto error;
+	}
+
+	events->epfd = ret;
+
+	/* This *must* be freed by using lttng_poll_free() */
+	events->events = zmalloc(size * sizeof(struct epoll_event));
+	if (events->events == NULL) {
+		PERROR("zmalloc epoll set");
+		goto error_close;
+	}
+
+	events->alloc_size = events->init_size = size;
+	events->nb_fd = 0;
+
+	return 0;
+
+error_close:
+	ret = close(events->epfd);
+	if (ret) {
+		PERROR("close");
+	}
+error:
+	return -1;
+}
+
+/*
+ * Add a fd to the epoll set with requesting events.
+ */
+LTTNG_HIDDEN
+int compat_epoll_add(struct lttng_poll_event *events, int fd, uint32_t req_events)
+{
+	int ret;
+	struct epoll_event ev;
+
+	if (events == NULL || events->events == NULL || fd < 0) {
+		ERR("Bad compat epoll add arguments");
+		goto error;
+	}
+
+	/*
+	 * Zero struct epoll_event to ensure all representations of its
+	 * union are zeroed.
+	 */
+	memset(&ev, 0, sizeof(ev));
+	ev.events = req_events;
+	ev.data.fd = fd;
+
+	ret = epoll_ctl(events->epfd, EPOLL_CTL_ADD, fd, &ev);
+	if (ret < 0) {
+		switch (errno) {
+		case EEXIST:
+			/* If exist, it's OK. */
+			goto end;
+		case ENOSPC:
+		case EPERM:
+			/* Print PERROR and goto end not failing. Show must go on. */
+			PERROR("epoll_ctl ADD");
+			goto end;
+		default:
+			PERROR("epoll_ctl ADD fatal");
+			goto error;
+		}
+	}
+
+	events->nb_fd++;
+
+end:
+	return 0;
+
+error:
+	return -1;
+}
+
+/*
+ * Remove a fd from the epoll set.
+ */
+LTTNG_HIDDEN
+int compat_epoll_del(struct lttng_poll_event *events, int fd)
+{
+	int ret;
+
+	if (events == NULL || fd < 0 || events->nb_fd == 0) {
+		goto error;
+	}
+
+	ret = epoll_ctl(events->epfd, EPOLL_CTL_DEL, fd, NULL);
+	if (ret < 0) {
+		switch (errno) {
+		case ENOENT:
+		case EPERM:
+			/* Print PERROR and goto end not failing. Show must go on. */
+			PERROR("epoll_ctl DEL");
+			goto end;
+		default:
+			PERROR("epoll_ctl DEL fatal");
+			goto error;
+		}
+	}
+
+	events->nb_fd--;
+
+end:
+	return 0;
+
+error:
+	return -1;
+}
+
+/*
+ * Set an fd's events.
+ */
+LTTNG_HIDDEN
+int compat_epoll_mod(struct lttng_poll_event *events, int fd, uint32_t req_events)
+{
+	int ret;
+	struct epoll_event ev;
+
+	if (events == NULL || fd < 0 || events->nb_fd == 0) {
+		goto error;
+	}
+
+	/*
+	 * Zero struct epoll_event to ensure all representations of its
+	 * union are zeroed.
+	 */
+	memset(&ev, 0, sizeof(ev));
+	ev.events = req_events;
+	ev.data.fd = fd;
+
+	ret = epoll_ctl(events->epfd, EPOLL_CTL_MOD, fd, &ev);
+	if (ret < 0) {
+		switch (errno) {
+		case ENOENT:
+		case EPERM:
+			/* Print PERROR and goto end not failing. Show must go on. */
+			PERROR("epoll_ctl MOD");
+			goto end;
+		default:
+			PERROR("epoll_ctl MOD fatal");
+			goto error;
+		}
+	}
+
+end:
+	return 0;
+
+error:
+	return -1;
+}
+
+/*
+ * Wait on epoll set. This is a blocking call of timeout value.
+ */
+LTTNG_HIDDEN
+int compat_epoll_wait(struct lttng_poll_event *events, int timeout,
+		bool interruptible)
+{
+	int ret;
+	uint32_t new_size;
+
+	if (events == NULL || events->events == NULL) {
+		ERR("Wrong arguments in compat_epoll_wait");
+		goto error;
+	}
+
+	if (events->nb_fd == 0) {
+		errno = EINVAL;
+		return -1;
+	}
+
+	/*
+	 * Resize if needed before waiting. We could either expand the array or
+	 * shrink it down. It's important to note that after this step, we are
+	 * ensured that the events argument of the epoll_wait call will be large
+	 * enough to hold every possible returned events.
+	 */
+	new_size = 1U << utils_get_count_order_u32(events->nb_fd);
+	if (new_size != events->alloc_size && new_size >= events->init_size) {
+		ret = resize_poll_event(events, new_size);
+		if (ret < 0) {
+			/* ENOMEM problem at this point. */
+			goto error;
+		}
+	}
+
+	do {
+		ret = epoll_wait(events->epfd, events->events, events->nb_fd, timeout);
+	} while (!interruptible && ret == -1 && errno == EINTR);
+	if (ret < 0) {
+		if (errno != EINTR) {
+			PERROR("epoll_wait");
+		}
+		goto error;
+	}
+
+	/*
+	 * Since the returned events are set sequentially in the "events" structure
+	 * we only need to return the epoll_wait value and iterate over it.
+	 */
+	return ret;
+
+error:
+	return -1;
+}
+
+/*
+ * Setup poll set maximum size.
+ */
+LTTNG_HIDDEN
+int compat_epoll_set_max_size(void)
+{
+	int ret, fd, retval = 0;
+	ssize_t size_ret;
+	char buf[64];
+
+	fd = open(COMPAT_EPOLL_PROC_PATH, O_RDONLY);
+	if (fd < 0) {
+		/*
+		 * Failing on opening [1] is not an error per see. [1] was
+		 * introduced in Linux 2.6.28 but epoll is available since
+		 * 2.5.44. Hence, goto end and set a default value without
+		 * setting an error return value.
+		 *
+		 * [1] /proc/sys/fs/epoll/max_user_watches
+		 */
+		retval = 0;
+		goto end;
+	}
+
+	size_ret = lttng_read(fd, buf, sizeof(buf));
+	/*
+	 * Allow reading a file smaller than buf, but keep space for
+	 * final \0.
+	 */
+	if (size_ret < 0 || size_ret >= sizeof(buf)) {
+		PERROR("read set max size");
+		retval = -1;
+		goto end_read;
+	}
+	buf[size_ret] = '\0';
+	poll_max_size = atoi(buf);
+end_read:
+	ret = close(fd);
+	if (ret) {
+		PERROR("close");
+	}
+end:
+	if (!poll_max_size) {
+		poll_max_size = DEFAULT_POLL_SIZE;
+	}
+	DBG("epoll set max size is %d", poll_max_size);
+	return retval;
+}
+
+#else /* HAVE_EPOLL */
+
+#include <sys/resource.h>
+#include <sys/time.h>
 
 /*
  * Maximum number of fd we can monitor.
@@ -392,3 +727,5 @@ end:
 	DBG("poll set max size set to %u", poll_max_size);
 	return retval;
 }
+
+#endif /* !HAVE_EPOLL */
--- src/common/fd-tracker/utils-poll.c
+++ src/common/fd-tracker/utils-poll.c
@@ -9,6 +9,66 @@
 
 #include "utils.h"
 
+#if HAVE_EPOLL
+
+struct create_args {
+	struct lttng_poll_event *events;
+	int size;
+	int flags;
+};
+
+static int open_epoll(void *data, int *out_fd)
+{
+	int ret;
+	struct create_args *args = data;
+
+	ret = lttng_poll_create(args->events, args->size, args->flags);
+	if (ret < 0) {
+		goto end;
+	}
+
+	*out_fd = args->events->epfd;
+end:
+	return ret;
+}
+
+static int close_epoll(void *data, int *in_fd)
+{
+	/* Will close the epfd. */
+	lttng_poll_clean((struct lttng_poll_event *) data);
+	return 0;
+}
+
+/*
+ * The epoll variant of the poll compat layer creates an unsuspendable fd which
+ * must be tracked.
+ */
+int fd_tracker_util_poll_create(struct fd_tracker *tracker,
+		const char *name,
+		struct lttng_poll_event *events,
+		int size,
+		int flags)
+{
+	int out_fd;
+	struct create_args create_args = {
+		.events = events,
+		.size = size,
+		.flags = flags,
+	};
+
+	return fd_tracker_open_unsuspendable_fd(
+			tracker, &out_fd, &name, 1, open_epoll, &create_args);
+}
+
+int fd_tracker_util_poll_clean(
+		struct fd_tracker *tracker, struct lttng_poll_event *events)
+{
+	return fd_tracker_close_unsuspendable_fd(
+			tracker, &events->epfd, 1, close_epoll, events);
+}
+
+#else /* HAVE_EPOLL */
+
 /*
  * The epoll variant of the poll compat layer creates an unsuspendable fd which
  * must be tracked.
@@ -28,3 +88,5 @@ int fd_tracker_util_poll_clean(
 	lttng_poll_clean(events);
 	return 0;
 }
+
+#endif /* !HAVE_EPOLL */
