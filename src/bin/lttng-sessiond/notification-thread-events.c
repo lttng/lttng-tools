lttng/lttng-tools

commit bc8daafb693d284dbebb8311b125d38a3b8d7ef7

Fix: sessiond: uninitialized credentials set for unbound trigger
The bound-object credentials should be left unset when the trigger
doesn't have to bind to an object.

Signed-off-by: Jérémie Galarneau <jeremie.galarneau@efficios.com>
Change-Id: I38f75b7c62ed796ff72d4b0692bcb51b361aa81b

src/bin/lttng-sessiond/notification-thread-events.c
@@ -2611,10 +2611,14 @@ int handle_notification_thread_command_register_trigger(
 				&evaluation, &object_uid,
 				&object_gid);
 		break;
+		LTTNG_OPTIONAL_SET(&object_creds.uid, object_uid);
+		LTTNG_OPTIONAL_SET(&object_creds.gid, object_gid);
 	case LTTNG_OBJECT_TYPE_CHANNEL:
 		ret = evaluate_channel_condition_for_client(condition, state,
 				&evaluation, &object_uid,
 				&object_gid);
+		LTTNG_OPTIONAL_SET(&object_creds.uid, object_uid);
+		LTTNG_OPTIONAL_SET(&object_creds.gid, object_gid);
 		break;
 	case LTTNG_OBJECT_TYPE_NONE:
 		ret = 0;
@@ -2629,9 +2633,6 @@ int handle_notification_thread_command_register_trigger(
 		/* Fatal error. */
 		goto error_put_client_list;
 	}
-
-	LTTNG_OPTIONAL_SET(&object_creds.uid, object_uid);
-	LTTNG_OPTIONAL_SET(&object_creds.gid, object_gid);
 
 	DBG("Newly registered trigger's condition evaluated to %s",
 			evaluation ? "true" : "false");

commit b42ada9013db5428c0fbca358e87c001f5b7e8c4

Fix: sessiond: notification: unreachable code in switch-case
A copy paste error introduced in bc8daafb causes the initialization
of the session credentials to not be initialized.

Reported-by: Francis Deslauriers <francis.deslauriers@efficios.com>
Signed-off-by: Jérémie Galarneau <jeremie.galarneau@efficios.com>
Change-Id: I50af722a498892bec764e115cfa6e9ca64302e05

src/bin/lttng-sessiond/notification-thread-events.c
@@ -2610,9 +2610,9 @@ int handle_notification_thread_command_register_trigger(
 		ret = evaluate_session_condition_for_client(condition, state,
 				&evaluation, &object_uid,
 				&object_gid);
-		break;
 		LTTNG_OPTIONAL_SET(&object_creds.uid, object_uid);
 		LTTNG_OPTIONAL_SET(&object_creds.gid, object_gid);
+		break;
 	case LTTNG_OBJECT_TYPE_CHANNEL:
 		ret = evaluate_channel_condition_for_client(condition, state,
 				&evaluation, &object_uid,