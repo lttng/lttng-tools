1bfbabab0171e4243edbf1d7e9e9b6c11f549460
Mathieu Desnoyers <mathieu.desnoyers@efficios.com>
2021-04-30 10:44:44 -0400

    Fix: kernel consumer: signal metadata ready condition variable
    The behaviour of the kernel consumer does not match that of the user
    space consumer. When a live metadata stream goes back to "sleep", it
    must broadcast on the metadata_rdv to wake-up any thread that is
    performing a metadata sync.
    
    This mismatch causes a hang in the kernel consumer during the kernel
    clear tests.
    
    Signed-off-by: Mathieu Desnoyers <mathieu.desnoyers@efficios.com>
    Signed-off-by: Jérémie Galarneau <jeremie.galarneau@efficios.com>
    Change-Id: I7c93007b6d08f340881c08f23931582443bfa397

--- src/common/kernel-consumer/kernel-consumer.c
+++ src/common/kernel-consumer/kernel-consumer.c
@@ -1623,6 +1623,14 @@ bool is_get_next_check_metadata_available(int tracer_fd)
 }
 
 static
+int signal_metadata(struct lttng_consumer_stream *stream,
+		struct lttng_consumer_local_data *ctx)
+{
+	ASSERT_LOCKED(stream->metadata_rdv_lock);
+	return pthread_cond_broadcast(&stream->metadata_rdv) ? -errno : 0;
+}
+
+static
 int lttng_kconsumer_set_stream_ops(
 		struct lttng_consumer_stream *stream)
 {
@@ -1652,6 +1660,8 @@ int lttng_kconsumer_set_stream_ops(
 			metadata_bucket_destroy(stream->metadata_bucket);
 			stream->metadata_bucket = NULL;
 		}
+
+		stream->read_subbuffer_ops.on_sleep = signal_metadata;
 	}
 
 	if (!stream->read_subbuffer_ops.get_next_subbuffer) {
