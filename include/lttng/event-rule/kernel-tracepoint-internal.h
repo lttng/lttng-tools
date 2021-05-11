b25b2570985bc32363deef34c015e1d861e523dc
Jonathan Rajotte <jonathan.rajotte-julien@efficios.com>
2021-05-11 19:52:37 -0400

    Introduce lttng_event_rule_kernel_tracepoint
    A following patch will remove lttng_event_rule_tracepoint.
    
    Signed-off-by: Jonathan Rajotte <jonathan.rajotte-julien@efficios.com>
    Signed-off-by: Jérémie Galarneau <jeremie.galarneau@efficios.com>
    Change-Id: I614e45be3ef5e41fa081ad24dc1a9dcddb7eb2af

---
+++ include/lttng/event-rule/kernel-tracepoint-internal.h
@@ -0,0 +1,52 @@
+/*
+ * Copyright (C) 2019 Jonathan Rajotte <jonathan.rajotte-julien@efficios.com>
+ *
+ * SPDX-License-Identifier: LGPL-2.1-only
+ *
+ */
+
+#ifndef LTTNG_EVENT_RULE_KERNEL_TRACEPOINT_INTERNAL_H
+#define LTTNG_EVENT_RULE_KERNEL_TRACEPOINT_INTERNAL_H
+
+#include <common/payload-view.h>
+#include <common/macros.h>
+#include <common/optional.h>
+#include <lttng/event-rule/event-rule-internal.h>
+#include <lttng/event-rule/kernel-tracepoint.h>
+#include <lttng/event.h>
+
+struct lttng_event_rule_kernel_tracepoint {
+	struct lttng_event_rule parent;
+
+	/* Name pattern. */
+	char *pattern;
+
+	/* Filter. */
+	char *filter_expression;
+
+	/* internal use only. */
+	struct {
+		char *filter;
+		struct lttng_bytecode *bytecode;
+	} internal_filter;
+};
+
+struct lttng_event_rule_kernel_tracepoint_comm {
+	/* Includes terminator `\0`. */
+	uint32_t pattern_len;
+	/* Includes terminator `\0`. */
+	uint32_t filter_expression_len;
+	/*
+	 * Payload is composed of, in that order:
+	 *   - pattern (null terminated),
+	 *   - filter expression (null terminated),
+	 */
+	char payload[];
+} LTTNG_PACKED;
+
+LTTNG_HIDDEN
+ssize_t lttng_event_rule_kernel_tracepoint_create_from_payload(
+		struct lttng_payload_view *view,
+		struct lttng_event_rule **rule);
+
+#endif /* LTTNG_EVENT_RULE_KERNEL_TRACEPOINT_INTERNAL_H */
---
+++ include/lttng/event-rule/kernel-tracepoint.h
@@ -0,0 +1,84 @@
+/*
+ * Copyright (C) 2019 Jonathan Rajotte <jonathan.rajotte-julien@efficios.com>
+ *
+ * SPDX-License-Identifier: LGPL-2.1-only
+ *
+ */
+
+#ifndef LTTNG_EVENT_RULE_KERNEL_TRACEPOINT_H
+#define LTTNG_EVENT_RULE_KERNEL_TRACEPOINT_H
+
+#include <lttng/event-rule/event-rule.h>
+#include <lttng/event.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/*
+ * Create a newly allocated kernel tracepoint event rule.
+ *
+ * The default pattern is '*'.
+ *
+ * Returns a new event rule on success, NULL on failure. This event rule must be
+ * destroyed using lttng_event_rule_destroy().
+ */
+extern struct lttng_event_rule *lttng_event_rule_kernel_tracepoint_create(void);
+
+/*
+ * Set the name pattern of a kernel tracepoint event rule.
+ *
+ * Pattern can contain wildcard '*'. See man lttng-enable-event.
+ *
+ * Return LTTNG_EVENT_RULE_STATUS_OK on success, LTTNG_EVENT_RULE_STATUS_INVALID
+ * if invalid parameters are passed.
+ */
+extern enum lttng_event_rule_status lttng_event_rule_kernel_tracepoint_set_name_pattern(
+		struct lttng_event_rule *rule, const char *pattern);
+
+/*
+ * Get the name pattern of a kernel tracepoint event rule.
+ *
+ * The caller does not assume the ownership of the returned pattern. The
+ * pattern shall only only be used for the duration of the event rule's
+ * lifetime, or before a different pattern is set.
+ *
+ * Returns LTTNG_EVENT_RULE_STATUS_OK and a pointer to the event rule's pattern
+ * on success, LTTNG_EVENT_RULE_STATUS_INVALID if an invalid
+ * parameter is passed, or LTTNG_EVENT_RULE_STATUS_UNSET if a pattern
+ * was not set prior to this call.
+ */
+extern enum lttng_event_rule_status lttng_event_rule_kernel_tracepoint_get_name_pattern(
+		const struct lttng_event_rule *rule, const char **pattern);
+
+/*
+ * Set the filter expression of a kernel tracepoint event rule.
+ *
+ * The expression is copied internally.
+ *
+ * Return LTTNG_EVENT_RULE_STATUS_OK on success, LTTNG_EVENT_RULE_STATUS_INVALID
+ * if invalid parameters are passed.
+ */
+extern enum lttng_event_rule_status lttng_event_rule_kernel_tracepoint_set_filter(
+		struct lttng_event_rule *rule, const char *expression);
+
+/*
+ * Get the filter expression of a kernel tracepoint event rule.
+ *
+ * The caller does not assume the ownership of the returned filter expression.
+ * The filter expression shall only only be used for the duration of the event
+ * rule's lifetime, or before a different filter expression is set.
+ *
+ * Returns LTTNG_EVENT_RULE_STATUS_OK and a pointer to the event rule's filter
+ * expression on success, LTTNG_EVENT_RULE_STATUS_INVALID if an invalid
+ * parameter is passed, or LTTNG_EVENT_RULE_STATUS_UNSET if a filter expression
+ * was not set prior to this call.
+ */
+extern enum lttng_event_rule_status lttng_event_rule_kernel_tracepoint_get_filter(
+		const struct lttng_event_rule *rule, const char **expression);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* LTTNG_EVENT_RULE_KERNEL_TRACEPOINT_H */
