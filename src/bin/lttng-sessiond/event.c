lttng/lttng-tools

commit 88e3c2f5610b9ac89b0923d448fee34140fc46fb

Fix: only synchronize application configuration on tracing start
The UST configuration of applications is currently replicated as it is
changed from the ltt_ust_{session, channel, event} data structures to
their ust_app_* equivalent as they are modified.

While this worked correctly for the most part, it caused a problem in
per-PID mode since the buffers would get allocated
(and files created, in applicable tracing modes) even though tracing
was never started during some applications' lifetime.

A previous fix attempt, 0498a00cb, adressed this problem but
introduced a regression that caused configurations to become
mismatched between the sessiond and applications in cases where a
tracing session was started, stopped, modified, and started again
within the lifetime of a given application.

This change introduces an explicit "synchronize" set of operations
that ensures that a session's channels and events configurations, as
known by the application(s), match those of the session daemon
whenever a session is started.

Signed-off-by: Jérémie Galarneau <jeremie.galarneau@efficios.com>

src/bin/lttng-sessiond/event.c
@@ -162,7 +162,7 @@ int event_ust_enable_tracepoint(struct ltt_ust_session *usess,
 		struct lttng_event_exclusion *exclusion,
 		bool internal_event)
 {
-	int ret, to_create = 0;
+	int ret = LTTNG_OK, to_create = 0;
 	struct ltt_ust_event *uevent;
 
 	assert(usess);
@@ -195,6 +195,14 @@ int event_ust_enable_tracepoint(struct ltt_ust_session *usess,
 	}
 
 	uevent->enabled = 1;
+	if (to_create) {
+		/* Add ltt ust event to channel */
+		add_unique_ust_event(uchan->events, uevent);
+	}
+
+	if (!usess->active) {
+		goto end;
+	}
 
 	if (to_create) {
 		/* Create event on all UST registered apps for session */
@@ -212,11 +220,6 @@ int event_ust_enable_tracepoint(struct ltt_ust_session *usess,
 			ret = LTTNG_ERR_UST_ENABLE_FAIL;
 			goto error;
 		}
-	}
-
-	if (to_create) {
-		/* Add ltt ust event to channel */
-		add_unique_ust_event(uchan->events, uevent);
 	}
 
 	DBG("Event UST %s %s in channel %s", uevent->attr.name,
@@ -291,17 +294,18 @@ int event_ust_disable_tracepoint(struct ltt_ust_session *usess,
 			/* It's already disabled so everything is OK */
 			goto next;
 		}
+		uevent->enabled = 0;
+		DBG2("Event UST %s disabled in channel %s", uevent->attr.name,
+				uchan->name);
 
+		if (!usess->active) {
+			goto next;
+		}
 		ret = ust_app_disable_event_glb(usess, uchan, uevent);
 		if (ret < 0 && ret != -LTTNG_UST_ERR_EXIST) {
 			ret = LTTNG_ERR_UST_DISABLE_FAIL;
 			goto error;
 		}
-		uevent->enabled = 0;
-
-		DBG2("Event UST %s disabled in channel %s", uevent->attr.name,
-				uchan->name);
-
 next:
 		/* Get next duplicate event by name. */
 		cds_lfht_next_duplicate(ht->ht, trace_ust_ht_match_event_by_name,
@@ -506,16 +510,17 @@ int event_agent_enable(struct ltt_ust_session *usess,
 		created = 1;
 	}
 
-	/* Already enabled? */
-	if (aevent->enabled) {
-		goto end;
-	}
-
 	if (created && filter) {
 		ret = add_filter_app_ctx(filter, filter_expression, agt);
 		if (ret != LTTNG_OK) {
 			goto error;
 		}
+	}
+
+	/* Already enabled? */
+	if (aevent->enabled) {
+		ret = LTTNG_OK;
+		goto end;
 	}
 
 	ret = agent_enable_event(aevent, agt->domain);
@@ -629,10 +634,12 @@ static int event_agent_disable_one(struct ltt_ust_session *usess,
 	/* If the agent event exists, it must be available on the UST side. */
 	assert(uevent);
 
-	ret = ust_app_disable_event_glb(usess, uchan, uevent);
-	if (ret < 0 && ret != -LTTNG_UST_ERR_EXIST) {
-		ret = LTTNG_ERR_UST_DISABLE_FAIL;
-		goto error;
+	if (usess->active) {
+		ret = ust_app_disable_event_glb(usess, uchan, uevent);
+		if (ret < 0 && ret != -LTTNG_UST_ERR_EXIST) {
+			ret = LTTNG_ERR_UST_DISABLE_FAIL;
+			goto error;
+		}
 	}
 
 	/*

src/bin/lttng-sessiond/channel.c
@@ -311,6 +311,18 @@ int channel_ust_enable(struct ltt_ust_session *usess,
 		DBG3("Channel %s already enabled. Skipping", uchan->name);
 		ret = LTTNG_ERR_UST_CHAN_EXIST;
 		goto end;
+	} else {
+		uchan->enabled = 1;
+		DBG2("Channel %s enabled successfully", uchan->name);
+	}
+
+	if (!usess->active) {
+		/*
+		 * The channel will be activated against the apps
+		 * when the session is started as part of the
+		 * application channel "synchronize" operation.
+		 */
+		goto end;
 	}
 
 	DBG2("Channel %s being enabled in UST domain", uchan->name);
@@ -325,8 +337,6 @@ int channel_ust_enable(struct ltt_ust_session *usess,
 	 */
 	(void) ust_app_enable_channel_glb(usess, uchan);
 
-	uchan->enabled = 1;
-	DBG2("Channel %s enabled successfully", uchan->name);
 
 end:
 	return ret;
@@ -465,11 +475,13 @@ int channel_ust_create(struct ltt_ust_session *usess,
 		goto error_free_chan;
 	}
 
-	/* Enable channel for global domain */
-	ret = ust_app_create_channel_glb(usess, uchan);
-	if (ret < 0 && ret != -LTTNG_UST_ERR_EXIST) {
-		ret = LTTNG_ERR_UST_CHAN_FAIL;
-		goto error_free_chan;
+	if (usess->active) {
+		/* Enable channel for global domain */
+		ret = ust_app_create_channel_glb(usess, uchan);
+		if (ret < 0 && ret != -LTTNG_UST_ERR_EXIST) {
+			ret = LTTNG_ERR_UST_CHAN_FAIL;
+			goto error_free_chan;
+		}
 	}
 
 	/* Adding the channel to the channel hash table. */
@@ -531,6 +543,9 @@ int channel_ust_disable(struct ltt_ust_session *usess,
 	/* Already disabled */
 	if (uchan->enabled == 0) {
 		DBG2("Channel UST %s already disabled", uchan->name);
+		goto end;
+	}
+	if (!usess->active) {
 		goto end;
 	}
 

src/bin/lttng-sessiond/context.c
@@ -169,18 +169,19 @@ static int add_uctx_to_channel(struct ltt_ust_session *usess,
 		goto error;
 	}
 
+	/* Add ltt UST context node to ltt UST channel */
+	lttng_ht_add_ulong(uchan->ctx, &uctx->node);
+	cds_list_add_tail(&uctx->list, &uchan->ctx_list);
+
+	if (!usess->active) {
+		goto end;
+	}
+
 	ret = ust_app_add_ctx_channel_glb(usess, uchan, uctx);
 	if (ret < 0) {
 		goto error;
 	}
-
-	rcu_read_lock();
-
-	/* Add ltt UST context node to ltt UST channel */
-	lttng_ht_add_ulong(uchan->ctx, &uctx->node);
-	rcu_read_unlock();
-	cds_list_add_tail(&uctx->list, &uchan->ctx_list);
-
+end:
 	DBG("Context UST %d added to channel %s", uctx->ctx.ctx, uchan->name);
 
 	return 0;

src/bin/lttng-sessiond/dispatch.c
@@ -61,7 +61,7 @@ static void update_ust_app(int app_sock)
 			continue;
 		}
 		session_lock(sess);
-		if (!sess->ust_session) {
+		if (!sess->active || !sess->ust_session) {
 			goto unlock_session;
 		}
 

src/bin/lttng-sessiond/trace-ust.c
@@ -827,13 +827,14 @@ int trace_ust_pid_tracker_lookup(struct ltt_ust_session *session, int pid)
 int trace_ust_track_pid(struct ltt_ust_session *session, int pid)
 {
 	int retval = LTTNG_OK;
+	bool should_update_apps = false;
 
 	if (pid == -1) {
 		/* Track all pids: destroy tracker if exists. */
 		if (session->pid_tracker.ht) {
 			fini_pid_tracker(&session->pid_tracker);
 			/* Ensure all apps have session. */
-			ust_app_global_update_all(session);
+			should_update_apps = true;
 		}
 	} else {
 		int ret;
@@ -852,7 +853,7 @@ int trace_ust_track_pid(struct ltt_ust_session *session, int pid)
 				goto end;
 			}
 			/* Remove all apps from session except pid. */
-			ust_app_global_update_all(session);
+			should_update_apps = true;
 		} else {
 			struct ust_app *app;
 
@@ -864,9 +865,12 @@ int trace_ust_track_pid(struct ltt_ust_session *session, int pid)
 			/* Add session to application */
 			app = ust_app_find_by_pid(pid);
 			if (app) {
-				ust_app_global_update(session, app);
+				should_update_apps = true;
 			}
 		}
+	}
+	if (should_update_apps && session->active) {
+		ust_app_global_update_all(session);
 	}
 end:
 	return retval;

src/bin/lttng-sessiond/ust-app.h
@@ -315,17 +315,12 @@ int ust_app_create_channel_glb(struct ltt_ust_session *usess,
 		struct ltt_ust_channel *uchan);
 int ust_app_create_event_glb(struct ltt_ust_session *usess,
 		struct ltt_ust_channel *uchan, struct ltt_ust_event *uevent);
-int ust_app_enable_event_pid(struct ltt_ust_session *usess,
-		struct ltt_ust_channel *uchan, struct ltt_ust_event *uevent,
-		pid_t pid);
 int ust_app_disable_channel_glb(struct ltt_ust_session *usess,
 		struct ltt_ust_channel *uchan);
 int ust_app_enable_channel_glb(struct ltt_ust_session *usess,
 		struct ltt_ust_channel *uchan);
 int ust_app_enable_event_glb(struct ltt_ust_session *usess,
 		struct ltt_ust_channel *uchan, struct ltt_ust_event *uevent);
-int ust_app_enable_all_event_glb(struct ltt_ust_session *usess,
-		struct ltt_ust_channel *uchan);
 int ust_app_disable_event_glb(struct ltt_ust_session *usess,
 		struct ltt_ust_channel *uchan, struct ltt_ust_event *uevent);
 int ust_app_add_ctx_channel_glb(struct ltt_ust_session *usess,
@@ -463,12 +458,6 @@ int ust_app_enable_channel_glb(struct ltt_ust_session *usess,
 }
 static inline
 int ust_app_create_channel_glb(struct ltt_ust_session *usess,
-		struct ltt_ust_channel *uchan)
-{
-	return 0;
-}
-static inline
-int ust_app_enable_all_event_glb(struct ltt_ust_session *usess,
 		struct ltt_ust_channel *uchan)
 {
 	return 0;

commit 3a2434036c2e836d58bfa5b89ba47b5898d86d35

Fix: ust: segfault on lttng start on filter bytecode copy
Observed issue
==============

A segmentation fault is observed for multiple UST timeout scenarios.

Backtrace:

 #0  __memmove_avx_unaligned_erms () at ../sysdeps/x86_64/multiarch/memmove-vec-unaligned-erms.S:384
 #1  0x0000557fe0395df9 in copy_filter_bytecode (orig_f=0x7f9c5802b790) at ust-app.c:1196
 #2  0x0000557fe0397702 in shadow_copy_event (ua_event=0x7f9c58025ff0, uevent=0x7f9c58033560) at ust-app.c:1824
 #3  0x0000557fe039ac46 in create_ust_app_event (ua_sess=0x7f9c5802ec20, ua_chan=0x7f9c58025cc0, uevent=0x7f9c58033560, app=0x7f9c5c001da0) at ust-app.c:3192
 #4  0x0000557fe03a054d in ust_app_channel_synchronize_event (ua_chan=0x7f9c58025cc0, uevent=0x7f9c58033560, ua_sess=0x7f9c5802ec20, app=0x7f9c5c001da0) at ust-app.c:5096
 #5  0x0000557fe03a0772 in ust_app_synchronize (usess=0x7f9c580074a0, app=0x7f9c5c001da0) at ust-app.c:5173
 #6  0x0000557fe03a0a70 in ust_app_global_update (usess=0x7f9c580074a0, app=0x7f9c5c001da0) at ust-app.c:5255
 #7  0x0000557fe03a00e0 in ust_app_start_trace_all (usess=0x7f9c580074a0) at ust-app.c:4987
 #8  0x0000557fe0355c6a in cmd_start_trace (session=0x7f9c5800a190) at cmd.c:2668
 #9  0x0000557fe0382e70 in process_client_msg (cmd_ctx=0x7f9c58003d70, sock=0x7f9c74bf44e0, sock_error=0x7f9c74bf44e4) at client.c:1527
 #10 0x0000557fe03848a2 in thread_manage_clients (data=0x557fe06d9440) at client.c:2200
 #11 0x0000557fe037d1cb in launch_thread (data=0x557fe06d94b0) at thread.c:75
 #12 0x00007f9c796af609 in start_thread (arg=<optimized out>) at pthread_create.c:477
 #13 0x00007f9c795b6293 in clone () at ../sysdeps/unix/sysv/linux/x86_64/clone.S:95

The scenario:

  # Start an instrumented app
  ./app
  gdb lttng-sessiond
  # put a breakpoint on ustctl_set_filter
  lttng create my_session
  lttng enable-event -u tp:tp_test
  lttng start
  lttng enable-event -u __dummy --filter 'my_field == "user34"'
  # The tracepoint should hit. Do not continue.
  kill -s SIGSTOP $(pgrep app)
  # Continue lttng-sessiond.
  # enable-event will return an error. This a bug in itself, still let's
  # continue with the current bug.
  lttng stop
  # Start a new app that will register.
  ./app &
  sleep 1
  lttng start
  # lttng-sessiond should segfault.

Cause
=====

During the "lttng enable-event" command, the timeout error bubbles up
all the way to event_ust_enable_tracepoint and is different from
LTTNG_UST_ERR_EXIST. `trace_ust_destroy_event` is called and frees the
`uevent` object. Note that contrary to the comment `uevent` is added to
the channel event hash table at this point.

On the next `lttng start` command, the event node is still present in
the hash table and is iterated on. lttng-sessiond segfault on the first
data access of the previously freed memory.

The problem was introduced by commit
88e3c2f5610b9ac89b0923d448fee34140fc46fb [1]. Which essentially move the

src/bin/lttng-sessiond/event.c
@@ -179,7 +179,7 @@ int event_ust_enable_tracepoint(struct ltt_ust_session *usess,
 		filter = NULL;
 		exclusion = NULL;
 		if (ret != LTTNG_OK) {
-			goto error;
+			goto end;
 		}
 
 		/* Valid to set it after the goto error since uevent is still NULL */
@@ -214,11 +214,10 @@ int event_ust_enable_tracepoint(struct ltt_ust_session *usess,
 	if (ret < 0) {
 		if (ret == -LTTNG_UST_ERR_EXIST) {
 			ret = LTTNG_ERR_UST_EVENT_EXIST;
-			goto end;
 		} else {
 			ret = LTTNG_ERR_UST_ENABLE_FAIL;
-			goto error;
 		}
+		goto end;
 	}
 
 	DBG("Event UST %s %s in channel %s", uevent->attr.name,
@@ -227,24 +226,6 @@ int event_ust_enable_tracepoint(struct ltt_ust_session *usess,
 	ret = LTTNG_OK;
 
 end:
-	rcu_read_unlock();
-	free(filter_expression);
-	free(filter);
-	free(exclusion);
-	return ret;
-
-error:
-	/*
-	 * Only destroy event on creation time (not enabling time) because if the
-	 * event is found in the channel (to_create == 0), it means that at some
-	 * point the enable_event worked and it's thus valid to keep it alive.
-	 * Destroying it also implies that we also destroy it's shadow copy to sync
-	 * everyone up.
-	 */
-	if (to_create) {
-		/* In this code path, the uevent was not added to the hash table */
-		trace_ust_destroy_event(uevent);
-	}
 	rcu_read_unlock();
 	free(filter_expression);
 	free(filter);