a878d96125906e6660e6ef3d1f970c13a4e22fbd
Mathieu Desnoyers <mathieu.desnoyers@efficios.com>
2022-01-11 15:57:37 -0500

    Introduce libcommon-lgpl for liblttng-ctl
    liblttng-ctl is a LGPLv2.1 library should should not use GPLv2 code.
    Introduce libcommon-lgpl as a static archive containing only LGPLv2.1
    compatible code.
    
    This also removes the dependency from liblttng-ctl to liburcu.
    
    Include some source files in libcommon-lgpl.a which are indirectly needed
    by source files required in libcommon-lgpl.a:
    
    - endpoint.cpp,
    - lttng-elf.cpp,
    - lttng-elf.h.
    
    Include some source files in libcommon-lgpl.a which are only needed to
    link the lttng executable:
    
    - domain.cpp,
    - spawn-viewer.cpp, spawn-viewer.h.
    
    Introduce the new source file hashtable/seed.cpp to move the
    lttng_ht_seed symbol in a source file which does not require
    liburcu-cds, so it can be present in libcommon-lgpl. This allows
    building compile units which are needed in the lgpl common library which
    also contain functions which directly refer to lttng_ht_seed.
    
    Programs and libraries which use libhashtable.la are changed to use
    libcommon-gpl.la instead. libhashtable becomes internal to libcommon.
    
    Signed-off-by: Mathieu Desnoyers <mathieu.desnoyers@efficios.com>
    Signed-off-by: Jérémie Galarneau <jeremie.galarneau@efficios.com>
    Change-Id: I27d2acb823a6d951692a5da88ce32bbe6bafb072

---
+++ src/common/hashtable/seed.c
@@ -0,0 +1,11 @@
+/*
+ * Copyright (C) 2021 Mathieu Desnoyers <mathieu.desnoyers@efficios.com>
+ *
+ * SPDX-License-Identifier: LGPL-2.1-only
+ */
+
+#define _LGPL_SOURCE
+#include "hashtable.h"
+#include "utils.h"
+
+unsigned long lttng_ht_seed;
