c922647daed3dd022be11980063b5fb816d8c091
Mathieu Desnoyers <mathieu.desnoyers@efficios.com>
2022-01-12 16:46:21 -0500

    Relicence all source and header files included in LGPL code
    All code included in libcommon-lgpl.a should be LGPL. Some were licensed
    as GPLv2 by mistake. We need to relicense those.
    
    EfficiOS owns the copyright to all of the affected source files and
    agrees to this relicensing from GPLv2 to LGPLv2.1.
    
    Signed-off-by: Mathieu Desnoyers <mathieu.desnoyers@efficios.com>
    Signed-off-by: Jérémie Galarneau <jeremie.galarneau@efficios.com>
    Change-Id: Ib4aa1a7cc8a6f8e2a9891f1bd05c7ea2f8530e9b

--- src/common/unix.cpp
+++ src/common/unix.cpp
@@ -2,7 +2,7 @@
  * Copyright (C) 2011 EfficiOS Inc.
  * Copyright (C) 2011 Mathieu Desnoyers <mathieu.desnoyers@efficios.com>
  *
- * SPDX-License-Identifier: GPL-2.0-only
+ * SPDX-License-Identifier: LGPL-2.1-only
  *
  */
 
