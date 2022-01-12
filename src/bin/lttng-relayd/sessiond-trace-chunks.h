72c696e8fb82949915fe107851169fdea87ae8bc
Jérémie Galarneau <jeremie.galarneau@efficios.com>
2022-01-12 12:43:07 -0500

    Docs: relayd: document the role of session_trace_chunk_registry
    Signed-off-by: Jérémie Galarneau <jeremie.galarneau@efficios.com>
    Change-Id: I6158dadfe92d23c0081aac888c3e54b3c9c711a3

--- src/bin/lttng-relayd/sessiond-trace-chunks.h
+++ src/bin/lttng-relayd/sessiond-trace-chunks.h
@@ -12,6 +12,17 @@
 #include <common/trace-chunk.h>
 #include <stdint.h>
 
+/*
+ * A session trace chunk registry allows the relay daemon to share trace chunks
+ * used by different "relay sessions" when they were created for the same
+ * user-visible session daemon session. Tracing multiple domains (e.g. ust and
+ * kernel) results in per-domain relay sessions being created.
+ *
+ * Sharing trace chunks, and their output directory more specifically, is
+ * essential to properly implement session rotations. The sharing of output
+ * directory handles allows directory renames to be performed once and without
+ * races that would stem from from multiple renames.
+ */
 struct sessiond_trace_chunk_registry;
 
 struct sessiond_trace_chunk_registry *
