lttng/lttng-tools

commit fb2772932d3207bd9d340f34f41d822d9c0ff0a9

Validate channel context mismatch across UST applications
Observed issue
==============

Applications traced with LTTng-UST are expected to all provide the exact
same layout for their channel's context fields, else it leads to
corrupted traces. This is only enforced within LTTng-UST. There is
nothing in the session daemon that prevents this scenario, and it is
only observable when reading the corrupted trace.

This makes the entire trace unreadable from the point where it is
corrupted.

Cause
=====

Even though LTTng-UST sends the entire description of its context fields
along with the channel registration notification, there is no validation
of the context fields' content against the context fields present in the
ust registry.

Solution
========

Validate each registered UST channel context fields against the fields
present in the registry. Reject the application if there is a mismatch.

Known drawbacks
===============

None.

Signed-off-by: Mathieu Desnoyers <mathieu.desnoyers@efficios.com>
Signed-off-by: Jérémie Galarneau <jeremie.galarneau@efficios.com>
Change-Id: I8b49032bf4f766e549dfccfafdce8cddcbb2873f

src/bin/lttng-sessiond/ust-app.cpp
@@ -49,7 +49,7 @@
 #include "rotate.h"
 #include "event.h"
 #include "event-notifier-error-accounting.h"
-
+#include "ust-field-utils.h"
 
 struct lttng_ht *ust_app_ht;
 struct lttng_ht *ust_app_ht_by_sock;
@@ -6386,6 +6386,9 @@ static int reply_ust_register_channel(int sock, int cobjd,
 	ust_reg_chan = ust_registry_channel_find(registry, chan_reg_key);
 	LTTNG_ASSERT(ust_reg_chan);
 
+	/* Channel id is set during the object creation. */
+	chan_id = ust_reg_chan->chan_id;
+
 	if (!ust_reg_chan->register_done) {
 		/*
 		 * TODO: eventually use the registry event count for
@@ -6400,9 +6403,19 @@ static int reply_ust_register_channel(int sock, int cobjd,
 	} else {
 		/* Get current already assigned values. */
 		type = ust_reg_chan->header_type;
+		/*
+		 * Validate that the context fields match between
+		 * registry and newcoming application.
+		 */
+		if (!match_lttng_ust_ctl_field_array(ust_reg_chan->ctx_fields,
+				ust_reg_chan->nr_ctx_fields,
+				fields, nr_fields)) {
+			ERR("Registering application channel due to context field mismatch: pid = %d, sock = %d",
+				app->pid, app->sock);
+			ret_code = -EINVAL;
+			goto reply;
+		}
 	}
-	/* Channel id is set during the object creation. */
-	chan_id = ust_reg_chan->chan_id;
 
 	/* Append to metadata */
 	if (!ust_reg_chan->metadata_dumped) {

src/bin/lttng-sessiond/ust-field-utils.cpp
@@ -338,3 +338,33 @@ int match_lttng_ust_ctl_field(const struct lttng_ust_ctl_field *first,
 no_match:
 	return false;
 }
+
+/*
+ * Compare two arrays of UST fields.
+ * Return true if both arrays have identical field definitions, false otherwise.
+ */
+bool match_lttng_ust_ctl_field_array(const struct lttng_ust_ctl_field *first,
+		size_t nr_first,
+		const struct lttng_ust_ctl_field *second,
+		size_t nr_second)
+{
+	size_t i;
+	const size_t nr_fields = nr_first;
+
+	/* Compare the array lengths. */
+	if (nr_first != nr_second) {
+		goto no_match;
+	}
+
+	/* Compare each field individually. */
+	for (i = 0; i < nr_fields; i++) {
+		if (!match_lttng_ust_ctl_field(&first[i], &second[i])) {
+			goto no_match;
+		}
+	}
+
+	return true;
+
+no_match:
+	return false;
+}

src/bin/lttng-sessiond/ust-field-utils.h
@@ -17,4 +17,13 @@
 int match_lttng_ust_ctl_field(const struct lttng_ust_ctl_field *first,
 		const struct lttng_ust_ctl_field *second);
 
+/*
+ * Compare two arrays of UST fields.
+ * Return true if both arrays have identical field definitions, false otherwise.
+ */
+bool match_lttng_ust_ctl_field_array(const struct lttng_ust_ctl_field *first,
+		size_t nr_first,
+		const struct lttng_ust_ctl_field *second,
+		size_t nr_second);
+
 #endif /* LTTNG_UST_FIELD_UTILS_H */

src/bin/lttng-sessiond/ust-registry.cpp
@@ -19,7 +19,6 @@
 #include "lttng-sessiond.h"
 #include "notification-thread-commands.h"
 
-
 /*
  * Hash table match function for event in the registry.
  */
@@ -27,7 +26,6 @@ static int ht_match_event(struct cds_lfht_node *node, const void *_key)
 {
 	const struct ust_registry_event *key;
 	struct ust_registry_event *event;
-	int i;
 
 	LTTNG_ASSERT(node);
 	LTTNG_ASSERT(_key);
@@ -46,16 +44,10 @@ static int ht_match_event(struct cds_lfht_node *node, const void *_key)
 		goto no_match;
 	}
 
-	/* Compare the number of fields. */
-	if (event->nr_fields != key->nr_fields) {
+	/* Compare the arrays of fields. */
+	if (!match_lttng_ust_ctl_field_array(event->fields, event->nr_fields,
+			key->fields, key->nr_fields)) {
 		goto no_match;
-	}
-
-	/* Compare each field individually. */
-	for (i = 0; i < event->nr_fields; i++) {
-		if (!match_lttng_ust_ctl_field(&event->fields[i], &key->fields[i])) {
-			goto no_match;
-		}
 	}
 
 	/* Compare model URI. */

commit c559ee63d6a599e594d65b96298229690685e948

Fix context mismatch across UST version due to legacy array context field
Observed issue
==============

Tracing applications linked against both LTTng-UST 2.12.x and 2.13.1 in
the same session fails with:

"Error: Registering application channel due to context field mismatch"

for some applications with the "procname" context enabled.

Cause
=====

The procname context uses the legacy array field type prior to LTTng-UST
2.13, and uses the array_nestable field type starting from LTTng-UST
2.13. The field comparison in lttng-sessiond is unaware of the fact that
they map to the exact same binary trace layout and are therefore
compatible.

Solution
========

Introduce a new fixup function "ust_app_fixup_legacy_context_fields" in
lttng-sessiond for channel context fields, which detects the presence of
the legacy array, struct and variant types, and rewrites them as
array_nestable, struct_nestable, and variant_nestable types.

Reject the legacy sequence type in channel context fields because it is
not used by LTTng-UST 2.12 and older.

Rewriting those legacy context types as the new "nestable" types ensures
that field comparison functions will correctly handle a mix of 2.12 and
2.13 LTTng-UST tracers using a procname context to a given session's
channel.

Known Drawbacks
===============

None.

Fixes: fb2772932 ("Validate channel context mismatch across UST applications")

Change-Id: Ic51b92130ae8e1f7b510924f78ff46459dc2f578
Signed-off-by: Mathieu Desnoyers <mathieu.desnoyers@efficios.com>
Signed-off-by: Jérémie Galarneau <jeremie.galarneau@efficios.com>

src/bin/lttng-sessiond/ust-app.cpp
@@ -6324,6 +6324,104 @@ error:
 }
 
 /*
+ * Fixup legacy context fields for comparison:
+ * - legacy array becomes array_nestable,
+ * - legacy struct becomes struct_nestable,
+ * - legacy variant becomes variant_nestable,
+ * legacy sequences are not emitted in LTTng-UST contexts.
+ */
+static int ust_app_fixup_legacy_context_fields(size_t *_nr_fields,
+		struct lttng_ust_ctl_field **_fields)
+{
+	struct lttng_ust_ctl_field *fields = *_fields, *new_fields = NULL;
+	size_t nr_fields = *_nr_fields, new_nr_fields = 0, i, j;
+	bool found = false;
+	int ret = 0;
+
+	for (i = 0; i < nr_fields; i++) {
+		const struct lttng_ust_ctl_field *field = &fields[i];
+
+		switch (field->type.atype) {
+		case lttng_ust_ctl_atype_sequence:
+			ERR("Unexpected legacy sequence context.");
+			ret = -EINVAL;
+			goto end;
+		case lttng_ust_ctl_atype_array:
+			switch (field->type.u.legacy.array.elem_type.atype) {
+			case lttng_ust_ctl_atype_integer:
+				break;
+			default:
+				ERR("Unexpected legacy array element type in context.");
+				ret = -EINVAL;
+				goto end;
+			}
+			found = true;
+			/* One field for array_nested, one field for elem type. */
+			new_nr_fields += 2;
+			break;
+
+		case lttng_ust_ctl_atype_struct:	/* Fallthrough */
+		case lttng_ust_ctl_atype_variant:
+			found = true;
+			new_nr_fields++;
+			break;
+		default:
+			new_nr_fields++;
+			break;
+		}
+	}
+	if (!found) {
+		goto end;
+	}
+	new_fields = (struct lttng_ust_ctl_field *) zmalloc(sizeof(*new_fields) * new_nr_fields);
+	if (!new_fields) {
+		ret = -ENOMEM;
+		goto end;
+	}
+	for (i = 0, j = 0; i < nr_fields; i++, j++) {
+		const struct lttng_ust_ctl_field *field = &fields[i];
+		struct lttng_ust_ctl_field *new_field = &new_fields[j];
+
+		switch (field->type.atype) {
+		case lttng_ust_ctl_atype_array:
+			/* One field for array_nested, one field for elem type. */
+			strncpy(new_field->name, field->name, LTTNG_UST_ABI_SYM_NAME_LEN - 1);
+			new_field->type.atype = lttng_ust_ctl_atype_array_nestable;
+			new_field->type.u.array_nestable.length = field->type.u.legacy.array.length;
+			new_field->type.u.array_nestable.alignment = 0;
+			new_field = &new_fields[++j];	/* elem type */
+			new_field->type.atype = field->type.u.legacy.array.elem_type.atype;
+			assert(new_field->type.atype == lttng_ust_ctl_atype_integer);
+			new_field->type.u.integer = field->type.u.legacy.array.elem_type.u.basic.integer;
+			break;
+		case lttng_ust_ctl_atype_struct:
+			strncpy(new_field->name, field->name, LTTNG_UST_ABI_SYM_NAME_LEN - 1);
+			new_field->type.atype = lttng_ust_ctl_atype_struct_nestable;
+			new_field->type.u.struct_nestable.nr_fields = field->type.u.legacy._struct.nr_fields;
+			new_field->type.u.struct_nestable.alignment = 0;
+			break;
+		case lttng_ust_ctl_atype_variant:
+			strncpy(new_field->name, field->name, LTTNG_UST_ABI_SYM_NAME_LEN - 1);
+			new_field->type.atype = lttng_ust_ctl_atype_variant_nestable;
+			new_field->type.u.variant_nestable.nr_choices = field->type.u.legacy.variant.nr_choices;
+			strncpy(new_field->type.u.variant_nestable.tag_name,
+				field->type.u.legacy.variant.tag_name,
+				LTTNG_UST_ABI_SYM_NAME_LEN - 1);
+			new_field->type.u.variant_nestable.alignment = 0;
+			break;
+		default:
+			*new_field = *field;
+			break;
+		}
+	}
+	free(fields);
+	*_fields = new_fields;
+	*_nr_fields = new_nr_fields;
+end:
+	return ret;
+}
+
+/*
  * Reply to a register channel notification from an application on the notify
  * socket. The channel metadata is also created.
  *
@@ -6337,7 +6435,7 @@ static int reply_ust_register_channel(int sock, int cobjd,
 	int ret, ret_code = 0;
 	uint32_t chan_id;
 	uint64_t chan_reg_key;
-	enum lttng_ust_ctl_channel_header type;
+	enum lttng_ust_ctl_channel_header type = LTTNG_UST_CTL_CHANNEL_HEADER_UNKNOWN;
 	struct ust_app *app;
 	struct ust_app_channel *ua_chan;
 	struct ust_app_session *ua_sess;
@@ -6389,6 +6487,13 @@ static int reply_ust_register_channel(int sock, int cobjd,
 	/* Channel id is set during the object creation. */
 	chan_id = ust_reg_chan->chan_id;
 
+	ret = ust_app_fixup_legacy_context_fields(&nr_fields, &fields);
+	if (ret < 0) {
+		ERR("Registering application channel due to legacy context fields fixup error: pid = %d, sock = %d",
+			app->pid, app->sock);
+		ret_code = -EINVAL;
+		goto reply;
+	}
 	if (!ust_reg_chan->register_done) {
 		/*
 		 * TODO: eventually use the registry event count for