2577e3adfd9c350dd3c809b09977a2f144277660
Jérémie Galarneau <jeremie.galarneau@efficios.com>
2022-03-09 07:56:47 -0500

    Build fix: consumer: aggregate initialization isn't supported
    Aggregate initialization is not supported in C. Change the syntax to use
    a compound literal.
    
    Signed-off-by: Jérémie Galarneau <jeremie.galarneau@efficios.com>
    Change-Id: I630ca41b13ea730e699e53ae9a23bdd78acbed57

--- src/common/consumer/consumer-stream.c
+++ src/common/consumer/consumer-stream.c
@@ -670,7 +670,8 @@ struct lttng_consumer_stream *consumer_stream_create(
 		goto error;
 	}
 
-	stream->send_node = CDS_LIST_HEAD_INIT(stream->send_node);
+	stream->send_node = (typeof(stream->send_node))
+			CDS_LIST_HEAD_INIT(stream->send_node);
 	stream->chan = channel;
 	stream->key = stream_key;
 	stream->trace_chunk = trace_chunk;
