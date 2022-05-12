5bb4ff54d92e0a4520f02d08601a0c25f4d2505f
Jérémie Galarneau <jeremie.galarneau@efficios.com>
2022-05-12 14:32:32 -0400

    format: add a custom formatter for std::type_info
    The name() returned by std::type_info is implementation-dependant. In
    practice, it is typically the type's mangled name. For GCC and clang, it
    is possible to use abi::__cxa_demangle to demangle the name at runtime
    while formatting a string.
    
    If this poses any compatibility problem on other platforms, we can
    fallback to using name() directly.
    
    Signed-off-by: Jérémie Galarneau <jeremie.galarneau@efficios.com>
    Change-Id: Ib80babe4039c91ce6fdf6d9a9442c31645a43b08

--- src/common/format.hpp
+++ src/common/format.hpp
@@ -9,11 +9,29 @@
 
 #include <common/macros.hpp>
 
+#include <string>
+#include <cxxabi.h>
+
 DIAGNOSTIC_PUSH
 DIAGNOSTIC_IGNORE_SUGGEST_ATTRIBUTE_FORMAT
 DIAGNOSTIC_IGNORE_DUPLICATED_BRANCHES
 #define FMT_HEADER_ONLY
 #include <vendor/fmt/core.h>
 DIAGNOSTIC_POP
+
+template <>
+struct fmt::formatter<std::type_info> : fmt::formatter<std::string> {
+	template <typename FormatCtx>
+	typename FormatCtx::iterator format(const std::type_info& type_info, FormatCtx& ctx)
+	{
+		int status;
+		auto demangled_name = abi::__cxa_demangle(type_info.name(), nullptr, 0, &status);
+		auto it = status == 0 ? fmt::formatter<std::string>::format(demangled_name, ctx) :
+					      fmt::formatter<std::string>::format(type_info.name(), ctx);
+
+		free(demangled_name);
+		return it;
+	}
+};
 
 #endif /* LTTNG_FORMAT_H */
