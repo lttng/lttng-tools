1823e905abb7044e38434cc7d16fdec8545dda1c
Jérémie Galarneau <jeremie.galarneau@efficios.com>
2022-05-12 14:24:07 -0400

    Add lttng::make_unique
    Add an equivalent to std::make_unique introduced in C++14.
    
    See https://herbsutter.com/gotw/_102/ for more details.
    
    Change-Id: I67e5b54d883e311869d7c1272f9a2905dc349212
    Signed-off-by: Jérémie Galarneau <jeremie.galarneau@efficios.com>

---
+++ src/common/make-unique.hpp
@@ -0,0 +1,23 @@
+/*
+ * Copyright (C) 2022 Jérémie Galarneau <jeremie.galarneau@efficios.com>
+ *
+ * SPDX-License-Identifier: LGPL-2.1-only
+ *
+ */
+
+#ifndef LTTNG_MAKE_UNIQUE_H
+#define LTTNG_MAKE_UNIQUE_H
+
+#include <memory>
+
+namespace lttng {
+
+template <typename Type, typename... Args>
+std::unique_ptr<Type> make_unique(Args&&...args)
+{
+	return std::unique_ptr<Type>(new Type(std::forward<Args>(args)...));
+}
+
+} /* namespace lttng */
+
+#endif /* LTTNG_MAKE_UNIQUE_H */
