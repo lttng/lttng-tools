lttng/lttng-tools

commit 66cefebdc240cbae0bc79594305f509b0779fa98

Clean-up: replace uses of `int enabled` with boolean flags
Signed-off-by: Jérémie Galarneau <jeremie.galarneau@efficios.com>
Change-Id: If9cf1b6e2db67d461479a43d8ed3c3e07c4133ba

src/bin/lttng/commands/disable_events.cpp
@@ -147,8 +147,8 @@ end:
  */
 static int disable_events(char *session_name, char *event_list)
 {
-	int ret = CMD_SUCCESS, warn = 0, command_ret = CMD_SUCCESS;
-	int enabled = 1, success = 1;
+	enum cmd_error_code ret = CMD_SUCCESS, command_ret = CMD_SUCCESS;
+	bool enabled = true, success = true, warn = false;
 	char *event_name, *channel_name = nullptr;
 	struct lttng_domain dom;
 	struct lttng_event event;
@@ -175,28 +175,28 @@ static int disable_events(char *session_name, char *event_list)
 
 	handle = lttng_create_handle(session_name, &dom);
 	if (handle == nullptr) {
-		ret = -1;
+		ret = CMD_ERROR;
 		goto error;
 	}
 
 	/* Mi print the channel and open the events element */
 	if (lttng_opt_mi) {
-		ret = mi_lttng_writer_open_element(writer, config_element_channel);
-		if (ret) {
+		int mi_ret = mi_lttng_writer_open_element(writer, config_element_channel);
+		if (mi_ret) {
 			ret = CMD_ERROR;
 			goto end;
 		}
 
-		ret = mi_lttng_writer_write_element_string(
+		mi_ret = mi_lttng_writer_write_element_string(
 			writer, config_element_name, print_channel_name(channel_name));
-		if (ret) {
+		if (mi_ret) {
 			ret = CMD_ERROR;
 			goto end;
 		}
 
 		/* Open events element */
-		ret = mi_lttng_writer_open_element(writer, config_element_events);
-		if (ret) {
+		mi_ret = mi_lttng_writer_open_element(writer, config_element_events);
+		if (mi_ret) {
 			ret = CMD_ERROR;
 			goto end;
 		}
@@ -210,15 +210,16 @@ static int disable_events(char *session_name, char *event_list)
 	event.type = (lttng_event_type) opt_event_type;
 
 	if (opt_disable_all) {
-		command_ret = lttng_disable_event_ext(handle, &event, channel_name, nullptr);
-		if (command_ret < 0) {
-			ERR("%s", lttng_strerror(command_ret));
-			enabled = 1;
-			success = 0;
+		const int disable_ret = lttng_disable_event_ext(handle, &event, channel_name, nullptr);
 
+		if (disable_ret < 0) {
+			ERR("%s", lttng_strerror(command_ret));
+			command_ret = CMD_ERROR;
+			enabled = true;
+			success = false;
 		} else {
-			enabled = 0;
-			success = 1;
+			enabled = false;
+			success = true;
 			MSG("All %s events of type %s are disabled in channel %s",
 			    lttng_domain_type_str(dom.type),
 			    print_event_type((lttng_event_type) opt_event_type),
@@ -226,8 +227,9 @@ static int disable_events(char *session_name, char *event_list)
 		}
 
 		if (lttng_opt_mi) {
-			ret = mi_print_event("*", enabled, success);
-			if (ret) {
+			const int mi_ret = mi_print_event("*", enabled, success);
+
+			if (mi_ret) {
 				ret = CMD_ERROR;
 				goto error;
 			}
@@ -240,9 +242,9 @@ static int disable_events(char *session_name, char *event_list)
 
 			strncpy(event.name, event_name, sizeof(event.name));
 			event.name[sizeof(event.name) - 1] = '\0';
-			command_ret =
+			const int disable_ret =
 				lttng_disable_event_ext(handle, &event, channel_name, nullptr);
-			if (command_ret < 0) {
+			if (disable_ret < 0) {
 				ERR("%s of type %s : %s (channel %s, session %s)",
 				    event_name,
 				    print_event_type((lttng_event_type) opt_event_type),
@@ -251,13 +253,14 @@ static int disable_events(char *session_name, char *event_list)
 					    print_raw_channel_name(channel_name) :
 					    print_channel_name(channel_name),
 				    session_name);
-				warn = 1;
-				success = 0;
+				warn = true;
+				success = false;
 				/*
 				 * If an error occurred we assume that the event is still
 				 * enabled.
 				 */
-				enabled = 1;
+				enabled = true;
+				command_ret = CMD_ERROR;
 			} else {
 				MSG("%s %s of type %s disabled in channel %s for session %s",
 				    lttng_domain_type_str(dom.type),
@@ -265,13 +268,14 @@ static int disable_events(char *session_name, char *event_list)
 				    print_event_type((lttng_event_type) opt_event_type),
 				    print_channel_name(channel_name),
 				    session_name);
-				success = 1;
-				enabled = 0;
+				success = true;
+				enabled = false;
 			}
 
 			if (lttng_opt_mi) {
-				ret = mi_print_event(event_name, enabled, success);
-				if (ret) {
+				const int mi_ret = mi_print_event(event_name, enabled, success);
+
+				if (mi_ret) {
 					ret = CMD_ERROR;
 					goto error;
 				}
@@ -285,8 +289,9 @@ static int disable_events(char *session_name, char *event_list)
 end:
 	if (lttng_opt_mi) {
 		/* Close events element and channel element */
-		ret = mi_lttng_close_multi_element(writer, 2);
-		if (ret) {
+		const int mi_ret = mi_lttng_close_multi_element(writer, 2);
+
+		if (mi_ret) {
 			ret = CMD_ERROR;
 		}
 	}

src/bin/lttng-sessiond/channel.cpp
@@ -145,7 +145,7 @@ int channel_kernel_disable(struct ltt_kernel_session *ksession, char *channel_na
 	}
 
 	/* Only if channel is enabled disable it. */
-	if (kchan->enabled == 1) {
+	if (kchan->enabled) {
 		ret = kernel_disable_channel(kchan);
 		if (ret < 0 && ret != -EEXIST) {
 			ret = LTTNG_ERR_KERN_CHAN_DISABLE_FAIL;
@@ -170,7 +170,7 @@ enum lttng_error_code channel_kernel_enable(struct ltt_kernel_session *ksession,
 	LTTNG_ASSERT(ksession);
 	LTTNG_ASSERT(kchan);
 
-	if (kchan->enabled == 0) {
+	if (!kchan->enabled) {
 		if (kernel_enable_channel(kchan) < 0) {
 			ret_code = LTTNG_ERR_KERN_CHAN_ENABLE_FAIL;
 			goto error;
@@ -284,7 +284,7 @@ enum lttng_error_code channel_ust_enable(struct ltt_ust_session *usess,
 		ret_code = LTTNG_ERR_UST_CHAN_EXIST;
 		goto end;
 	} else {
-		uchan->enabled = 1;
+		uchan->enabled = true;
 		DBG2("Channel %s enabled successfully", uchan->name);
 	}
 
@@ -425,7 +425,7 @@ enum lttng_error_code channel_ust_create(struct ltt_ust_session *usess,
 		goto error;
 	}
 
-	uchan->enabled = 1;
+	uchan->enabled = true;
 	if (trace_ust_is_max_id(usess->used_channel_id)) {
 		ret_code = LTTNG_ERR_UST_CHAN_FAIL;
 		goto error;
@@ -501,12 +501,12 @@ int channel_ust_disable(struct ltt_ust_session *usess, struct ltt_ust_channel *u
 	LTTNG_ASSERT(uchan);
 
 	/* Already disabled */
-	if (uchan->enabled == 0) {
+	if (!uchan->enabled) {
 		DBG2("Channel UST %s already disabled", uchan->name);
 		goto end;
 	}
 
-	uchan->enabled = 0;
+	uchan->enabled = false;
 
 	/*
 	 * If session is inactive we don't notify the tracer right away. We

src/bin/lttng-sessiond/cmd.cpp
@@ -921,7 +921,7 @@ close_sock:
 		 * since the relayd connection failed thus making any tracing or/and
 		 * streaming not usable.
 		 */
-		consumer->enabled = 0;
+		consumer->enabled = false;
 	}
 	(void) relayd_close(rsock);
 	free(rsock);
@@ -1149,7 +1149,7 @@ int start_kernel_session(struct ltt_kernel_session *ksess)
 	/* Quiescent wait after starting trace */
 	kernel_wait_quiescent();
 
-	ksess->active = 1;
+	ksess->active = true;
 
 	ret = LTTNG_OK;
 
@@ -1194,7 +1194,7 @@ int stop_kernel_session(struct ltt_kernel_session *ksess)
 		}
 	}
 
-	ksess->active = 0;
+	ksess->active = false;
 	if (error_occurred) {
 		ret = LTTNG_ERR_UNK;
 	} else {
@@ -2758,7 +2758,7 @@ int cmd_start_trace(struct ltt_session *session)
 		goto error;
 	}
 
-	session->active = 1;
+	session->active = true;
 	session->rotated_after_last_stop = false;
 	session->cleared_after_last_stop = false;
 	if (session->output_traces && !session->current_trace_chunk) {
@@ -2852,9 +2852,9 @@ int cmd_start_trace(struct ltt_session *session)
 error:
 	if (ret == LTTNG_OK) {
 		/* Flag this after a successful start. */
-		session->has_been_started |= 1;
+		session->has_been_started = true;
 	} else {
-		session->active = 0;
+		session->active = false;
 		/* Restore initial state on error. */
 		session->rotated_after_last_stop = session_rotated_after_last_stop;
 		session->cleared_after_last_stop = session_cleared_after_last_stop;
@@ -2900,7 +2900,7 @@ int cmd_stop_trace(struct ltt_session *session)
 
 	DBG("Completed stop session \"%s\" (id %" PRIu64 ")", session->name, session->id);
 	/* Flag inactive after a successful stop. */
-	session->active = 0;
+	session->active = false;
 	ret = LTTNG_OK;
 
 error:
@@ -3202,7 +3202,7 @@ cmd_create_session_from_descriptor(struct lttng_session_descriptor *descriptor,
 	if (ret_code != LTTNG_OK) {
 		goto end;
 	}
-	new_session->consumer->enabled = 1;
+	new_session->consumer->enabled = true;
 	ret_code = LTTNG_OK;
 end:
 	/* Release reference provided by the session_create function. */

src/bin/lttng-sessiond/consumer.cpp
@@ -519,7 +519,7 @@ struct consumer_output *consumer_create_output(enum consumer_dst_type type)
 	}
 
 	/* By default, consumer output is enabled */
-	output->enabled = 1;
+	output->enabled = true;
 	output->type = type;
 	output->net_seq_index = (uint64_t) -1ULL;
 	urcu_ref_init(&output->ref);

src/bin/lttng-sessiond/consumer.hpp
@@ -147,7 +147,7 @@ struct consumer_output {
 	struct urcu_ref ref;	/* Refcount */
 
 	/* If the consumer is enabled meaning that should be used */
-	unsigned int enabled;
+	bool enabled;
 	enum consumer_dst_type type;
 
 	/*

src/bin/lttng-sessiond/event.cpp
@@ -128,7 +128,7 @@ int event_kernel_enable_event(struct ltt_kernel_channel *kchan,
 		if (ret) {
 			goto end;
 		}
-	} else if (kevent->enabled == 0) {
+	} else if (!kevent->enabled) {
 		ret = kernel_enable_event(kevent);
 		if (ret < 0) {
 			ret = LTTNG_ERR_KERN_ENABLE_FAIL;
@@ -202,7 +202,7 @@ int event_ust_enable_tracepoint(struct ltt_ust_session *usess,
 		goto end;
 	}
 
-	uevent->enabled = 1;
+	uevent->enabled = true;
 	if (to_create) {
 		/* Add ltt ust event to channel */
 		add_unique_ust_event(uchan->events, uevent);
@@ -285,11 +285,11 @@ int event_ust_disable_tracepoint(struct ltt_ust_session *usess,
 		uevent = lttng::utils::container_of(node, &ltt_ust_event::node);
 		LTTNG_ASSERT(uevent);
 
-		if (uevent->enabled == 0) {
+		if (!uevent->enabled) {
 			/* It's already disabled so everything is OK */
 			goto next;
 		}
-		uevent->enabled = 0;
+		uevent->enabled = false;
 		DBG2("Event UST %s disabled in channel %s", uevent->attr.name, uchan->name);
 
 		if (!usess->active) {
@@ -331,7 +331,7 @@ int event_ust_disable_all_tracepoints(struct ltt_ust_session *usess, struct ltt_
 
 	/* Disabling existing events */
 	cds_lfht_for_each_entry (uchan->events->ht, &iter.iter, uevent, node.node) {
-		if (uevent->enabled == 1) {
+		if (uevent->enabled) {
 			ret = event_ust_disable_tracepoint(usess, uchan, uevent->attr.name);
 			if (ret < 0) {
 				error = LTTNG_ERR_UST_DISABLE_FAIL;
@@ -807,7 +807,7 @@ static int event_agent_disable_one(struct ltt_ust_session *usess,
 	 * Flag event that it's disabled so the shadow copy on the ust app side
 	 * will disable it if an application shows up.
 	 */
-	uevent->enabled = 0;
+	uevent->enabled = false;
 
 	ret = agent_disable_event(aevent, agt->domain);
 	if (ret != LTTNG_OK) {

src/bin/lttng-sessiond/kernel.cpp
@@ -691,7 +691,7 @@ int kernel_disable_channel(struct ltt_kernel_channel *chan)
 		goto error;
 	}
 
-	chan->enabled = 0;
+	chan->enabled = false;
 	DBG("Kernel channel %s disabled (fd: %d, key: %" PRIu64 ")",
 	    chan->channel->name,
 	    chan->fd,
@@ -718,7 +718,7 @@ int kernel_enable_channel(struct ltt_kernel_channel *chan)
 		goto error;
 	}
 
-	chan->enabled = 1;
+	chan->enabled = true;
 	DBG("Kernel channel %s enabled (fd: %d, key: %" PRIu64 ")",
 	    chan->channel->name,
 	    chan->fd,
@@ -752,7 +752,7 @@ int kernel_enable_event(struct ltt_kernel_event *event)
 		goto error;
 	}
 
-	event->enabled = 1;
+	event->enabled = true;
 	DBG("Kernel event %s enabled (fd: %d)", event->event->name, event->fd);
 
 	return 0;
@@ -778,7 +778,7 @@ int kernel_disable_event(struct ltt_kernel_event *event)
 		goto error;
 	}
 
-	event->enabled = 0;
+	event->enabled = false;
 	DBG("Kernel event %s disabled (fd: %d)", event->event->name, event->fd);
 
 	return 0;
@@ -808,7 +808,7 @@ static int kernel_disable_event_notifier_rule(struct ltt_kernel_event_notifier_r
 		goto error;
 	}
 
-	event->enabled = 0;
+	event->enabled = false;
 	DBG("Disabled kernel event notifier: fd = %d, token = %" PRIu64, event->fd, event->token);
 
 error:

src/bin/lttng-sessiond/session.hpp
@@ -125,12 +125,9 @@ struct ltt_session {
 	 */
 	bool has_user_specified_directory;
 	/* Did at least ONE start command has been triggered?. */
-	unsigned int has_been_started:1;
-	/*
-	 * Is the session active? Start trace command sets this to 1 and the stop
-	 * command reset it to 0.
-	 */
-	unsigned int active:1;
+	bool has_been_started;
+	/* Is the session active? */
+	bool active;
 
 	/* Snapshot representation in a session. */
 	struct snapshot snapshot;

commit ed0c6be4278ed647f5f7d682169557b72776484e

Fix: lttng-disable-event: erroneous error code used in logging
66cefebdc introduced `disable_ret` to hold the integral result of
lttng_disable_event_ext() instead of mixing enums and integers.

In doing so, one use site was left unmodified which resulted in the
wrong variable being used to log an error when the command failed.

This was spotted by clang:
  commands/disable_events.cpp:252:21: warning: result of comparison of
  constant -83 with expression of type 'enum   cmd_error_code' is always false [-Wtautological-constant-out-of-range-compare]
                                      command_ret == -LTTNG_ERR_NEED_CHANNEL_NAME ?

Signed-off-by: Jérémie Galarneau <jeremie.galarneau@efficios.com>
Change-Id: I282ebee0140f7044847aab6c43ba6bd0c185fc46

src/bin/lttng/commands/disable_events.cpp
@@ -248,8 +248,8 @@ static int disable_events(char *session_name, char *event_list)
 				ERR("%s of type %s : %s (channel %s, session %s)",
 				    event_name,
 				    print_event_type((lttng_event_type) opt_event_type),
-				    lttng_strerror(command_ret),
-				    command_ret == -LTTNG_ERR_NEED_CHANNEL_NAME ?
+				    lttng_strerror(disable_ret),
+				    disable_ret == -LTTNG_ERR_NEED_CHANNEL_NAME ?
 					    print_raw_channel_name(channel_name) :
 					    print_channel_name(channel_name),
 				    session_name);