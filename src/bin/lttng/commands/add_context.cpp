ca400a290a3e98e9f2578d4cbaede87019f2ee94
Jérémie Galarneau <jeremie.galarneau@efficios.com>
2023-07-27 12:26:42 -0400

    Fix: lttng-add-context: context type options possible null dereference
    Coverity reports that:
    ** CID 1518091:  Null pointer dereferences  (FORWARD_NULL)
    /src/bin/lttng/commands/add_context.cpp: 820 in destroy_ctx_type(<unnamed>::ctx_type *)(
    
    Free application context options only if type->opt isn't null.
    
    Signed-off-by: Jérémie Galarneau <jeremie.galarneau@efficios.com>
    Change-Id: Icc27d04480c4821ed33127f5baf293510cdb314e

--- src/bin/lttng/commands/add_context.cpp
+++ src/bin/lttng/commands/add_context.cpp
@@ -815,11 +815,11 @@ static void destroy_ctx_type(struct ctx_type *type)
 
 	if (type->opt) {
 		free(type->opt->symbol);
-	}
 
-	if (type->opt->ctx_type == CONTEXT_APP_CONTEXT) {
-		free(type->opt->u.app_ctx.ctx_name);
-		free(type->opt->u.app_ctx.provider_name);
+		if (type->opt->ctx_type == CONTEXT_APP_CONTEXT) {
+			free(type->opt->u.app_ctx.ctx_name);
+			free(type->opt->u.app_ctx.provider_name);
+		}
 	}
 
 	delete type->opt;
