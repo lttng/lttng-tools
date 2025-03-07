fcf53150987ae07332264a08113b9db81b3195fb
Kienan Stewart <kstewart@efficios.com>
2025-03-07 11:21:10 -0500

    common: Add stub lttng-relayd tracepoint provider
    Signed-off-by: Kienan Stewart <kstewart@efficios.com>
    Change-Id: Ia7c2e93ddce0babcff4713e0179c7a3067e23fca
    Signed-off-by: Jérémie Galarneau <jeremie.galarneau@efficios.com>

---
+++ src/lib/tpp/relayd.cpp
@@ -0,0 +1,8 @@
+/*
+ * SPDX-License-Identifier: GPL-2.0-only
+ * SPDX-FileCopyrightText: 2025 Kienan Stewart <kstewart@efficios.com>
+ */
+
+#define LTTNG_UST_TRACEPOINT_CREATE_PROBES
+
+#include "relayd.hpp"
