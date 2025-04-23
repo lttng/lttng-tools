0f76b4e7f29fa812838f26a183bd31c44c004d70
Philippe Proulx <eeppeliteloop@gmail.com>
2025-04-23 15:27:06 -0400

    Document `include/lttng/event-rule/event-rule.h` with Doxygen
    Signed-off-by: Philippe Proulx <eeppeliteloop@gmail.com>
    Change-Id: I8e20e8ad8f2c1b1f92928c7ce7dd652a70e5a632
    Signed-off-by: Jérémie Galarneau <jeremie.galarneau@efficios.com>

--- include/lttng/event-rule/event-rule.h
+++ include/lttng/event-rule/event-rule.h
@@ -14,48 +14,124 @@
 extern "C" {
 #endif
 
+/*!
+@addtogroup api_er
+@{
+*/
+
+/*!
+@struct lttng_event_rule
+
+@brief
+    Event rule (opaque type).
+*/
 struct lttng_event_rule;
 
+/*!
+@brief
+    \ref api-er-conds-inst-pt-type "Instrumentation point type condition"
+    of an event rule.
+*/
 enum lttng_event_rule_type {
+	/// Unknown (error).
 	LTTNG_EVENT_RULE_TYPE_UNKNOWN = -1,
+
+	/// Match Linux kernel system calls.
 	LTTNG_EVENT_RULE_TYPE_KERNEL_SYSCALL = 0,
+
+	/*!
+	Match Linux
+	<a href="https://www.kernel.org/doc/html/latest/trace/kprobes.html">kprobes</a>.
+	*/
 	LTTNG_EVENT_RULE_TYPE_KERNEL_KPROBE = 1,
+
+	/// Match LTTng kernel tracepoints.
 	LTTNG_EVENT_RULE_TYPE_KERNEL_TRACEPOINT = 2,
+
+	/*!
+	Match Linux
+	<a href="https://lwn.net/Articles/499190/">user space probes</a>.
+	*/
 	LTTNG_EVENT_RULE_TYPE_KERNEL_UPROBE = 3,
+
+	/// Match user space tracepoints.
 	LTTNG_EVENT_RULE_TYPE_USER_TRACEPOINT = 4,
+
+	/// Match \lt_jul (JUL) logging statements.
 	LTTNG_EVENT_RULE_TYPE_JUL_LOGGING = 5,
+
+	/*!
+	Match
+	<a href="https://logging.apache.org/log4j/1.x/">\lt_log4j1</a>
+	logging statements.
+	*/
 	LTTNG_EVENT_RULE_TYPE_LOG4J_LOGGING = 6,
+
+	/// Match Python logging statements.
 	LTTNG_EVENT_RULE_TYPE_PYTHON_LOGGING = 7,
+
+	/*!
+	Match
+	<a href="https://logging.apache.org/log4j/2.x/">\lt_log4j2</a>
+	logging statements.
+	*/
 	LTTNG_EVENT_RULE_TYPE_LOG4J2_LOGGING = 8,
 };
 
+/*!
+@brief
+    Return type of event rule API functions.
+*/
 enum lttng_event_rule_status {
+	/// Success.
 	LTTNG_EVENT_RULE_STATUS_OK = 0,
+
+	/// Error.
 	LTTNG_EVENT_RULE_STATUS_ERROR = -1,
+
+	/* Unused for the moment */
 	LTTNG_EVENT_RULE_STATUS_UNKNOWN = -2,
+
+	/// Unsatisfied precondition.
 	LTTNG_EVENT_RULE_STATUS_INVALID = -3,
+
+	/// Not set.
 	LTTNG_EVENT_RULE_STATUS_UNSET = -4,
+
+	/// Unsupported.
 	LTTNG_EVENT_RULE_STATUS_UNSUPPORTED = -5,
 };
 
-/**
- * An event rule describes a set of criteria to be used as a discriminant in
- * regards to a set of events.
- */
+/*!
+@brief
+    Returns the type of the event rule \lt_p{rule}.
 
-/*
- * Get the event rule type.
- *
- * Returns the type of an event rule on success, LTTNG_EVENT_RULE_UNKNOWN on
- * error.
- */
+@param[in] rule
+    Event rule of which to get the type.
+
+@returns
+    Type of \lt_p{rule}.
+
+@pre
+    @lt_pre_not_null{rule}
+*/
 LTTNG_EXPORT extern enum lttng_event_rule_type
-lttng_event_rule_get_type(const struct lttng_event_rule *event_rule);
+lttng_event_rule_get_type(const struct lttng_event_rule *rule);
 
-/*
- * Destroy an event rule object.
- */
+/*!
+@brief
+    Destroys the event rule \lt_p{rule}.
+
+@param[in] rule
+    @parblock
+    Event rule to destroy.
+
+    May be \c NULL.
+    @endparblock
+*/
 LTTNG_EXPORT extern void lttng_event_rule_destroy(struct lttng_event_rule *rule);
+
+/// @}
 
 #ifdef __cplusplus
 }
