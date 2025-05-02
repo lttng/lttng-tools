6dde53e19139cde9fd358fcfa53fda5d727ba809
Philippe Proulx <eeppeliteloop@gmail.com>
2025-05-02 18:42:59 -0400

    include/lttng/event-rule/kernel-kprobe.h: document with Doxygen
    Signed-off-by: Philippe Proulx <eeppeliteloop@gmail.com>
    Change-Id: Ifeb0c285269db515c25f1e7b41b8ed7b5fa98b2f
    Signed-off-by: Jérémie Galarneau <jeremie.galarneau@efficios.com>

--- include/lttng/event-rule/kernel-kprobe.h
+++ include/lttng/event-rule/kernel-kprobe.h
@@ -15,60 +15,186 @@
 extern "C" {
 #endif
 
+/*!
+@addtogroup api_kprobe_er
+@{
+*/
+
 struct lttng_kernel_probe_location;
 
-/*
- * Create a newly allocated kernel kprobe event rule.
- *
- * The location is copied internally.
- *
- * Returns a new event rule on success, NULL on failure. The returned event rule
- * must be destroyed using lttng_event_rule_destroy().
- */
+/*!
+@brief
+    Creates a default Linux kprobe event rule for the kprobe
+    location \lt_p{location}.
+
+On success, the returned event rule has the following
+condition:
+
+<table>
+  <tr>
+    <th>Condition
+    <th>Value
+  <tr>
+    <td>\ref api-er-conds-inst-pt-type "Instrumentation point type"
+    <td>#LTTNG_EVENT_RULE_TYPE_KERNEL_KPROBE using \lt_p{location}
+</table>
+
+The initial event name of the returned event rule is, depending
+on the type \lt_p{location}:
+
+<dl>
+  <dt>#LTTNG_KERNEL_PROBE_LOCATION_TYPE_SYMBOL_OFFSET
+  <dd>
+    The
+    \link lttng_kernel_probe_location_symbol_get_name() symbol name\endlink
+    of \lt_p{location} followed with, if not zero,
+    the
+    \link lttng_kernel_probe_location_symbol_get_offset() offset\endlink
+    from symbol of \lt_p{location} (hexadecimal with the <code>0x</code>
+    prefix).
+
+    Examples:
+
+    - <code>copy_from_user</code>
+    - <code>schedule+0x4c0</code>
+
+  <dt>#LTTNG_KERNEL_PROBE_LOCATION_TYPE_ADDRESS
+  <dd>
+    The
+    \link lttng_kernel_probe_location_address_get_address() address\endlink
+    within the kernel of \lt_p{location}
+    (hexadecimal with the <code>0x</code> prefix).
+
+    Example: <code>0xffffffff814abc30</code>.
+</dl>
+
+Override this initial event name with
+lttng_event_rule_kernel_kprobe_set_event_name().
+
+@param[in] location
+    Location of kprobe events to match (copied).
+
+@returns
+    @parblock
+    Linux kprobe event rule on success, or \c NULL on error.
+
+    Destroy the returned event rule with
+    lttng_event_rule_destroy().
+    @endparblock
+*/
 LTTNG_EXPORT extern struct lttng_event_rule *
 lttng_event_rule_kernel_kprobe_create(const struct lttng_kernel_probe_location *location);
 
-/*
- * Get the kernel probe location of a kernel kprobe event rule.
- *
- * The caller does not assume the ownership of the returned location.
- * The location shall only be used for the duration of the event
- * rule's lifetime, or before a different location is set.
- *
- * Returns LTTNG_EVENT_RULE_STATUS_OK and a pointer to the event rule's location
- * on success, LTTNG_EVENT_RULE_STATUS_INVALID if an invalid parameter is
- * passed, or LTTNG_EVENT_RULE_STATUS_UNSET if a location was not set prior to
- * this call.
- */
+/*!
+@brief
+    Sets \lt_p{*location} to the kprobe location of the
+    Linux kprobe event rule \lt_p{rule}, a copy of the
+    kprobe location you passed when you called
+    lttng_event_rule_kernel_kprobe_create().
+
+See the \ref api-er-conds-inst-pt-type "instrumentation point type"
+condition.
+
+@param[in] rule
+    Linux kprobe event rule of which to get the kprobe location.
+@param[out] location
+    @parblock
+    <strong>On success</strong>, this function sets \lt_p{*location}
+    to the kprobe location of \lt_p{rule}.
+
+    \lt_p{rule} owns \lt_p{*location}.
+
+    \lt_p{*location} remains valid until the next
+    function call with \lt_p{rule}.
+    @endparblock
+
+@retval #LTTNG_EVENT_RULE_STATUS_OK
+    Success.
+@retval #LTTNG_EVENT_RULE_STATUS_INVALID
+    Unsatisfied precondition.
+
+@pre
+    @lt_pre_not_null{rule}
+    @lt_pre_has_type{rule,LTTNG_EVENT_RULE_TYPE_KERNEL_KPROBE}
+    @lt_pre_not_null{location}
+*/
 LTTNG_EXPORT extern enum lttng_event_rule_status
 lttng_event_rule_kernel_kprobe_get_location(const struct lttng_event_rule *rule,
 					    const struct lttng_kernel_probe_location **location);
 
-/*
- * Set the name of a kernel kprobe event rule.
- *
- * The name is copied internally.
- *
- * Returns LTTNG_EVENT_RULE_STATUS_OK on success, LTTNG_EVENT_RULE_STATUS_INVALID
- * if invalid parameters are passed.
- */
+/*!
+@brief
+    Sets the event name of the Linux kprobe
+    event rule \lt_p{rule} to \lt_p{name}.
+
+\lt_p{name} is the name of the event which LTTng creates when
+the Linux kprobe of \lt_p{rule}
+(see lttng_event_rule_kernel_kprobe_get_location()) is executed.
+
+@param[in] rule
+    Linux kprobe event rule of which to set the event name.
+@param[in] name
+    Event name of \lt_p{rule} (copied).
+
+@retval #LTTNG_EVENT_RULE_STATUS_OK
+    Success.
+@retval #LTTNG_EVENT_RULE_STATUS_INVALID
+    Unsatisfied precondition.
+
+@pre
+    @lt_pre_not_null{rule}
+    @lt_pre_has_type{rule,LTTNG_EVENT_RULE_TYPE_KERNEL_KPROBE}
+    @lt_pre_not_null{name}
+    @lt_pre_not_empty{name}
+
+@sa lttng_event_rule_kernel_kprobe_get_event_name() --
+    Get the event name of a Linux kprobe event rule.
+*/
 LTTNG_EXPORT extern enum lttng_event_rule_status
 lttng_event_rule_kernel_kprobe_set_event_name(struct lttng_event_rule *rule, const char *name);
 
-/*
- * Get the name of a kernel kprobe event rule.
- *
- * The caller does not assume the ownership of the returned name.
- * The name shall only only be used for the duration of the event
- * rule's lifetime, or before a different name is set.
- *
- * Returns LTTNG_EVENT_RULE_STATUS_OK and a pointer to the event rule's name on
- * success, LTTNG_EVENT_RULE_STATUS_INVALID if an invalid parameter is passed,
- * or LTTNG_EVENT_RULE_STATUS_UNSET if a name was not set prior to this call.
- */
+/*!
+@brief
+    Sets \lt_p{*name} to the event name of the
+    Linux kprobe event rule \lt_p{rule}.
+
+\lt_p{name} is the name of the event which LTTng creates when
+the Linux kprobe of \lt_p{rule}
+(see lttng_event_rule_kernel_kprobe_get_location()) is executed.
+
+@param[in] rule
+    Linux kprobe event rule of which to get the event name.
+@param[out] name
+    @parblock
+    <strong>On success</strong>, this function sets \lt_p{*name}
+    to the event name of \lt_p{rule}.
+
+    \lt_p{rule} owns \lt_p{*name}.
+
+    \lt_p{*name} remains valid until the next
+    function call with \lt_p{rule}.
+    @endparblock
+
+@retval #LTTNG_EVENT_RULE_STATUS_OK
+    Success.
+@retval #LTTNG_EVENT_RULE_STATUS_UNSET
+    \lt_p{rule} has no event name.
+@retval #LTTNG_EVENT_RULE_STATUS_INVALID
+    Unsatisfied precondition.
+
+@pre
+    @lt_pre_not_null{rule}
+    @lt_pre_has_type{rule,LTTNG_EVENT_RULE_TYPE_KERNEL_KPROBE}
+    @lt_pre_not_null{name}
+
+@sa lttng_event_rule_kernel_kprobe_set_event_name() --
+    Set the event name of a Linux kprobe event rule.
+*/
 LTTNG_EXPORT extern enum lttng_event_rule_status
 lttng_event_rule_kernel_kprobe_get_event_name(const struct lttng_event_rule *rule,
 					      const char **name);
+
+/// @}
 
 #ifdef __cplusplus
 }
