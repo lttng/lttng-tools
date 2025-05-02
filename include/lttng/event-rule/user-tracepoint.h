778081c9cc8b6ea3ec5ca600d1fba65222a1f3a4
Philippe Proulx <eeppeliteloop@gmail.com>
2025-05-02 13:06:08 -0400

    include/lttng/event-rule/user-tracepoint.h: document with Doxygen
    Signed-off-by: Philippe Proulx <eeppeliteloop@gmail.com>
    Change-Id: Ic1268ac435662d5ae67a9c006f3d389ddaf2c714
    Signed-off-by: Jérémie Galarneau <jeremie.galarneau@efficios.com>

--- include/lttng/event-rule/user-tracepoint.h
+++ include/lttng/event-rule/user-tracepoint.h
@@ -18,132 +18,403 @@
 extern "C" {
 #endif
 
-/*
- * Create a newly allocated user tracepoint event rule.
- *
- * The default pattern is '*'.
- *
- * Returns a new event rule on success, NULL on failure. This event rule must be
- * destroyed using lttng_event_rule_destroy().
- */
+/*!
+@addtogroup api_user_tp_er
+@{
+*/
+
+/*!
+@brief
+    Creates a default LTTng user space tracepoint event rule.
+
+On success, the returned event rule has the following
+initial conditions:
+
+<table>
+  <tr>
+    <th>Condition
+    <th>Value
+    <th>Setter(s)
+  <tr>
+    <td>\ref api-er-conds-inst-pt-type "Instrumentation point type"
+    <td>#LTTNG_EVENT_RULE_TYPE_USER_TRACEPOINT
+    <td>Not applicable
+  <tr>
+    <td>\ref api-er-conds-event-name "Event name"
+    <td>Any name
+    <td>
+      - lttng_event_rule_user_tracepoint_set_name_pattern()
+      - lttng_event_rule_user_tracepoint_add_name_pattern_exclusion()
+  <tr>
+    <td>\ref api-er-conds-ll "Instrumentation point log level"
+    <td>Any log level
+    <td>lttng_event_rule_user_tracepoint_set_log_level_rule()
+  <tr>
+    <td>\ref api-er-conds-filter "Event payload and context filter"
+    <td>No filter
+    <td>lttng_event_rule_user_tracepoint_set_filter()
+</table>
+
+@returns
+    @parblock
+    LTTng user space tracepoint event rule on success,
+    or \c NULL on error.
+
+    Destroy the returned event rule with lttng_event_rule_destroy().
+    @endparblock
+*/
 LTTNG_EXPORT extern struct lttng_event_rule *lttng_event_rule_user_tracepoint_create(void);
 
-/*
- * Set the name pattern of a user tracepoint event rule.
- *
- * Pattern can contain wildcard '*'. See man lttng-enable-event.
- *
- * Return LTTNG_EVENT_RULE_STATUS_OK on success, LTTNG_EVENT_RULE_STATUS_INVALID
- * if invalid parameters are passed.
- */
+/*!
+@brief
+    Sets the event name pattern of the LTTng user space tracepoint
+    event rule \lt_p{rule} to \lt_p{pattern}.
+
+See the \ref api-er-conds-event-name "event name" condition.
+
+@param[in] rule
+    LTTng user space tracepoint event rule of which to set the
+    event name pattern.
+@param[in] pattern
+    New event name pattern of \lt_p{rule} (copied).
+
+@retval #LTTNG_EVENT_RULE_STATUS_OK
+    Success.
+@retval #LTTNG_EVENT_RULE_STATUS_INVALID
+    Unsatisfied precondition.
+
+@pre
+    @lt_pre_not_null{rule}
+    @lt_pre_has_type{rule,LTTNG_EVENT_RULE_TYPE_USER_TRACEPOINT}
+    @lt_pre_not_null{pattern}
+    @lt_pre_not_empty{pattern}
+
+@sa lttng_event_rule_user_tracepoint_get_name_pattern() --
+    Get the event name pattern of an LTTng user space tracepoint
+    event rule.
+@sa lttng_event_rule_user_tracepoint_add_name_pattern_exclusion() --
+    Add an event name exclusion pattern to an LTTng user space
+    tracepoint event rule.
+*/
 LTTNG_EXPORT extern enum lttng_event_rule_status
 lttng_event_rule_user_tracepoint_set_name_pattern(struct lttng_event_rule *rule,
 						  const char *pattern);
 
-/*
- * Get the name pattern of a user tracepoint event rule.
- *
- * The caller does not assume the ownership of the returned pattern. The
- * pattern shall only only be used for the duration of the event rule's
- * lifetime, or before a different pattern is set.
- *
- * Returns LTTNG_EVENT_RULE_STATUS_OK and a pointer to the event rule's pattern
- * on success, LTTNG_EVENT_RULE_STATUS_INVALID if an invalid
- * parameter is passed, or LTTNG_EVENT_RULE_STATUS_UNSET if a pattern
- * was not set prior to this call.
- */
+/*!
+@brief
+    Sets \lt_p{*pattern} to the event name pattern of the
+    LTTng user space tracepoint event rule \lt_p{rule}.
+
+See the \ref api-er-conds-event-name "event name" condition.
+
+@param[in] rule
+    LTTng user space tracepoint event rule of which to get the
+    event name pattern.
+@param[out] pattern
+    @parblock
+    <strong>On success</strong>, this function sets \lt_p{*pattern}
+    to the event name pattern of \lt_p{rule}.
+
+    \lt_p{rule} owns \lt_p{*pattern}.
+
+    \lt_p{*pattern} remains valid until the next
+    function call with \lt_p{rule}.
+    @endparblock
+
+@retval #LTTNG_EVENT_RULE_STATUS_OK
+    Success.
+@retval #LTTNG_EVENT_RULE_STATUS_UNSET
+    \lt_p{rule} has no event name pattern, which corresponds to
+    the pattern <code>*</code> (any event name).
+@retval #LTTNG_EVENT_RULE_STATUS_INVALID
+    Unsatisfied precondition.
+
+@pre
+    @lt_pre_not_null{rule}
+    @lt_pre_has_type{rule,LTTNG_EVENT_RULE_TYPE_USER_TRACEPOINT}
+    @lt_pre_not_null{pattern}
+
+@sa lttng_event_rule_user_tracepoint_set_name_pattern() --
+    Set the event name pattern of an LTTng user space tracepoint
+    event rule.
+*/
 LTTNG_EXPORT extern enum lttng_event_rule_status
 lttng_event_rule_user_tracepoint_get_name_pattern(const struct lttng_event_rule *rule,
 						  const char **pattern);
 
-/*
- * Set the filter expression of a user tracepoint event rule.
- *
- * The expression is copied internally.
- *
- * Return LTTNG_EVENT_RULE_STATUS_OK on success, LTTNG_EVENT_RULE_STATUS_INVALID
- * if invalid parameters are passed.
- */
-LTTNG_EXPORT extern enum lttng_event_rule_status
-lttng_event_rule_user_tracepoint_set_filter(struct lttng_event_rule *rule, const char *expression);
+/*!
+@brief
+    Sets the event payload and context filter of the LTTng user space
+    tracepoint event rule \lt_p{rule} to \lt_p{filter_expr}.
 
-/*
- * Get the filter expression of a user tracepoint event rule.
- *
- * The caller does not assume the ownership of the returned filter expression.
- * The filter expression shall only only be used for the duration of the event
- * rule's lifetime, or before a different filter expression is set.
- *
- * Returns LTTNG_EVENT_RULE_STATUS_OK and a pointer to the event rule's filter
- * expression on success, LTTNG_EVENT_RULE_STATUS_INVALID if an invalid
- * parameter is passed, or LTTNG_EVENT_RULE_STATUS_UNSET if a filter expression
- * was not set prior to this call.
- */
+See the \ref api-er-conds-filter "event payload and context filter"
+condition.
+
+@param[in] rule
+    LTTng user space tracepoint event rule of which to set the
+    event payload and context filter.
+@param[in] filter_expr
+    Event payload and context filter expression of \lt_p{rule} (copied).
+
+@retval #LTTNG_EVENT_RULE_STATUS_OK
+    Success.
+@retval #LTTNG_EVENT_RULE_STATUS_INVALID
+    Unsatisfied precondition.
+
+@pre
+    @lt_pre_not_null{rule}
+    @lt_pre_has_type{rule,LTTNG_EVENT_RULE_TYPE_USER_TRACEPOINT}
+    @lt_pre_not_null{filter_expr}
+    - \lt_p{filter_expr} is a valid filter expression (see
+      the \ref api-er-conds-filter "event payload and context filter"
+      condition).
+
+@sa lttng_event_rule_user_tracepoint_get_filter() --
+    Get the event payload and context filter of an LTTng
+    user space tracepoint event rule.
+*/
+LTTNG_EXPORT extern enum lttng_event_rule_status
+lttng_event_rule_user_tracepoint_set_filter(struct lttng_event_rule *rule, const char *filter_expr);
+
+/*!
+@brief
+    Sets \lt_p{*filter_expr} to the event payload and context filter
+    expression of the LTTng user space tracepoint
+    event rule \lt_p{rule}.
+
+See the \ref api-er-conds-filter "event payload and context filter"
+condition.
+
+@param[in] rule
+    LTTng user space tracepoint event rule of which to get the
+    event payload and context filter.
+@param[out] filter_expr
+    @parblock
+    <strong>On success</strong>, this function sets \lt_p{*filter_expr}
+    to the event payload and context filter expression of \lt_p{rule}.
+
+    \lt_p{rule} owns \lt_p{*filter_expr}.
+
+    \lt_p{*filter_expr} remains valid until the next
+    function call with \lt_p{rule}.
+    @endparblock
+
+@retval #LTTNG_EVENT_RULE_STATUS_OK
+    Success.
+@retval #LTTNG_EVENT_RULE_STATUS_UNSET
+    \lt_p{rule} has no event payload and context filter, which
+    corresponds to the filter expression <code>1</code>.
+@retval #LTTNG_EVENT_RULE_STATUS_INVALID
+    Unsatisfied precondition.
+
+@pre
+    @lt_pre_not_null{rule}
+    @lt_pre_has_type{rule,LTTNG_EVENT_RULE_TYPE_USER_TRACEPOINT}
+    @lt_pre_not_null{filter_expr}
+
+@sa lttng_event_rule_user_tracepoint_set_filter() --
+    Set the event payload and context filter of an LTTng
+    user space tracepoint event rule.
+*/
 LTTNG_EXPORT extern enum lttng_event_rule_status
 lttng_event_rule_user_tracepoint_get_filter(const struct lttng_event_rule *rule,
-					    const char **expression);
+					    const char **filter_expr);
 
-/*
- * Set the log level rule of a user tracepoint event rule.
- *
- * The log level rule is copied internally.
- *
- * Return LTTNG_EVENT_RULE_STATUS_OK on success, LTTNG_EVENT_RULE_STATUS_INVALID
- * if invalid parameters are passed.
- */
+/*!
+@brief
+    Sets the instrumentation point log level rule of the LTTng user
+    space tracepoint event rule
+    \lt_p{event_rule} to \lt_p{log_level_rule}.
+
+See the \ref api-er-conds-ll "instrumentation point log level"
+condition.
+
+@param[in] event_rule
+    LTTng user space tracepoint event rule of which to set the
+    instrumentation point log level rule.
+@param[in] log_level_rule
+    Instrumentation point log level rule of \lt_p{event_rule} (copied).
+
+@retval #LTTNG_EVENT_RULE_STATUS_OK
+    Success.
+@retval #LTTNG_EVENT_RULE_STATUS_INVALID
+    Unsatisfied precondition.
+
+@pre
+    @lt_pre_not_null{event_rule}
+    @lt_pre_has_type{event_rule,LTTNG_EVENT_RULE_TYPE_USER_TRACEPOINT}
+    @lt_pre_not_null{log_level_rule}
+
+@sa lttng_event_rule_user_tracepoint_get_log_level_rule() --
+    Get the instrumentation point log level rule of an LTTng user space
+    tracepoint event rule.
+*/
 LTTNG_EXPORT extern enum lttng_event_rule_status
 lttng_event_rule_user_tracepoint_set_log_level_rule(
-	struct lttng_event_rule *rule, const struct lttng_log_level_rule *log_level_rule);
+	struct lttng_event_rule *event_rule, const struct lttng_log_level_rule *log_level_rule);
 
-/*
- * Get the log level rule of a user tracepoint event rule.
- *
- * The caller does not assume the ownership of the returned log level rule. The
- * log level rule shall only only be used for the duration of the event rule's
- * lifetime, or before a different log level rule is set.
- *
- * Returns LTTNG_EVENT_RULE_STATUS_OK and sets the log level rule output
- * parameter on success, LTTNG_EVENT_RULE_STATUS_INVALID if an invalid parameter
- * is passed, or LTTNG_EVENT_RULE_STATUS_UNSET if a log level rule was not set prior
- * to this call.
- */
+/*!
+@brief
+    Sets \lt_p{*log_level_rule} to the instrumentation point log level
+    rule of the LTTng user space tracepoint event rule
+    \lt_p{event_rule}.
+
+See the \ref api-er-conds-ll "instrumentation point log level"
+condition.
+
+@param[in] event_rule
+    LTTng user space tracepoint event rule of which to get the
+    instrumentation point log level rule.
+@param[out] log_level_rule
+    @parblock
+    <strong>On success</strong>, this function sets
+    \lt_p{*log_level_rule} to the instrumentation point log level
+    rule of \lt_p{event_rule}.
+
+    \lt_p{event_rule} owns \lt_p{*log_level_rule}.
+
+    \lt_p{*log_level_rule} remains valid until the next
+    function call with \lt_p{event_rule}.
+    @endparblock
+
+@retval #LTTNG_EVENT_RULE_STATUS_OK
+    Success.
+@retval #LTTNG_EVENT_RULE_STATUS_UNSET
+    \lt_p{event_rule} has no instrumentation point log level rule.
+@retval #LTTNG_EVENT_RULE_STATUS_INVALID
+    Unsatisfied precondition.
+
+@pre
+    @lt_pre_not_null{rule}
+    @lt_pre_has_type{rule,LTTNG_EVENT_RULE_TYPE_USER_TRACEPOINT}
+    @lt_pre_not_null{log_level_rule}
+
+@sa lttng_event_rule_user_tracepoint_set_log_level_rule() --
+    Set the instrumentation point log level rule of an LTTng user space
+    tracepoint event rule.
+*/
 LTTNG_EXPORT extern enum lttng_event_rule_status
 lttng_event_rule_user_tracepoint_get_log_level_rule(
-	const struct lttng_event_rule *rule, const struct lttng_log_level_rule **log_level_rule);
+	const struct lttng_event_rule *event_rule,
+	const struct lttng_log_level_rule **log_level_rule);
 
-/*
- * Add a name pattern exclusion to the set of name pattern exclusion of an event rule.
- *
- * The passed exclusion will be copied to the event_rule.
- *
- * Returns LTTNG_EVENT_RULE_STATUS_OK on success,
- * LTTNG_EVENT_RULE_STATUS_INVALID if invalid parameters are passed.
- */
+/*!
+@brief
+    Adds the event name exclusion pattern \lt_p{exclusion_pattern} to
+    the LTTng user space tracepoint event rule \lt_p{rule}.
+
+See the \ref api-er-conds-event-name "event name" condition.
+
+@param[in] rule
+    LTTng user space tracepoint event rule to which to add the
+    event name exclusion pattern
+    \lt_p{exclusion_pattern}.
+@param[in] exclusion_pattern
+    Event name exclusion pattern to add to \lt_p{rule} (copied).
+
+@retval #LTTNG_EVENT_RULE_STATUS_OK
+    Success.
+@retval #LTTNG_EVENT_RULE_STATUS_INVALID
+    Unsatisfied precondition.
+
+@pre
+    @lt_pre_not_null{rule}
+    @lt_pre_has_type{rule,LTTNG_EVENT_RULE_TYPE_USER_TRACEPOINT}
+    @lt_pre_not_null{exclusion_pattern}
+
+@sa lttng_event_rule_user_tracepoint_set_name_pattern() --
+    Set the event name pattern of an LTTng user space tracepoint
+    event rule.
+@sa lttng_event_rule_user_tracepoint_get_name_pattern_exclusion_at_index() --
+    Get an event name exclusion pattern of an LTTng
+    user space tracepoint event rule by index.
+*/
 LTTNG_EXPORT extern enum lttng_event_rule_status
 lttng_event_rule_user_tracepoint_add_name_pattern_exclusion(struct lttng_event_rule *rule,
-							    const char *exclusion);
+							    const char *exclusion_pattern);
 
-/*
- * Get the name pattern exclusions property count of an event rule.
- *
- * Returns LTTNG_EVENT_RULE_STATUS_OK and sets the count output parameter
- * on success, LTTNG_EVENT_RULE_STATUS_INVALID if an invalid parameter is
- * passed.
- */
+/*!
+@brief
+    Sets \lt_p{*count} to the number of event name exclusion patterns
+    of the LTTng user space tracepoint event rule \lt_p{rule}.
+
+See the \ref api-er-conds-event-name "event name" condition.
+
+@param[in] rule
+    LTTng user space tracepoint event rule of which to get
+    the number of event name exclusion patterns.
+@param[out] count
+    <strong>On success</strong>, this function sets \lt_p{*count}
+    to the number of event name exclusion patterns of \lt_p{rule}.
+
+@retval #LTTNG_EVENT_RULE_STATUS_OK
+    Success.
+@retval #LTTNG_EVENT_RULE_STATUS_INVALID
+    Unsatisfied precondition.
+
+@pre
+    @lt_pre_not_null{rule}
+    @lt_pre_has_type{rule,LTTNG_EVENT_RULE_TYPE_USER_TRACEPOINT}
+    @lt_pre_not_null{count}
+
+@sa lttng_event_rule_user_tracepoint_get_name_pattern_exclusion_at_index() --
+    Get an event name exclusion pattern of an LTTng
+    user space tracepoint event rule by index.
+*/
 LTTNG_EXPORT extern enum lttng_event_rule_status
 lttng_event_rule_user_tracepoint_get_name_pattern_exclusion_count(
 	const struct lttng_event_rule *rule, unsigned int *count);
 
-/*
- * Get the pattern name exclusion at the given index.
- *
- * Returns LTTNG_EVENT_RULE_STATUS_OK and sets the exclusion output parameter
- * on success, LTTNG_EVENT_RULE_STATUS_INVALID if an invalid parameter is
- * passed.
- */
+/*!
+@brief
+    Sets \lt_p{*exclusion_pattern} to the event name exclusion pattern
+    of the LTTng user space tracepoint event rule \lt_p{rule}
+    at the index \lt_p{index}.
+
+See the \ref api-er-conds-event-name "event name" condition.
+
+@param[in] rule
+    LTTng user space tracepoint event rule of which to get the
+    event name exclusion pattern at the index \lt_p{index}.
+@param[in] index
+    Index of the event name exclusion pattern to get from \lt_p{rule}.
+@param[in] exclusion_pattern
+    @parblock
+    <strong>On success</strong>, this function sets
+    \lt_p{*exclusion_pattern} to the event name exclusion pattern
+    of \lt_p{rule} at the index \lt_p{index}.
+
+    \lt_p{event_rule} owns \lt_p{*exclusion_pattern}.
+
+    \lt_p{*exclusion_pattern} remains valid until the next
+    function call with \lt_p{event_rule}.
+    @endparblock
+
+@retval #LTTNG_EVENT_RULE_STATUS_OK
+    Success.
+@retval #LTTNG_EVENT_RULE_STATUS_INVALID
+    Unsatisfied precondition.
+
+@pre
+    @lt_pre_not_null{rule}
+    @lt_pre_has_type{rule,LTTNG_EVENT_RULE_TYPE_USER_TRACEPOINT}
+    - \lt_p{index} is less than the number of event name exclusion
+      patterns of \lt_p{rule} (as given by
+      lttng_event_rule_user_tracepoint_get_name_pattern_exclusion_count()).
+    @lt_pre_not_null{exclusion_pattern}
+
+@sa lttng_event_rule_user_tracepoint_get_name_pattern_exclusion_count() --
+    Get the number of event name exclusion patterns of an LTTng
+    user space tracepoint event rule.
+@sa lttng_event_rule_user_tracepoint_add_name_pattern_exclusion() --
+    Adds an event name exclusion pattern to an LTTng
+    user space tracepoint event rule.
+*/
 LTTNG_EXPORT extern enum lttng_event_rule_status
 lttng_event_rule_user_tracepoint_get_name_pattern_exclusion_at_index(
-	const struct lttng_event_rule *rule, unsigned int index, const char **exclusion);
+	const struct lttng_event_rule *rule, unsigned int index, const char **exclusion_pattern);
+
+/// @}
 
 #ifdef __cplusplus
 }
