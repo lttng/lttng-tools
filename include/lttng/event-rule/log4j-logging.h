321caf91abbda2b1749d023415da4a8151f9d23b
Philippe Proulx <eeppeliteloop@gmail.com>
2025-05-02 19:32:47 -0400

    include/lttng/event-rule/log4j-logging.h: document with Doxygen
    Signed-off-by: Philippe Proulx <eeppeliteloop@gmail.com>
    Change-Id: Ice8b46d5d079f24d3ecc19e41c400233efbc55f5
    Signed-off-by: Jérémie Galarneau <jeremie.galarneau@efficios.com>

--- include/lttng/event-rule/log4j-logging.h
+++ include/lttng/event-rule/log4j-logging.h
@@ -17,95 +17,266 @@
 extern "C" {
 #endif
 
-/*
- * Create a newly allocated log4j logging event rule.
- *
- * The default name pattern is '*'.
- *
- * Returns a new event rule on success, NULL on failure. This event rule must be
- * destroyed using lttng_event_rule_destroy().
- */
+/*!
+@addtogroup api_log4j1_er
+@{
+*/
+
+/*!
+@brief
+    Creates a default \lt_log4j1 event rule.
+
+On success, the returned event rule has the following
+initial conditions:
+
+<table>
+  <tr>
+    <th>Condition
+    <th>Value
+    <th>Setter
+  <tr>
+    <td>\ref api-er-conds-inst-pt-type "Instrumentation point type"
+    <td>#LTTNG_EVENT_RULE_TYPE_LOG4J_LOGGING
+    <td>Not applicable
+  <tr>
+    <td>\ref api-er-conds-event-name "Event name"
+    <td>Any name
+    <td>lttng_event_rule_log4j_logging_set_name_pattern()
+  <tr>
+    <td>\ref api-er-conds-ll "Instrumentation point log level"
+    <td>Any log level
+    <td>lttng_event_rule_log4j_logging_set_log_level_rule()
+  <tr>
+    <td>\ref api-er-conds-filter "Event payload and context filter"
+    <td>No filter
+    <td>lttng_event_rule_log4j_logging_set_filter()
+</table>
+
+@returns
+    @parblock
+    \lt_log4j1 event rule on success, or \c NULL on error.
+
+    Destroy the returned event rule with lttng_event_rule_destroy().
+    @endparblock
+*/
 LTTNG_EXPORT extern struct lttng_event_rule *lttng_event_rule_log4j_logging_create(void);
 
-/*
- * Set the name pattern of a log4j logging event rule.
- *
- * Pattern can contain wildcard '*'. See man lttng-enable-event.
- *
- * Return LTTNG_EVENT_RULE_STATUS_OK on success, LTTNG_EVENT_RULE_STATUS_INVALID
- * if invalid parameters are passed.
- */
+/*!
+@brief
+    Sets the event name pattern of the \lt_log4j1 event rule \lt_p{rule}
+    to \lt_p{pattern}.
+
+See the \ref api-er-conds-event-name "event name" condition.
+
+@param[in] rule
+    \lt_log4j1 event rule of which to set the event name pattern.
+@param[in] pattern
+    New event name pattern of \lt_p{rule} (copied).
+
+@retval #LTTNG_EVENT_RULE_STATUS_OK
+    Success.
+@retval #LTTNG_EVENT_RULE_STATUS_INVALID
+    Unsatisfied precondition.
+
+@pre
+    @lt_pre_not_null{rule}
+    @lt_pre_has_type{rule,LTTNG_EVENT_RULE_TYPE_LOG4J_LOGGING}
+    @lt_pre_not_null{pattern}
+    @lt_pre_not_empty{pattern}
+
+@sa lttng_event_rule_log4j_logging_get_name_pattern() --
+    Get the event name pattern of an \lt_log4j1 event rule.
+*/
 LTTNG_EXPORT extern enum lttng_event_rule_status
 lttng_event_rule_log4j_logging_set_name_pattern(struct lttng_event_rule *rule, const char *pattern);
 
-/*
- * Get the name pattern of a log4j logging event rule.
- *
- * The caller does not assume the ownership of the returned pattern. The
- * pattern shall only only be used for the duration of the event rule's
- * lifetime, or before a different pattern is set.
- *
- * Returns LTTNG_EVENT_RULE_STATUS_OK and a pointer to the event rule's pattern
- * on success, LTTNG_EVENT_RULE_STATUS_INVALID if an invalid
- * parameter is passed, or LTTNG_EVENT_RULE_STATUS_UNSET if a pattern
- * was not set prior to this call.
- */
+/*!
+@brief
+    Sets \lt_p{*pattern} to the event name pattern of the \lt_log4j1
+    event rule \lt_p{rule}.
+
+See the \ref api-er-conds-event-name "event name" condition.
+
+@param[in] rule
+    \lt_log4j1 event rule of which to get the event name pattern.
+@param[out] pattern
+    @parblock
+    <strong>On success</strong>, this function sets \lt_p{*pattern} to
+    the event name pattern of \lt_p{rule}.
+
+    \lt_p{rule} owns \lt_p{*pattern}.
+
+    \lt_p{*pattern} remains valid until the next function call
+    with \lt_p{rule}.
+    @endparblock
+
+@retval #LTTNG_EVENT_RULE_STATUS_OK
+    Success.
+@retval #LTTNG_EVENT_RULE_STATUS_UNSET
+    \lt_p{rule} has no event name pattern, which corresponds to
+    the pattern <code>*</code> (any event name).
+@retval #LTTNG_EVENT_RULE_STATUS_INVALID
+    Unsatisfied precondition.
+
+@pre
+    @lt_pre_not_null{rule}
+    @lt_pre_has_type{rule,LTTNG_EVENT_RULE_TYPE_LOG4J_LOGGING}
+    @lt_pre_not_null{pattern}
+
+@sa lttng_event_rule_log4j_logging_set_name_pattern() --
+    Set the event name pattern of an \lt_log4j1 event rule.
+*/
 LTTNG_EXPORT extern enum lttng_event_rule_status
 lttng_event_rule_log4j_logging_get_name_pattern(const struct lttng_event_rule *rule,
 						const char **pattern);
 
-/*
- * Set the filter expression of a log4j logging event rule.
- *
- * The expression is copied internally.
- *
- * Return LTTNG_EVENT_RULE_STATUS_OK on success, LTTNG_EVENT_RULE_STATUS_INVALID
- * if invalid parameters are passed.
- */
-LTTNG_EXPORT extern enum lttng_event_rule_status
-lttng_event_rule_log4j_logging_set_filter(struct lttng_event_rule *rule, const char *expression);
+/*!
+@brief
+    Sets the event payload and context filter of the \lt_log4j1
+    event rule \lt_p{rule} to \lt_p{filter_expr}.
 
-/*
- * Get the filter expression of a log4j logging event rule.
- *
- * The caller does not assume the ownership of the returned filter expression.
- * The filter expression shall only only be used for the duration of the event
- * rule's lifetime, or before a different filter expression is set.
- *
- * Returns LTTNG_EVENT_RULE_STATUS_OK and a pointer to the event rule's filter
- * expression on success, LTTNG_EVENT_RULE_STATUS_INVALID if an invalid
- * parameter is passed, or LTTNG_EVENT_RULE_STATUS_UNSET if a filter expression
- * was not set prior to this call.
- */
+See the \ref api-er-conds-filter "event payload and context filter"
+condition.
+
+@param[in] rule
+    \lt_log4j1 event rule of which to set the
+    event payload and context filter.
+@param[in] filter_expr
+    Event payload and context filter expression of \lt_p{rule} (copied).
+
+@retval #LTTNG_EVENT_RULE_STATUS_OK
+    Success.
+@retval #LTTNG_EVENT_RULE_STATUS_INVALID
+    Unsatisfied precondition.
+
+@pre
+    @lt_pre_not_null{rule}
+    @lt_pre_has_type{rule,LTTNG_EVENT_RULE_TYPE_LOG4J_LOGGING}
+    @lt_pre_not_null{filter_expr}
+    - \lt_p{filter_expr} is a valid filter expression (see
+      the \ref api-er-conds-filter "event payload and context filter"
+      condition).
+
+@sa lttng_event_rule_log4j_logging_get_filter() --
+    Get the event payload and context filter of an \lt_log4j1
+    event rule.
+*/
+LTTNG_EXPORT extern enum lttng_event_rule_status
+lttng_event_rule_log4j_logging_set_filter(struct lttng_event_rule *rule, const char *filter_expr);
+
+/*!
+@brief
+    Sets \lt_p{*filter_expr} to the event payload and context
+    filter expression of the \lt_log4j1 event rule \lt_p{rule}.
+
+See the \ref api-er-conds-filter "event payload and context filter"
+condition.
+
+@param[in] rule
+    \lt_log4j1 event rule of which to get the
+    event payload and context filter.
+@param[out] filter_expr
+    @parblock
+    <strong>On success</strong>, this function sets \lt_p{*filter_expr}
+    to the filter expression of \lt_p{rule}.
+
+    \lt_p{rule} owns \lt_p{*filter_expr}.
+
+    \lt_p{*filter_expr} remains valid until the next function call
+    with \lt_p{rule}.
+    @endparblock
+
+@retval #LTTNG_EVENT_RULE_STATUS_OK
+    Success.
+@retval #LTTNG_EVENT_RULE_STATUS_UNSET
+    \lt_p{rule} has no event payload and context filter,
+    which corresponds to the filter expression <code>1</code>.
+@retval #LTTNG_EVENT_RULE_STATUS_INVALID
+    Unsatisfied precondition.
+
+@pre
+    @lt_pre_not_null{rule}
+    @lt_pre_has_type{rule,LTTNG_EVENT_RULE_TYPE_LOG4J_LOGGING}
+    @lt_pre_not_null{filter_expr}
+
+@sa lttng_event_rule_log4j_logging_set_filter() --
+    Set the event payload and context filter of an \lt_log4j1
+    event rule.
+*/
 LTTNG_EXPORT extern enum lttng_event_rule_status
 lttng_event_rule_log4j_logging_get_filter(const struct lttng_event_rule *rule,
-					  const char **expression);
+					  const char **filter_expr);
 
-/*
- * Set the log level rule of a log4j logging event rule.
- *
- * The log level rule is copied internally.
- *
- * Return LTTNG_EVENT_RULE_STATUS_OK on success, LTTNG_EVENT_RULE_STATUS_INVALID
- * if invalid parameters are passed.
- */
+/*!
+@brief
+    Sets the instrumentation point log level rule of the \lt_log4j1
+    event rule \lt_p{event_rule} to \lt_p{log_level_rule}.
+
+See the \ref api-er-conds-ll "instrumentation point log level"
+condition.
+
+@param[in] event_rule
+    \lt_log4j1 event rule of which to set the log level rule.
+@param[in] log_level_rule
+    Log level rule of \lt_p{event_rule} (copied).
+
+@retval #LTTNG_EVENT_RULE_STATUS_OK
+    Success.
+@retval #LTTNG_EVENT_RULE_STATUS_INVALID
+    Unsatisfied precondition.
+
+@pre
+    @lt_pre_not_null{event_rule}
+    @lt_pre_has_type{event_rule,LTTNG_EVENT_RULE_TYPE_LOG4J_LOGGING}
+    @lt_pre_not_null{log_level_rule}
+
+@sa lttng_event_rule_log4j_logging_get_log_level_rule() --
+    Get the log level rule of an \lt_log4j1 event rule.
+*/
 LTTNG_EXPORT extern enum lttng_event_rule_status lttng_event_rule_log4j_logging_set_log_level_rule(
-	struct lttng_event_rule *rule, const struct lttng_log_level_rule *log_level_rule);
+	struct lttng_event_rule *event_rule, const struct lttng_log_level_rule *log_level_rule);
 
-/*
- * Get the log level rule of a log4j logging event rule.
- *
- * The caller does not assume the ownership of the returned log level rule. The
- * log level rule shall only only be used for the duration of the event rule's
- * lifetime, or before a different log level rule is set.
- *
- * Returns LTTNG_EVENT_RULE_STATUS_OK and sets the log level rule output
- * parameter on success, LTTNG_EVENT_RULE_STATUS_INVALID if an invalid parameter
- * is passed, or LTTNG_EVENT_RULE_STATUS_UNSET if a log level rule was not set prior
- * to this call.
- */
+/*!
+@brief
+    Sets \lt_p{*log_level_rule} to the instrumentation point
+    log level rule of the \lt_log4j1 event rule \lt_p{event_rule}.
+
+See the \ref api-er-conds-ll "instrumentation point log level"
+condition.
+
+@param[in] event_rule
+    \lt_log4j1 event rule of which to get the log level rule.
+@param[out] log_level_rule
+    @parblock
+    <strong>On success</strong>, this function sets
+    \lt_p{*log_level_rule} to the log level rule of \lt_p{event_rule}.
+
+    \lt_p{event_rule} owns \lt_p{*log_level_rule}.
+
+    \lt_p{*log_level_rule} remains valid until the next function call
+    with \lt_p{event_rule}.
+    @endparblock
+
+@retval #LTTNG_EVENT_RULE_STATUS_OK
+    Success.
+@retval #LTTNG_EVENT_RULE_STATUS_UNSET
+    \lt_p{event_rule} has no log level rule.
+@retval #LTTNG_EVENT_RULE_STATUS_INVALID
+    Unsatisfied precondition.
+
+@pre
+    @lt_pre_not_null{event_rule}
+    @lt_pre_has_type{event_rule,LTTNG_EVENT_RULE_TYPE_LOG4J_LOGGING}
+    @lt_pre_not_null{log_level_rule}
+
+@sa lttng_event_rule_log4j_logging_set_log_level_rule() --
+    Set the log level rule of an \lt_log4j1 event rule.
+*/
 LTTNG_EXPORT extern enum lttng_event_rule_status lttng_event_rule_log4j_logging_get_log_level_rule(
-	const struct lttng_event_rule *rule, const struct lttng_log_level_rule **log_level_rule);
+	const struct lttng_event_rule *event_rule,
+	const struct lttng_log_level_rule **log_level_rule);
+
+/// @}
 
 #ifdef __cplusplus
 }
