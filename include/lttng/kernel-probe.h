ff4081ae03e0bcc8def12cca6fe4fad6b60396a3
Philippe Proulx <eeppeliteloop@gmail.com>
2025-05-02 22:06:49 -0400

    include/lttng/kernel-probe.h: document with Doxygen
    Signed-off-by: Philippe Proulx <eeppeliteloop@gmail.com>
    Change-Id: Iaf4d528cbe83762f7fd8f663162c999b40baa03f
    Signed-off-by: Jérémie Galarneau <jeremie.galarneau@efficios.com>

--- include/lttng/kernel-probe.h
+++ include/lttng/kernel-probe.h
@@ -16,67 +16,212 @@
 extern "C" {
 #endif
 
+/*!
+@addtogroup api_kprobe_loc
+@{
+*/
+
+/*!
+@struct lttng_kernel_probe_location
+
+@brief
+    Linux <a href="https://www.kernel.org/doc/html/latest/trace/kprobes.html">kprobe</a>
+    location (opaque type).
+*/
 struct lttng_kernel_probe_location;
 
+/*!
+@brief
+    Return type of Linux kprobe location API functions.
+*/
 enum lttng_kernel_probe_location_status {
+	/// Success.
 	LTTNG_KERNEL_PROBE_LOCATION_STATUS_OK = 0,
-	/* Invalid parameters provided. */
+
+	/// Unsatisfied precondition.
 	LTTNG_KERNEL_PROBE_LOCATION_STATUS_INVALID = -1,
 };
 
+/*!
+@brief
+    Linux kprobe location type.
+
+Get the type of a Linux kprobe location with
+lttng_kernel_probe_location_get_type().
+*/
 enum lttng_kernel_probe_location_type {
-	LTTNG_KERNEL_PROBE_LOCATION_TYPE_UNKNOWN = -1,
-	/* Location derived from a symbol and an offset. */
+	/// Offset from a named symbol.
 	LTTNG_KERNEL_PROBE_LOCATION_TYPE_SYMBOL_OFFSET = 0,
-	/* Location derived from an address. */
+
+	/// Address within the kernel.
 	LTTNG_KERNEL_PROBE_LOCATION_TYPE_ADDRESS = 1,
+
+	/// Unknown (error).
+	LTTNG_KERNEL_PROBE_LOCATION_TYPE_UNKNOWN = -1,
 };
 
-/*
- * Get the type of the kernel probe location.
- */
+/*!
+@brief
+    Returns the type of the Linux kprobe location \lt_p{location}.
+
+@param[in] location
+    Linux kprobe location of which to get the type.
+
+@returns
+    Type of \lt_p{location}.
+
+@pre
+    @lt_pre_not_null{location}
+*/
 LTTNG_EXPORT extern enum lttng_kernel_probe_location_type
 lttng_kernel_probe_location_get_type(const struct lttng_kernel_probe_location *location);
 
-/*
- * Destroy the kernel probe location.
- */
+/*!
+@brief
+    Destroys the Linux kprobe location \lt_p{location}.
+
+@param[in] location
+    @parblock
+    Linux kprobe location to destroy.
+
+    May be \c NULL.
+    @endparblock
+*/
 LTTNG_EXPORT extern void
 lttng_kernel_probe_location_destroy(struct lttng_kernel_probe_location *location);
 
-/*
- * Create a symbol derived probe location.
- * On failure, NULL is returned.
- */
+/*!
+@brief
+    Creates a Linux kprobe location at the offset \lt_p{offset} bytes
+    from the kernel symbol named \lt_p{symbol}.
+
+@param[in] symbol_name
+    Symbol name of the Linux kprobe location to create (copied).
+@param[in] offset
+    Offset (bytes) from \lt_p{symbol} of the Linux kprobe location
+    to create.
+
+@returns
+    @parblock
+    Linux kprobe location with the type
+    #LTTNG_KERNEL_PROBE_LOCATION_TYPE_SYMBOL_OFFSET on success,
+    or \c NULL on error.
+
+    Destroy the returned location with
+    lttng_kernel_probe_location_destroy().
+    @endparblock
+
+@pre
+    @lt_pre_not_null{symbol_name}
+*/
 LTTNG_EXPORT extern struct lttng_kernel_probe_location *
 lttng_kernel_probe_location_symbol_create(const char *symbol_name, uint64_t offset);
 
-/*
- * Get the symbol name of a symbol derived probe location.
- */
+/*!
+@brief
+    Returns the symbol name of the
+    Linux kprobe location \lt_p{location}.
+
+@param[in] location
+    Linux kprobe location of which to get the symbol name.
+
+@returns
+    @parblock
+    Symbol name of \lt_p{location}, or \c NULL on error.
+
+    \lt_p{location} owns the returned string.
+
+    The returned string remains valid as long as \lt_p{location}
+    exists.
+    @endparblock
+
+@pre
+    @lt_pre_not_null{location}
+    @lt_pre_has_type{location,LTTNG_KERNEL_PROBE_LOCATION_TYPE_SYMBOL_OFFSET}
+
+@sa lttng_kernel_probe_location_symbol_get_offset() --
+    Get the offset from symbol of a Linux kprobe location.
+*/
 LTTNG_EXPORT extern const char *
 lttng_kernel_probe_location_symbol_get_name(const struct lttng_kernel_probe_location *location);
 
-/*
- * Get the offset of a symbol derived location.
- */
+/*!
+@brief
+    Sets \lt_p{*offset} to the offset from symbol of the
+    Linux kprobe location \lt_p{location}.
+
+@param[in] location
+    Linux kprobe location of which to get the offset from symbol.
+@param[out] offset
+    <strong>On success</strong>, this function sets \lt_p{*offset}
+    to the offset (bytes) from symbol of \lt_p{location}.
+
+@retval #LTTNG_KERNEL_PROBE_LOCATION_STATUS_OK
+    Success.
+@retval #LTTNG_KERNEL_PROBE_LOCATION_STATUS_INVALID
+    Unsatisfied precondition.
+
+@pre
+    @lt_pre_not_null{location}
+    @lt_pre_has_type{location,LTTNG_KERNEL_PROBE_LOCATION_TYPE_SYMBOL_OFFSET}
+    @lt_pre_not_null{offset}
+
+@sa lttng_kernel_probe_location_symbol_get_name() --
+    Get the symbol name of a Linux kprobe location.
+*/
 LTTNG_EXPORT extern enum lttng_kernel_probe_location_status
 lttng_kernel_probe_location_symbol_get_offset(const struct lttng_kernel_probe_location *location,
 					      uint64_t *offset);
 
-/*
- * Create an address derived probe location.
- * On failure, NULL is returned.
- */
+/*!
+@brief
+    Creates a Linux kprobe location at the address \lt_p{address} bytes
+    within the kernel.
+
+@param[in] address
+    Address (bytes) within the kernel of the
+    Linux kprobe location to create.
+
+@returns
+    @parblock
+    Linux kprobe location with the type
+    #LTTNG_KERNEL_PROBE_LOCATION_TYPE_ADDRESS on success,
+    or \c NULL on error.
+
+    Destroy the returned location with
+    lttng_kernel_probe_location_destroy().
+    @endparblock
+*/
 LTTNG_EXPORT extern struct lttng_kernel_probe_location *
 lttng_kernel_probe_location_address_create(uint64_t address);
 
-/*
- * Get the address of an address derived probe location.
- */
+/*!
+@brief
+    Sets \lt_p{*address} to the address within the kernel of the
+    Linux kprobe location \lt_p{location}.
+
+@param[in] location
+    Linux kprobe location of which to get the address within the
+    kernel.
+@param[out] address
+    <strong>On success</strong>, this function sets \lt_p{*address}
+    to the address (bytes) within the kernel of \lt_p{location}.
+
+@retval #LTTNG_KERNEL_PROBE_LOCATION_STATUS_OK
+    Success.
+@retval #LTTNG_KERNEL_PROBE_LOCATION_STATUS_INVALID
+    Unsatisfied precondition.
+
+@pre
+    @lt_pre_not_null{location}
+    @lt_pre_has_type{location,LTTNG_KERNEL_PROBE_LOCATION_TYPE_ADDRESS}
+    @lt_pre_not_null{address}
+*/
 LTTNG_EXPORT extern enum lttng_kernel_probe_location_status
 lttng_kernel_probe_location_address_get_address(const struct lttng_kernel_probe_location *location,
-						uint64_t *offset);
+						uint64_t *address);
+
+/// @}
 
 #ifdef __cplusplus
 }
