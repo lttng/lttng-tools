1cf0e950674820fe8ab30477c5f953e367a1ace4
Philippe Proulx <eeppeliteloop@gmail.com>
2025-05-05 15:04:21 -0400

    include/lttng/userspace-probe.h: document with Doxygen
    Signed-off-by: Philippe Proulx <eeppeliteloop@gmail.com>
    Change-Id: Ib4a666ab54a16ccdd839b87d00d49ffb82f0f97c
    Signed-off-by: Jérémie Galarneau <jeremie.galarneau@efficios.com>

--- include/lttng/userspace-probe.h
+++ include/lttng/userspace-probe.h
@@ -14,75 +14,181 @@
 extern "C" {
 #endif
 
-/*
- * Userspace probe lookup methods specifies how the userspace probe location
- * specified by the user should be interpreted.
- */
+/*!
+@addtogroup api_uprobe_loc
+@{
+*/
+
+/*!
+@struct lttng_userspace_probe_location_lookup_method
+
+@brief
+    Lookup method of a
+    Linux <a href="https://lwn.net/Articles/499190/">user space probe</a>
+    location (opaque type).
+*/
 struct lttng_userspace_probe_location_lookup_method;
 
+/*!
+@brief
+    Linux user space probe location lookup method type.
+
+Get the type of a Linux user space probe location lookup method
+with lttng_userspace_probe_location_lookup_method_get_type().
+*/
 enum lttng_userspace_probe_location_lookup_method_type {
-	LTTNG_USERSPACE_PROBE_LOCATION_LOOKUP_METHOD_TYPE_UNKNOWN = -1,
+	/// Default.
 	LTTNG_USERSPACE_PROBE_LOCATION_LOOKUP_METHOD_TYPE_FUNCTION_DEFAULT = 0,
+
+	/// Executable and Linkable Format (ELF) function.
 	LTTNG_USERSPACE_PROBE_LOCATION_LOOKUP_METHOD_TYPE_FUNCTION_ELF = 1,
+
+	/*!
+	SystemTap Userland Statically Defined Tracing
+	(USDT; a DTrace-style marker) probe.
+	*/
 	LTTNG_USERSPACE_PROBE_LOCATION_LOOKUP_METHOD_TYPE_TRACEPOINT_SDT = 2,
+
+	/// Unknown (error).
+	LTTNG_USERSPACE_PROBE_LOCATION_LOOKUP_METHOD_TYPE_UNKNOWN = -1,
 };
 
-/*
- * Get the type of a lookup method.
- */
+/*!
+@brief
+    Returns the type of the
+    Linux user space probe location lookup method \lt_p{lookup_method}.
+
+@param[in] lookup_method
+    Linux user space probe location lookup method
+    of which to get the type.
+
+@returns
+    Type of \lt_p{lookup_method}.
+
+@pre
+    @lt_pre_not_null{lookup_method}
+*/
 LTTNG_EXPORT extern enum lttng_userspace_probe_location_lookup_method_type
 lttng_userspace_probe_location_lookup_method_get_type(
 	const struct lttng_userspace_probe_location_lookup_method *lookup_method);
 
-/*
- * Destroy a lookup method.
- */
+/*!
+@brief
+    Destroys the Linux user space probe location lookup method
+    \lt_p{lookup_method}.
+
+@param[in] lookup_method
+    @parblock
+    Linux user space probe location lookup method to destroy.
+
+    May be \c NULL.
+    @endparblock
+*/
 LTTNG_EXPORT extern void lttng_userspace_probe_location_lookup_method_destroy(
 	struct lttng_userspace_probe_location_lookup_method *lookup_method);
 
-/*
- * Create a tracepoint ELF function lookup method struct.
- * Return NULL on failure.
- */
+/*!
+@brief
+    Creates an Executable and Linkable Format (ELF) Linux user space
+    probe location lookup method.
+
+@returns
+    @parblock
+    ELF Linux user space probe location lookup method with the type
+    #LTTNG_USERSPACE_PROBE_LOCATION_LOOKUP_METHOD_TYPE_FUNCTION_ELF
+    on success, or \c NULL on error.
+
+    Destroy the returned lookup method with
+    lttng_userspace_probe_location_lookup_method_destroy().
+    @endparblock
+*/
 LTTNG_EXPORT extern struct lttng_userspace_probe_location_lookup_method *
 lttng_userspace_probe_location_lookup_method_function_elf_create(void);
 
-/*
- * Create a tracepoint SDT tracepoint lookup method struct.
- * Return NULL on failure.
- */
+/*!
+@brief
+    Creates an USDT probe Linux user space probe location lookup method.
+
+@returns
+    @parblock
+    USDT Linux user space probe location lookup method with the type
+    #LTTNG_USERSPACE_PROBE_LOCATION_LOOKUP_METHOD_TYPE_TRACEPOINT_SDT
+    on success, or \c NULL on error.
+
+    Destroy the returned lookup method with
+    lttng_userspace_probe_location_lookup_method_destroy().
+    @endparblock
+*/
 LTTNG_EXPORT extern struct lttng_userspace_probe_location_lookup_method *
 lttng_userspace_probe_location_lookup_method_tracepoint_sdt_create(void);
 
-/*
- * Contains all the information needed to compute the instrumentation point in
- * the binary. It is used in conjonction with a lookup method.
- */
+/*!
+@struct lttng_userspace_probe_location
+
+@brief
+    Linux <a href="https://lwn.net/Articles/499190/">user space probe</a>
+    location (opaque type).
+*/
 struct lttng_userspace_probe_location;
 
+/*!
+@brief
+    Return type of Linux user space probe location API functions.
+*/
 enum lttng_userspace_probe_location_status {
+	/// Success.
 	LTTNG_USERSPACE_PROBE_LOCATION_STATUS_OK = 0,
-	/* Invalid parameters provided. */
+
+	/// Unsatisfied precondition.
 	LTTNG_USERSPACE_PROBE_LOCATION_STATUS_INVALID = -1,
 };
 
+/*!
+@brief
+    Linux user space probe location type.
+
+Get the type of a Linux user space probe location
+with lttng_userspace_probe_location_get_type().
+*/
 enum lttng_userspace_probe_location_type {
-	LTTNG_USERSPACE_PROBE_LOCATION_TYPE_UNKNOWN = -1,
-	/* Function. */
+	/// Function.
 	LTTNG_USERSPACE_PROBE_LOCATION_TYPE_FUNCTION = 0,
-	/* SDT probe's callsites. */
+
+	/// USDT probe.
 	LTTNG_USERSPACE_PROBE_LOCATION_TYPE_TRACEPOINT = 1,
+
+	/// Unknown (error).
+	LTTNG_USERSPACE_PROBE_LOCATION_TYPE_UNKNOWN = -1,
 };
 
-/*
- * Get the type of the userspace probe location.
- */
+/*!
+@brief
+    Returns the type of the
+    Linux user space probe location \lt_p{location}.
+
+@param[in] location
+    Linux user space probe location of which to get the type.
+
+@returns
+    Type of \lt_p{location}.
+
+@pre
+    @lt_pre_not_null{location}
+*/
 LTTNG_EXPORT extern enum lttng_userspace_probe_location_type
 lttng_userspace_probe_location_get_type(const struct lttng_userspace_probe_location *location);
 
-/*
- * Destroy the userspace probe location.
- */
+/*!
+@brief
+    Destroys the Linux user space probe location \lt_p{location}.
+
+@param[in] location
+    @parblock
+    Linux user space probe location to destroy.
+
+    May be \c NULL.
+    @endparblock
+*/
 LTTNG_EXPORT extern void
 lttng_userspace_probe_location_destroy(struct lttng_userspace_probe_location *location);
 
@@ -92,36 +198,114 @@ enum lttng_userspace_probe_location_function_instrumentation_type {
 	LTTNG_USERSPACE_PROBE_LOCATION_FUNCTION_INSTRUMENTATION_TYPE_ENTRY = 0,
 };
 
-/*
- * Create a probe location of the function type.
- * Receives the target binary file path and function to instrument.
- * On failure, NULL is returned.
- *
- * The ownership of the lookup method is transferred to the created probe
- * location.
- */
+/*!
+@brief
+    Creates a Linux user space probe location at the entry of the
+    function named \lt_p{function_name} within the binary
+    \lt_p{binary_path} using the lookup method \lt_p{lookup_method}.
+
+This function opens \lt_p{binary_path} and keeps the file open. Get the
+resulting file descriptor with
+lttng_userspace_probe_location_function_get_binary_fd().
+
+@param[in] binary_path
+    Path to the binary containing the function named
+    \lt_p{function_name} to locate (copied).
+@param[in] function_name
+    Name of the function within \lt_p{binary_path} to locate (copied).
+@param[in] lookup_method
+    Lookup method (ownership moved to the returned location on success).
+
+@returns
+    @parblock
+    Linux user space probe location with the type
+    #LTTNG_USERSPACE_PROBE_LOCATION_TYPE_FUNCTION on success,
+    or \c NULL on error.
+
+    Destroy the returned location with
+    lttng_userspace_probe_location_destroy().
+    @endparblock
+
+@pre
+    @lt_pre_not_null{binary_path}
+    @lt_pre_not_null{function_name}
+    @lt_pre_not_null{lookup_method}
+    @lt_pre_has_type{lookup_method,LTTNG_USERSPACE_PROBE_LOCATION_LOOKUP_METHOD_TYPE_FUNCTION_ELF}
+*/
 LTTNG_EXPORT extern struct lttng_userspace_probe_location *
 lttng_userspace_probe_location_function_create(
 	const char *binary_path,
 	const char *function_name,
 	struct lttng_userspace_probe_location_lookup_method *lookup_method);
 
-/*
- * Get the target binary path of the probe location of the function type.
- */
+/*!
+@brief
+    Returns the binary path of the function Linux user space probe
+    location \lt_p{location}.
+
+@param[in] location
+    Function Linux user space probe location of which to get the
+    binary path.
+
+@returns
+    @parblock
+    Binary path of \lt_p{location}, or \c NULL on error.
+
+    \lt_p{location} owns the returned string.
+
+    The returned string remains valid as long as \lt_p{location}
+    exists.
+    @endparblock
+
+@pre
+    @lt_pre_not_null{location}
+    @lt_pre_has_type{location,LTTNG_USERSPACE_PROBE_LOCATION_TYPE_FUNCTION}
+*/
 LTTNG_EXPORT extern const char *lttng_userspace_probe_location_function_get_binary_path(
 	const struct lttng_userspace_probe_location *location);
 
-/*
- * Get the target function type of the probe location of the function type.
- */
+/*!
+@brief
+    Returns the function name of the function Linux user space probe
+    location \lt_p{location}.
+
+@param[in] location
+    Function Linux user space probe location of which to get the
+    function name.
+
+@returns
+    @parblock
+    Function name of \lt_p{location}, or \c NULL on error.
+
+    \lt_p{location} owns the returned string.
+
+    The returned string remains valid as long as \lt_p{location}
+    exists.
+    @endparblock
+
+@pre
+    @lt_pre_not_null{location}
+    @lt_pre_has_type{location,LTTNG_USERSPACE_PROBE_LOCATION_TYPE_FUNCTION}
+*/
 LTTNG_EXPORT extern const char *lttng_userspace_probe_location_function_get_function_name(
 	const struct lttng_userspace_probe_location *location);
 
-/*
- * Get the FD to the target binary file to the probe location of the function
- * type. The FD is only valid for the duration of the lifetime of `location`.
- */
+/*!
+@brief
+    Returns the file descriptor of the target binary of the function
+    Linux user space probe location \lt_p{location}.
+
+@param[in] location
+    Function Linux user space probe location of which to get the
+    binary file descriptor.
+
+@returns
+    Binary file descriptor of \lt_p{location}, or -1 on error.
+
+@pre
+    @lt_pre_not_null{location}
+    @lt_pre_has_type{location,LTTNG_USERSPACE_PROBE_LOCATION_TYPE_FUNCTION}
+*/
 LTTNG_EXPORT extern int lttng_userspace_probe_location_function_get_binary_fd(
 	const struct lttng_userspace_probe_location *location);
 
@@ -146,25 +330,68 @@ lttng_userspace_probe_location_function_set_instrumentation_type(
 	const struct lttng_userspace_probe_location *location,
 	enum lttng_userspace_probe_location_function_instrumentation_type instrumentation_type);
 
-/*
- * Get the lookup method of the given userspace probe location.
- * Returns NULL if the probe location type is unsupported.
- *
- * The ownership of the lookup method is NOT transferred to the caller.
- */
+/*!
+@brief
+    Returns the lookup method of the Linux user space probe
+    location \lt_p{location}.
+
+@param[in] location
+    Linux user space probe location of which to get the lookup method.
+
+@returns
+    @parblock
+    Lookup method of \lt_p{location}, or \c NULL on error.
+
+    \lt_p{location} owns the returned object.
+
+    The returned object remains valid as long as \lt_p{location} exists.
+    @endparblock
+
+@pre
+    @lt_pre_not_null{location}
+*/
 LTTNG_EXPORT extern const struct lttng_userspace_probe_location_lookup_method *
 lttng_userspace_probe_location_get_lookup_method(
 	const struct lttng_userspace_probe_location *location);
 
-/*
- * Create a probe location of the tracepoint type.
- * Receives the target binary file path, probename and probe provider to
- * instrument.
- * On failure, NULL is returned.
- *
- * The ownership of the lookup method is transferred to the created probe
- * location.
- */
+/*!
+@brief
+    Creates a Linux user space probe location at the USDT probe
+    named \lt_p{probe_name}, for the provider named
+    \lt_p{provider_name}, within the binary
+    \lt_p{binary_path} using the lookup method \lt_p{lookup_method}.
+
+This function opens \lt_p{binary_path} and keeps the file open. Get the
+resulting file descriptor with
+lttng_userspace_probe_location_tracepoint_get_binary_fd().
+
+@param[in] binary_path
+    Path to the binary containing the USDT probe to locate (copied).
+@param[in] probe_name
+    Name of the USDT probe to locate within \lt_p{binary_path} (copied).
+@param[in] provider_name
+    Name of the provider of the USDT probe to locate
+    within \lt_p{binary_path} (copied).
+@param[in] lookup_method
+    Lookup method (ownership moved to the returned location on success).
+
+@returns
+    @parblock
+    Linux user space probe location with the type
+    #LTTNG_USERSPACE_PROBE_LOCATION_TYPE_TRACEPOINT on success,
+    or \c NULL on error.
+
+    Destroy the returned location with
+    lttng_userspace_probe_location_destroy().
+    @endparblock
+
+@pre
+    @lt_pre_not_null{binary_path}
+    @lt_pre_not_null{probe_name}
+    @lt_pre_not_null{provider_name}
+    @lt_pre_not_null{lookup_method}
+    @lt_pre_has_type{lookup_method,LTTNG_USERSPACE_PROBE_LOCATION_LOOKUP_METHOD_TYPE_TRACEPOINT_SDT}
+*/
 LTTNG_EXPORT extern struct lttng_userspace_probe_location *
 lttng_userspace_probe_location_tracepoint_create(
 	const char *binary_path,
@@ -172,31 +399,104 @@ lttng_userspace_probe_location_tracepoint_create(
 	const char *provider_name,
 	struct lttng_userspace_probe_location_lookup_method *lookup_method);
 
-/*
- * Get the target binary path of the probe location of the tracepoint type.
- */
+/*!
+@brief
+    Returns the binary path of the USDT Linux user space probe
+    location \lt_p{location}.
+
+@param[in] location
+    USDT Linux user space probe location of which to get the
+    binary path.
+
+@returns
+    @parblock
+    Binary path of \lt_p{location}, or \c NULL on error.
+
+    \lt_p{location} owns the returned string.
+
+    The returned string remains valid as long as \lt_p{location}
+    exists.
+    @endparblock
+
+@pre
+    @lt_pre_not_null{location}
+    @lt_pre_has_type{location,LTTNG_USERSPACE_PROBE_LOCATION_TYPE_TRACEPOINT}
+*/
 LTTNG_EXPORT extern const char *lttng_userspace_probe_location_tracepoint_get_binary_path(
 	const struct lttng_userspace_probe_location *location);
 
-/*
- * Get the target probe name of the probe location of the tracepoint type.
- */
+/*!
+@brief
+    Returns the probe name of the USDT Linux user space probe
+    location \lt_p{location}.
+
+@param[in] location
+    USDT Linux user space probe location of which to get the
+    probe name.
+
+@returns
+    @parblock
+    Probe name of \lt_p{location}, or \c NULL on error.
+
+    \lt_p{location} owns the returned string.
+
+    The returned string remains valid as long as \lt_p{location}
+    exists.
+    @endparblock
+
+@pre
+    @lt_pre_not_null{location}
+    @lt_pre_has_type{location,LTTNG_USERSPACE_PROBE_LOCATION_TYPE_TRACEPOINT}
+*/
 LTTNG_EXPORT extern const char *lttng_userspace_probe_location_tracepoint_get_probe_name(
 	const struct lttng_userspace_probe_location *location);
 
-/*
- * Get the target probe provider name of the probe location of the tracepoint
- * type.
- */
+/*!
+@brief
+    Returns the provider name of the USDT Linux user space probe
+    location \lt_p{location}.
+
+@param[in] location
+    USDT Linux user space probe location of which to get the
+    provider name.
+
+@returns
+    @parblock
+    Probe name of \lt_p{location}, or \c NULL on error.
+
+    \lt_p{location} owns the returned string.
+
+    The returned string remains valid as long as \lt_p{location}
+    exists.
+    @endparblock
+
+@pre
+    @lt_pre_not_null{location}
+    @lt_pre_has_type{location,LTTNG_USERSPACE_PROBE_LOCATION_TYPE_TRACEPOINT}
+*/
 LTTNG_EXPORT extern const char *lttng_userspace_probe_location_tracepoint_get_provider_name(
 	const struct lttng_userspace_probe_location *location);
 
-/*
- * Get the FD to the target binary file to the probe location of the tracepoint
- * type. The FD is only valid for the duration of the lifetime of `location`.
- */
+/*!
+@brief
+    Returns the file descriptor of the target binary of the USDT
+    Linux user space probe location \lt_p{location}.
+
+@param[in] location
+    USDT Linux user space probe location of which to get the
+    binary file descriptor.
+
+@returns
+    Binary file descriptor of \lt_p{location}, or -1 on error.
+
+@pre
+    @lt_pre_not_null{location}
+    @lt_pre_has_type{location,LTTNG_USERSPACE_PROBE_LOCATION_TYPE_TRACEPOINT}
+*/
 LTTNG_EXPORT extern int lttng_userspace_probe_location_tracepoint_get_binary_fd(
 	const struct lttng_userspace_probe_location *location);
+
+/// @}
 
 #ifdef __cplusplus
 }
