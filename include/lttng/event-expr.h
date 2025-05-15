e6dbdafef908748ecf5374937f340b1d154005fa
Philippe Proulx <eeppeliteloop@gmail.com>
2025-05-15 12:29:23 -0400

    include/lttng/event-expr.h: document with Doxygen
    Also document the topic in `doc/api/liblttng-ctl/dox/groups.dox`.
    
    Signed-off-by: Philippe Proulx <eeppeliteloop@gmail.com>
    Change-Id: If327d4afa50ca94fea134a2ef145017cd4f0a03d
    Signed-off-by: Jérémie Galarneau <jeremie.galarneau@efficios.com>

--- include/lttng/event-expr.h
+++ include/lttng/event-expr.h
@@ -18,222 +18,367 @@ struct lttng_event_expr;
 extern "C" {
 #endif
 
-/*
- * Types of an event expression.
- */
-enum lttng_event_expr_type {
-	/*
-	 * Returned by lttng_event_expr_get_type() with an invalid
-	 * parameter.
-	 */
-	LTTNG_EVENT_EXPR_TYPE_INVALID = -1,
+/*!
+@addtogroup api_ev_expr
+@{
+*/
 
-	/*
-	 * The named payload field of an event.
-	 *
-	 * Command-line expression example:
-	 *
-	 *     next_prio
-	 */
+/*!
+@brief
+    Event expression type.
+
+Get the type of an event expression with
+lttng_event_expr_get_type().
+*/
+enum lttng_event_expr_type {
+	/// Payload field reference.
 	LTTNG_EVENT_EXPR_TYPE_EVENT_PAYLOAD_FIELD = 0,
 
-	/*
-	 * The named per-channel context field of an event.
-	 *
-	 * Command-line expression example:
-	 *
-	 *     $ctx.vpid
-	 */
+	/// Statically-known context field reference.
 	LTTNG_EVENT_EXPR_TYPE_CHANNEL_CONTEXT_FIELD = 1,
 
-	/*
-	 * The named application-specific context field of an event.
-	 *
-	 * Command-line expression example:
-	 *
-	 *     $app.iga:active-clients
-	 */
+	/// Application-specific context field reference.
 	LTTNG_EVENT_EXPR_TYPE_APP_SPECIFIC_CONTEXT_FIELD = 2,
 
-	/*
-	 * The element of an array field.
-	 *
-	 * Command-line expression example:
-	 *
-	 *     my_field[4]
-	 *     $ctx.some_context[5][1]
-	 */
+	/// Array field element reference.
 	LTTNG_EVENT_EXPR_TYPE_ARRAY_FIELD_ELEMENT = 3,
+
+	/// Unsatisfied precondition.
+	LTTNG_EVENT_EXPR_TYPE_INVALID = -1,
 };
 
-/*
- * Event expression API status codes.
- */
+/*!
+@brief
+    Return type of event expression API functions.
+*/
 enum lttng_event_expr_status {
-	/*
-	 * Invalid parameter.
-	 */
-	LTTNG_EVENT_EXPR_STATUS_INVALID = -1,
-
-	/*
-	 * Success.
-	 */
+	/// Success.
 	LTTNG_EVENT_EXPR_STATUS_OK = 0,
+
+	/// Unsatisfied precondition.
+	LTTNG_EVENT_EXPR_STATUS_INVALID = -1,
 };
 
-/*
- * Returns the type of the event expression `expr`, or
- * `LTTNG_EVENT_EXPR_TYPE_INVALID` if `expr` is `NULL`.
- */
+/*!
+@brief
+    Returns the type of the event expression \lt_p{expr}.
+
+@param[in] expr
+    Event expression of which to get the type.
+
+@returns
+    Type of \lt_p{expr}.
+
+@pre
+    @lt_pre_not_null{expr}
+*/
 LTTNG_EXPORT extern enum lttng_event_expr_type
 lttng_event_expr_get_type(const struct lttng_event_expr *expr);
 
-/*
- * Creates an event payload field expression for the payload field named
- * `field_name`.
- *
- * Returns `NULL` if:
- *
- * * There's a memory error.
- * * `field_name` is `NULL`.
- */
+/*!
+@brief
+    Creates an event payload field reference expression for the payload
+    field named \lt_p{field_name}.
+
+@param[in] field_name
+    Name of the payload field to reference to (copied).
+
+@returns
+    Event payload field reference expression for the payload field named
+    \lt_p{field_name}, or \c NULL on error.
+
+@pre
+    @lt_pre_not_null{field_name}
+*/
 LTTNG_EXPORT extern struct lttng_event_expr *
 lttng_event_expr_event_payload_field_create(const char *field_name);
 
-/*
- * Returns the field name of the event payload field expression `expr`,
- * or `NULL` if:
- *
- * * `expr` is `NULL`.
- * * The type of `expr` is not
- *   `LTTNG_EVENT_EXPR_TYPE_EVENT_PAYLOAD_FIELD`.
- */
+/*!
+@brief
+    Returns the name of the referenced payload field of the
+    event payload field reference expression \lt_p{expr}.
+
+@param[in] expr
+    Event payload field reference expression of which to get the name of
+    the referenced field.
+
+@returns
+    @parblock
+    Name of the referenced field of \lt_p{expr}, or \c NULL on error.
+
+    \lt_p{expr} owns the returned string.
+
+    The returned string remains valid as long as \lt_p{expr} exists.
+    @endparblock
+
+@pre
+    @lt_pre_not_null{expr}
+    @lt_pre_has_type{expr,LTTNG_EVENT_EXPR_TYPE_EVENT_PAYLOAD_FIELD}
+*/
 LTTNG_EXPORT extern const char *
 lttng_event_expr_event_payload_field_get_name(const struct lttng_event_expr *expr);
 
-/*
- * Creates a per-channel context field expression for the per-channel
- * context field named `field_name`.
- *
- * Returns `NULL` if:
- *
- * * There's a memory error.
- * * `field_name` is `NULL`.
- */
+/*!
+@brief
+    Creates a statically-known context field reference expression for
+    the context field named \lt_p{field_name}.
+
+@param[in] field_name
+    @parblock
+    Name of the statically-known context field to refer to (copied).
+
+    Use one of the statically known names in the “Field name” column
+    of the table of #lttng_event_context_type
+    (that is, excluding the
+    #LTTNG_EVENT_CONTEXT_PERF_CPU_COUNTER,
+    #LTTNG_EVENT_CONTEXT_PERF_THREAD_COUNTER,
+    and #LTTNG_EVENT_CONTEXT_APP_CONTEXT rows).
+    @endparblock
+
+@returns
+    Statically-known context field reference expression for the context
+    field named \lt_p{field_name}, or \c NULL on error.
+
+@pre
+    @lt_pre_not_null{field_name}
+    - \lt_p{field_name} is one of the names in the “Field name” column
+      of the table of #lttng_event_context_type,
+      excluding the #LTTNG_EVENT_CONTEXT_PERF_CPU_COUNTER,
+      #LTTNG_EVENT_CONTEXT_PERF_THREAD_COUNTER,
+      and #LTTNG_EVENT_CONTEXT_APP_CONTEXT rows.
+*/
 LTTNG_EXPORT extern struct lttng_event_expr *
 lttng_event_expr_channel_context_field_create(const char *field_name);
 
-/*
- * Returns the field name of the per-channel context field
- * expression `expr`, or `NULL` if:
- *
- * `expr` is `NULL`.
- * * The type of `expr` is not
- *   `LTTNG_EVENT_EXPR_TYPE_CHANNEL_CONTEXT_FIELD`.
- */
+/*!
+@brief
+    Returns the name of the referenced context field of the
+    statically-known context field reference expression \lt_p{expr}.
+
+@param[in] expr
+    Event payload field reference expression of which to get the name of
+    the referenced field.
+
+@returns
+    @parblock
+    Name of the referenced field of \lt_p{expr}, or \c NULL on error.
+
+    \lt_p{expr} owns the returned string.
+
+    The returned string remains valid as long as \lt_p{expr} exists.
+    @endparblock
+
+@pre
+    @lt_pre_not_null{expr}
+    @lt_pre_has_type{expr,LTTNG_EVENT_EXPR_TYPE_CHANNEL_CONTEXT_FIELD}
+*/
 LTTNG_EXPORT extern const char *
 lttng_event_expr_channel_context_field_get_name(const struct lttng_event_expr *expr);
 
-/*
- * Creates an application-specific context field expression for the
- * application-specific context field provided by the provider named
- * `provider_name` and having the type named `type_name`.
- *
- * Returns `NULL` if:
- *
- * * There's a memory error.
- * * `provider_name` is `NULL`.
- * * `type_name` is `NULL`.
- */
+/*!
+@brief
+    Creates an application-specific context field reference expression
+    for the application-specific context field provided by the provider
+    named \lt_p{provider_name} and having the type named
+    \lt_p{type_name}.
+
+@param[in] provider_name
+    Name of the provider of the application-specific context field
+    to refer to (copied).
+@param[in] type_name
+    Name of the type of the application-specific context field
+    to refer to (copied).
+
+@returns
+    Application-specific context field reference expression for the
+    provider named \lt_p{provider_name} and the type named
+    \lt_p{type_name}, or \c NULL on error.
+
+@pre
+    @lt_pre_not_null{provider_name}
+    @lt_pre_not_null{type_name}
+*/
 LTTNG_EXPORT extern struct lttng_event_expr *
 lttng_event_expr_app_specific_context_field_create(const char *provider_name,
 						   const char *type_name);
 
-/*
- * Returns the provider name of the application-specific context field
- * expression `expr`, or `NULL` if:
- *
- * * `expr` is `NULL`.
- * * The type of `expr` is not
- *   `LTTNG_EVENT_EXPR_TYPE_APP_SPECIFIC_CONTEXT_FIELD`.
- */
+/*!
+@brief
+    Returns the provider name of the referenced field of the
+    application-specific context field reference expression \lt_p{expr}.
+
+@param[in] expr
+    Application-specific context field reference expression of which to
+    get the provider name of the referenced field.
+
+@returns
+    @parblock
+    Provider name of the referenced field of \lt_p{expr},
+    or \c NULL on error.
+
+    \lt_p{expr} owns the returned string.
+
+    The returned string remains valid as long as \lt_p{expr} exists.
+    @endparblock
+
+@pre
+    @lt_pre_not_null{expr}
+    @lt_pre_has_type{expr,LTTNG_EVENT_EXPR_TYPE_APP_SPECIFIC_CONTEXT_FIELD}
+*/
 LTTNG_EXPORT extern const char *
 lttng_event_expr_app_specific_context_field_get_provider_name(const struct lttng_event_expr *expr);
 
-/*
- * Returns the type name of the application-specific context field
- * expression `expr`, or `NULL` if:
- *
- * * `expr` is `NULL`.
- * * The type of `expr` is not
- *   `LTTNG_EVENT_EXPR_TYPE_APP_SPECIFIC_CONTEXT_FIELD`.
- */
+/*!
+@brief
+    Returns the type name of the referenced field of the
+    application-specific context field reference expression \lt_p{expr}.
+
+@param[in] expr
+    Application-specific context field reference expression of which to
+    get the type name of the referenced field.
+
+@returns
+    @parblock
+    Type name of the referenced field of \lt_p{expr},
+    or \c NULL on error.
+
+    \lt_p{expr} owns the returned string.
+
+    The returned string remains valid as long as \lt_p{expr} exists.
+    @endparblock
+
+@pre
+    @lt_pre_not_null{expr}
+    @lt_pre_has_type{expr,LTTNG_EVENT_EXPR_TYPE_APP_SPECIFIC_CONTEXT_FIELD}
+*/
 LTTNG_EXPORT extern const char *
 lttng_event_expr_app_specific_context_field_get_type_name(const struct lttng_event_expr *expr);
 
-/*
- * Creates an array field element expression for the parent array field
- * `array_field_expr` (transfering the ownership) and the index `index`.
- *
- * Returns `NULL` if:
- *
- * * There's a memory error.
- * * `array_field_expr` is `NULL`.
- * * `array_field_expr` is not a locator expression, that is, its type
- *   is not one of:
- *
- *    * `LTTNG_EVENT_EXPR_TYPE_EVENT_PAYLOAD_FIELD`
- *    * `LTTNG_EVENT_EXPR_TYPE_CHANNEL_CONTEXT_FIELD`
- *    * `LTTNG_EVENT_EXPR_TYPE_APP_SPECIFIC_CONTEXT_FIELD`
- *    * `LTTNG_EVENT_EXPR_TYPE_ARRAY_FIELD_ELEMENT`
- */
+/*!
+@brief
+    Creates an array field element reference expression
+    for the parent array field referred by \lt_p{array_field_expr} and
+    the index \lt_p{index}.
+
+@param[in] array_field_expr
+    @parblock
+    Parent array field containing the field element to refer to.
+
+    <strong>On success</strong>, the ownership of this expression is
+    moved to the returned expression.
+    @endparblock
+@param[in] index
+    Index of the field element to refer to
+    within \lt_p{array_field_expr}.
+
+@returns
+    Array field element reference expression for the parent array field
+    \lt_p{array_field_expr} and the index \lt_p{index}.
+
+@pre
+    @lt_pre_not_null{array_field_expr}
+*/
 LTTNG_EXPORT extern struct lttng_event_expr *
 lttng_event_expr_array_field_element_create(struct lttng_event_expr *array_field_expr,
 					    unsigned int index);
 
-/*
- * Returns the parent array field expression of the array field element
- * expression `expr`, or `NULL` if:
- *
- * * `expr` is `NULL`.
- * * The type of `expr` is not
- *   `LTTNG_EVENT_EXPR_TYPE_ARRAY_FIELD_ELEMENT`.
- */
+/*!
+@brief
+    Returns the parent array field expression of the
+    array field element reference expression\lt_p{expr}.
+
+@param[in] expr
+    Array field element reference expression of which to
+    get the parent array field expression.
+
+@returns
+    @parblock
+    Parent array field expression of \lt_p{expr},
+    or \c NULL on error.
+
+    \lt_p{expr} owns the returned expression.
+
+    The returned expression remains valid as long as \lt_p{expr} exists.
+    @endparblock
+
+@pre
+    @lt_pre_not_null{expr}
+    @lt_pre_has_type{expr,LTTNG_EVENT_EXPR_TYPE_ARRAY_FIELD_ELEMENT}
+
+@sa lttng_event_expr_array_field_element_get_index() --
+    Get the index of an array field element reference expression.
+*/
 LTTNG_EXPORT extern const struct lttng_event_expr *
 lttng_event_expr_array_field_element_get_parent_expr(const struct lttng_event_expr *expr);
 
-/*
- * Sets `*index` to the index of the array field element expression
- * `expr`.
- *
- * Returns:
- *
- * `LTTNG_EVENT_EXPR_STATUS_OK`:
- *     Success.
- *
- * `LTTNG_EVENT_EXPR_STATUS_INVALID`:
- *     * `expr` is `NULL`.
- *     * The type of `expr` is not
- *       `LTTNG_EVENT_EXPR_TYPE_ARRAY_FIELD_ELEMENT`.
- *     * `index` is `NULL`.
- */
+/*!
+@brief
+    Sets \lt_p{*index} to the index of the
+    array field element reference expression\lt_p{expr}.
+
+@param[in] expr
+    Array field element reference expression of which to
+    get the index.
+@param[out] index
+    <strong>On success</strong>, this function sets \lt_p{*index}
+    to the index of \lt_p{expr}.
+
+@retval #LTTNG_EVENT_EXPR_STATUS_OK
+    Success.
+@retval #LTTNG_EVENT_EXPR_STATUS_INVALID
+    Unsatisfied precondition.
+
+@pre
+    @lt_pre_not_null{expr}
+    @lt_pre_has_type{expr,LTTNG_EVENT_EXPR_TYPE_ARRAY_FIELD_ELEMENT}
+    @lt_pre_not_null{index}
+
+@sa lttng_event_expr_array_field_element_get_parent_expr() --
+    Get the parent array field expression of an
+    array field element reference expression.
+*/
 LTTNG_EXPORT extern enum lttng_event_expr_status
 lttng_event_expr_array_field_element_get_index(const struct lttng_event_expr *expr,
 					       unsigned int *index);
 
-/*
- * Returns whether or not the event expressions `expr_a` and `expr_b`
- * are equal.
- *
- * `expr_a` and `expr_b` can be `NULL`.
- */
+/*!
+@brief
+    Returns whether or not the event expressions \lt_p{expr_a} and
+    \lt_p{expr_b} are equal.
+
+@param[in] expr_a
+    @parblock
+    Event expression to compare to \lt_p{expr_b}.
+
+    May be \c NULL.
+    @endparblock
+@param[in] expr_b
+    @parblock
+    Event expression to compare to \lt_p{expr_a}.
+
+    May be \c NULL.
+    @endparblock
+
+@returns
+    \c true if \lt_p{expr_a} and \lt_p{expr_b} are equal.
+*/
 LTTNG_EXPORT extern bool lttng_event_expr_is_equal(const struct lttng_event_expr *expr_a,
 						   const struct lttng_event_expr *expr_b);
 
-/*
- * Destroys the event expression `expr` if not `NULL`.
- */
+/*!
+@brief
+    Destroys the event expression \lt_p{expr}.
+
+@param[in] expr
+    @parblock
+    Event expression to destroy.
+
+    May be \c NULL.
+    @endparblock
+*/
 LTTNG_EXPORT extern void lttng_event_expr_destroy(struct lttng_event_expr *expr);
+
+/// @}
 
 #ifdef __cplusplus
 }
