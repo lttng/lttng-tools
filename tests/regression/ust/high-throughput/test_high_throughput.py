3a1484b98a9465289a14ccb7c956c9a5eeb46a2f
Jérémie Galarneau <jeremie.galarneau@efficios.com>
2025-06-27 20:49:38 +0000

    Tests: ust/high_throughput: parallelize event counting
    Most of the runtime of this test is spent counting events in the
    resulting trace especially now that it uses twice the number of
    CPUs to decide how many apps it should launch.
    
    Informally, on my 168 core dev machine, this cuts down the time
    spent decoding the trace from ~20 seconds to 1 second.
    
    Change-Id: Ibf3128000807960d12d9d592b8fd12e36fea0618
    Signed-off-by: Jérémie Galarneau <jeremie.galarneau@efficios.com>

--- tests/regression/ust/high-throughput/test_high_throughput.py
+++ tests/regression/ust/high-throughput/test_high_throughput.py
@@ -11,15 +11,75 @@ events matches the expected output of the traced applications
 
 import os
 import pathlib
-import subprocess
 import sys
-import time
+import multiprocessing
 
 test_utils_import_path = pathlib.Path(__file__).absolute().parents[3] / "utils"
 sys.path.append(str(test_utils_import_path))
 
 import bt2
 import lttngtest
+import tempfile
+
+
+def count_events_worker(args):
+    trace_path = args
+    received = 0
+    discarded = 0
+    for msg in bt2.TraceCollectionMessageIterator(str(trace_path)):
+        if type(msg) is bt2._EventMessageConst:
+            received += 1
+            continue
+
+        if type(msg) is bt2._DiscardedEventsMessageConst:
+            discarded += msg.count
+
+    return (received, discarded)
+
+
+def parallel_count_events(trace_path, test_env):
+    """
+    Count the number of events in a trace using parallel processing.
+    This is a workaround for the performance issues with bt2 bindings.
+    """
+    streams_path = None
+    for root, dirs, files in os.walk(str(trace_path)):
+        if "metadata" in files:
+            streams_path = pathlib.Path(root)
+            break
+
+    if streams_path is None:
+        raise RuntimeError("No metadata found in trace path: {}".format(trace_path))
+
+    metadata_path = streams_path / "metadata"
+    stream_dirs = []
+
+    for file in streams_path.iterdir():
+        if file.name == "metadata":
+            continue
+
+        temp_dir_path = test_env.create_temporary_directory()
+
+        # Symlink to the stream file
+        stream_link = temp_dir_path / file.name
+        stream_link.symlink_to(file)
+        # Symlink to the metadata file
+        metadata_link = temp_dir_path / "metadata"
+        metadata_link.symlink_to(metadata_path)
+
+        stream_dirs.append(temp_dir_path)
+
+    received = 0
+    discarded = 0
+
+    with multiprocessing.Pool() as pool:
+        results = pool.map(count_events_worker, stream_dirs)
+
+    for this_received, this_discarded in results:
+        received += this_received
+        discarded += this_discarded
+
+    return received, discarded
 
 
 def test_high_throughput(
@@ -41,15 +101,7 @@ def test_high_throughput(
     session.stop()
 
     expected = app_count * events_per_app
-    received = 0
-    discarded = 0
-    for msg in bt2.TraceCollectionMessageIterator(str(output_path)):
-        if type(msg) is bt2._EventMessageConst:
-            received += 1
-            continue
-
-        if type(msg) is bt2._DiscardedEventsMessageConst:
-            discarded += msg.count
+    received, discarded = parallel_count_events(output_path, test_env)
 
     total = received + discarded
     tap.diagnostic("Trace output path: {}".format(str(output_path)))
