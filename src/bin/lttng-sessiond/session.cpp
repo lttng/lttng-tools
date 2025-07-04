bf958ed44e5c7d0a43bcb84b9edbb21936b3069b
Jérémie Galarneau <jeremie.galarneau@efficios.com>
2025-07-04 17:58:29 +0000

    Clean-up: sessiond: session.cpp: fix comment style
    Signed-off-by: Jérémie Galarneau <jeremie.galarneau@efficios.com>
    Depends-on: lttng-ust: I01f7a97479d13fc7033c50a8ad0b8d296b225a6d
    Change-Id: Ice046d3529e5537efe8adf7b9191d17d4a911b3a

--- src/bin/lttng-sessiond/session.cpp
+++ src/bin/lttng-sessiond/session.cpp
@@ -392,7 +392,7 @@ static bool ltt_sessions_ht_empty()
 	LTTNG_ASSERT(ltt_sessions_ht_by_name);
 
 	if (lttng_ht_get_count(ltt_sessions_ht_by_id) != 0) {
-		/* Not empty.*/
+		/* Not empty. */
 		goto end;
 	}
 
