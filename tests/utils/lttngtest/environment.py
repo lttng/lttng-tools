b23d1b950fd59a5895b664383c84999d5e2333a1
Kienan Stewart <kstewart@efficios.com>
2025-07-14 09:45:26 -0400

    tests: Move `parallel_count_events` to to lttngtest
    Change-Id: I33a8f0e878ee09fe683ee5a307d0b97afb3909c8
    Signed-off-by: Kienan Stewart <kstewart@efficios.com>
    Signed-off-by: Jérémie Galarneau <jeremie.galarneau@efficios.com>

--- tests/utils/lttngtest/environment.py
+++ tests/utils/lttngtest/environment.py
@@ -5,6 +5,7 @@
 # SPDX-License-Identifier: GPL-2.0-only
 #
 
+import multiprocessing
 from types import FrameType
 from typing import Callable, Iterator, Optional, Tuple, List, Generator
 import sys
@@ -1472,6 +1473,66 @@ def count_events(trace_path, ignore_exceptions=False):
                 raise e
 
     return (received, discarded)
+
+
+def count_events_worker(args):
+    trace_path = args
+    received = 0
+    discarded = 0
+    for msg in bt2.TraceCollectionMessageIterator(str(trace_path)):
+        if type(msg) is bt2._EventMessageConst:
+            received += 1
+            continue
+
+        if type(msg) is bt2._DiscardedEventsMessageConst:
+            discarded += msg.count
+
+    return (received, discarded)
+
+
+def parallel_count_events(trace_path, test_env):
+    """
+    Count the number of events in a trace using parallel processing.
+    This is a workaround for the performance issues with bt2 bindings.
+    """
+    streams_path = None
+    for root, dirs, files in os.walk(str(trace_path)):
+        if "metadata" in files:
+            streams_path = pathlib.Path(root)
+            break
+
+    if streams_path is None:
+        raise RuntimeError("No metadata found in trace path: {}".format(trace_path))
+
+    metadata_path = streams_path / "metadata"
+    stream_dirs = []
+
+    for file in streams_path.iterdir():
+        if file.name == "metadata":
+            continue
+
+        temp_dir_path = test_env.create_temporary_directory()
+
+        # Symlink to the stream file
+        stream_link = temp_dir_path / file.name
+        stream_link.symlink_to(file)
+        # Symlink to the metadata file
+        metadata_link = temp_dir_path / "metadata"
+        metadata_link.symlink_to(metadata_path)
+
+        stream_dirs.append(temp_dir_path)
+
+    received = 0
+    discarded = 0
+
+    with multiprocessing.Pool() as pool:
+        results = pool.map(count_events_worker, stream_dirs)
+
+    for this_received, this_discarded in results:
+        received += this_received
+        discarded += this_discarded
+
+    return received, discarded
 
 
 @contextlib.contextmanager
