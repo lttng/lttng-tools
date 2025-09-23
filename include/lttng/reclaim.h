25214019d2b5a505fcb91b0098c7881ff2618e14
Philippe Proulx <eeppeliteloop@gmail.com>
2025-09-23 09:23:15 -0400

    liblttng-ctl API doc: document channel memory reclaim and DS info APIs
    Signed-off-by: Philippe Proulx <eeppeliteloop@gmail.com>
    Change-Id: Ifd8fdca29478292a4dd866d5b717954c17a40a9f
    Signed-off-by: Jérémie Galarneau <jeremie.galarneau@efficios.com>

--- include/lttng/reclaim.h
+++ include/lttng/reclaim.h
@@ -16,22 +16,116 @@
 extern "C" {
 #endif
 
+/*!
+@addtogroup api_channel_reclaim
+@{
+*/
+
+/*!
+@struct lttng_reclaim_handle
+
+@brief
+    \lt_obj_c_channel memory reclaim operation handle (opaque type).
+*/
 struct lttng_reclaim_handle;
 
+/*!
+@brief
+    Return type of lttng_reclaim_channel_memory().
+
+Error status enumerators have a negative value.
+*/
 enum lttng_reclaim_channel_memory_status {
+	/// Success.
 	LTTNG_RECLAIM_CHANNEL_MEMORY_STATUS_OK = 0,
+
+	/// Error.
 	LTTNG_RECLAIM_CHANNEL_MEMORY_STATUS_ERROR = -1,
+
+	/// Unsatisfied precondition.
 	LTTNG_RECLAIM_CHANNEL_MEMORY_STATUS_INVALID_PARAMETER = -2,
 };
 
+/*!
+@brief
+    Return type of \lt_obj_channel memory reclaim handle functions.
+
+Error status enumerators have a negative value.
+*/
 enum lttng_reclaim_handle_status {
+	/// Success.
 	LTTNG_RECLAIM_HANDLE_STATUS_OK = 0,
+
+	/// Reclamation operation completed.
 	LTTNG_RECLAIM_HANDLE_STATUS_COMPLETED = 1,
+
+	/// Timeout reached.
 	LTTNG_RECLAIM_HANDLE_STATUS_TIMEOUT = 2,
+
+	/// Unsatisfied precondition.
 	LTTNG_RECLAIM_HANDLE_STATUS_INVALID = -1,
+
+	/// Other error.
 	LTTNG_RECLAIM_HANDLE_STATUS_ERROR = -2,
 };
 
+/*!
+@brief
+    Initiates an immediate memory reclaim operation for
+    the \lt_obj_channel named \lt_p{channel_name} within the
+    \lt_obj_session named \lt_p{session_name} and the
+    tracing domain \lt_p{domain}.
+
+This function requests LTTng to immediately reclaim memory which the
+buffers of the targeted channel use in order to free space.
+
+This function only works with a user space channel.
+
+Use \lt_p{*handle} to wait for the completion of the memory reclaim
+operation and to fetch its result.
+
+@param[in] session_name
+    Name of the recording session which contains the targeted channel.
+@param[in] channel_name
+    Name of the targeted channel within \lt_p{session_name} from which
+    to reclaim memory.
+@param[in] domain
+    Tracing domain of the targeted channel.
+@param[in] older_than_us
+    @parblock
+    Minimum age (µs) of buffered data to consider to reclaim memory.
+
+    Set to 0 for no age constraint.
+    @endparblock
+@param[out] handle
+    @parblock
+    <strong>On success</strong>, this function sets \lt_p{*handle} to a
+    handle identifying this memory reclaim operation.
+
+    May be \c NULL.
+
+    Wait for the completion of this operation with
+    lttng_reclaim_handle_wait_for_completion().
+
+    Destroy \lt_p{*handle} with lttng_reclaim_handle_destroy().
+    @endparblock
+
+@retval #LTTNG_RECLAIM_CHANNEL_MEMORY_STATUS_OK
+    Success.
+@retval #LTTNG_RECLAIM_CHANNEL_MEMORY_STATUS_ERROR
+    Error.
+@retval #LTTNG_RECLAIM_CHANNEL_MEMORY_STATUS_INVALID_PARAMETER
+    Unsatisfied precondition.
+
+@pre
+    @lt_pre_conn
+    @lt_pre_not_null{session_name}
+    @lt_pre_sess_exists{session_name}
+    @lt_pre_not_null{channel_name}
+    - \lt_p{domain} is #LTTNG_DOMAIN_UST, #LTTNG_DOMAIN_JUL,
+      #LTTNG_DOMAIN_LOG4J, #LTTNG_DOMAIN_LOG4J2, or
+      #LTTNG_DOMAIN_PYTHON.
+*/
 LTTNG_EXPORT extern enum lttng_reclaim_channel_memory_status
 lttng_reclaim_channel_memory(const char *session_name,
 			     const char *channel_name,
@@ -39,15 +133,125 @@ lttng_reclaim_channel_memory(const char *session_name,
 			     uint64_t older_than_us,
 			     struct lttng_reclaim_handle **handle);
 
+/*!
+@brief
+    Destroys the channel memory reclaim operation handle \lt_p{handle}.
+
+@param[in] handle
+    @parblock
+    Channel memory reclaim operation handle to destroy.
+
+    May be \c NULL.
+    @endparblock
+*/
 LTTNG_EXPORT extern void lttng_reclaim_handle_destroy(struct lttng_reclaim_handle *handle);
+
+/*!
+@brief
+    Waits for the channel memory reclaim operation identified by
+    \lt_p{handle} to complete.
+
+If this function returns #LTTNG_RECLAIM_HANDLE_STATUS_COMPLETED, then
+the memory reclaim operation identified by \lt_p{handle} completed. This
+does not indicate whether the memory reclaim operation itself succeeded;
+use lttng_reclaim_handle_get_reclaimed_memory_size_bytes() to get
+the reclaimed amount.
+
+@param[in] handle
+    Channel memory reclaim operation handle of which to wait for
+    completion.
+@param[in] timeout_ms
+    Maximum time (milliseconds) to wait for the completion of the
+    memory reclaim operation identified by \lt_p{handle} before returning
+    #LTTNG_RECLAIM_HANDLE_STATUS_TIMEOUT, or <code>-1</code> to wait
+    indefinitely.
+
+@retval #LTTNG_RECLAIM_HANDLE_STATUS_COMPLETED
+    The memory reclaim operation identified by \lt_p{handle} completed
+    (with or without success).
+@retval #LTTNG_RECLAIM_HANDLE_STATUS_INVALID
+    Unsatisfied precondition.
+@retval #LTTNG_RECLAIM_HANDLE_STATUS_TIMEOUT
+    The function waited for the completion of the memory reclaim
+    operation more than \lt_p{timeout_ms}&nbsp;ms.
+@retval #LTTNG_RECLAIM_HANDLE_STATUS_ERROR
+    Other error.
+
+@pre
+    @lt_pre_not_null{handle}
+
+@sa lttng_reclaim_handle_get_reclaimed_memory_size_bytes() --
+    Returns the number of bytes reclaimed.
+*/
 LTTNG_EXPORT extern enum lttng_reclaim_handle_status
 lttng_reclaim_handle_wait_for_completion(struct lttng_reclaim_handle *handle, int timeout_ms);
+
+/*!
+@brief
+    Sets \lt_p{*memory_size_bytes} to the total number of bytes
+    successfully reclaimed by the channel memory reclaim operation
+    identified by \lt_p{handle}.
+
+The reclaimed memory size is available immediately after
+lttng_reclaim_channel_memory() returns successfully.
+
+@param[in] handle
+    Handle of the channel memory reclaim operation from which to get
+    the number of reclaimed bytes.
+@param[out] memory_size_bytes
+    <strong>On success</strong>, this function sets
+    \lt_p{*memory_size_bytes} to the number of bytes successfully
+    reclaimed.
+
+@retval #LTTNG_RECLAIM_HANDLE_STATUS_OK
+    Success.
+@retval #LTTNG_RECLAIM_HANDLE_STATUS_INVALID
+    Unsatisfied precondition.
+
+@pre
+    @lt_pre_not_null{handle}
+    @lt_pre_not_null{memory_size_bytes}
+*/
 LTTNG_EXPORT extern enum lttng_reclaim_handle_status
 lttng_reclaim_handle_get_reclaimed_memory_size_bytes(const struct lttng_reclaim_handle *handle,
 						     uint64_t *memory_size_bytes);
+
+/*!
+@brief
+    Sets \lt_p{*memory_size_bytes} to the total number of bytes
+    pending reclamation from the channel memory reclaim operation
+    identified by \lt_p{handle}.
+
+Pending bytes are sub-buffers that were eligible for reclamation but
+are still being consumed (transferred to output destination). These
+bytes will be reclaimed once consumption completes.
+
+The pending memory size is available immediately after
+lttng_reclaim_channel_memory() returns successfully.
+
+@param[in] handle
+    Handle of the channel memory reclaim operation from which to get
+    the number of pending bytes.
+@param[out] memory_size_bytes
+    <strong>On success</strong>, this function sets
+    \lt_p{*memory_size_bytes} to the number of bytes pending
+    reclamation.
+
+@retval #LTTNG_RECLAIM_HANDLE_STATUS_OK
+    Success.
+@retval #LTTNG_RECLAIM_HANDLE_STATUS_INVALID
+    Unsatisfied precondition.
+
+@pre
+    @lt_pre_not_null{handle}
+    @lt_pre_not_null{memory_size_bytes}
+*/
 LTTNG_EXPORT extern enum lttng_reclaim_handle_status
 lttng_reclaim_handle_get_pending_memory_size_bytes(const struct lttng_reclaim_handle *handle,
 						   uint64_t *memory_size_bytes);
+
+/// @}
+
 #ifdef __cplusplus
 }
 #endif
--- include/lttng/stream-info.h
+++ include/lttng/stream-info.h
@@ -16,24 +16,104 @@
 extern "C" {
 #endif
 
+/*!
+@addtogroup api_channel_ds_info
+@{
+*/
+
+/*!
+@struct lttng_data_stream_info
+
+@brief
+    Data stream info (opaque type).
+*/
 struct lttng_data_stream_info;
 
+/*!
+@brief
+    Bitness of an instrumented application.
+*/
 enum lttng_app_bitness {
+	/// 32-bit.
 	LTTNG_APP_BITNESS_32 = 0,
+
+	/// 64-bit.
 	LTTNG_APP_BITNESS_64 = 1,
 };
 
+/*!
+@brief
+    Return type for data stream info functions.
+
+Error status enumerators have a negative value.
+*/
 enum lttng_data_stream_info_status {
+	/// Success.
 	LTTNG_DATA_STREAM_INFO_STATUS_OK = 0,
+
+	/// Information not available.
 	LTTNG_DATA_STREAM_INFO_STATUS_NONE = 1,
+
+	/// Other error.
 	LTTNG_DATA_STREAM_INFO_STATUS_ERROR = -1,
+
+	/// Unsatisfied precondition.
 	LTTNG_DATA_STREAM_INFO_STATUS_INVALID_PARAMETER = -2,
 };
 
+/*!
+@brief
+    Sets \lt_p{*cpu_id} to the CPU ID of the data stream info
+    \lt_p{stream_info}.
+
+When a channel uses a
+\ref api-channel-per-cpu-buf "per-CPU" buffer ownership model,
+each data stream of the channel is bound to a specific CPU and
+therefore has a CPU ID. Otherwise, the CPU ID isn't set and this
+function returns #LTTNG_DATA_STREAM_INFO_STATUS_NONE.
+
+@param[in] stream_info
+    Data stream information from which to get the CPU ID.
+@param[out] cpu_id
+    <strong>On success</strong>, this function sets \lt_p{*cpu_id} to
+    the ID of the CPU to which the data stream of \lt_p{stream_info}
+    is bound.
+
+@retval #LTTNG_DATA_STREAM_INFO_STATUS_OK
+    Success.
+@retval #LTTNG_DATA_STREAM_INFO_STATUS_NONE
+    The data stream of \lt_p{stream_info} isn't bound to a CPU.
+@retval #LTTNG_DATA_STREAM_INFO_STATUS_INVALID_PARAMETER
+    Unsatisfied precondition.
+
+@pre
+    @lt_pre_not_null{stream_info}
+    @lt_pre_not_null{cpu_id}
+*/
 extern LTTNG_EXPORT enum lttng_data_stream_info_status
 lttng_data_stream_info_get_cpu_id(const struct lttng_data_stream_info *stream_info,
 				  unsigned int *cpu_id);
 
+/*!
+@brief
+    Sets \lt_p{*value} to the current memory usage (bytes) of
+    the data stream info \lt_p{stream_info}.
+
+@param[in] stream_info
+    Data stream information object from which to get the memory usage.
+@param[out] value
+    <strong>On success</strong>, this function sets \lt_p{*value} to the
+    current memory usage (bytes) of \lt_p{stream_info}.
+
+@retval #LTTNG_DATA_STREAM_INFO_STATUS_OK
+    Success.
+@retval #LTTNG_DATA_STREAM_INFO_STATUS_INVALID_PARAMETER
+    Unsatisfied precondition.
+
+@pre
+    @lt_pre_not_null{stream_info}
+    @lt_pre_not_null{value}
+*/
 extern LTTNG_EXPORT enum lttng_data_stream_info_status
 lttng_data_stream_info_get_memory_usage(const struct lttng_data_stream_info *stream_info,
 					uint64_t *value);
@@ -42,48 +122,312 @@ extern LTTNG_EXPORT enum lttng_data_stream_info_status
 lttng_data_stream_info_get_max_memory_usage(const struct lttng_data_stream_info *stream_info,
 					    uint64_t *value);
 
+/*!
+@struct lttng_data_stream_info_set
+
+@brief
+    Data stream info set (opaque type).
+*/
 struct lttng_data_stream_info_set;
 
+/*!
+@brief
+    Sets \lt_p{*count} to the number of data stream infos in the
+    data stream info set \lt_p{set}.
+
+@param[in] set
+    Set of data stream info of which to get the count.
+@param[out] count
+    <strong>On success</strong>, this function sets \lt_p{*count} to the
+    number of data stream infos in \lt_p{set}.
+
+@retval #LTTNG_DATA_STREAM_INFO_STATUS_OK
+    Success.
+@retval #LTTNG_DATA_STREAM_INFO_STATUS_INVALID_PARAMETER
+    Unsatisfied precondition.
+
+@pre
+    @lt_pre_not_null{set}
+    @lt_pre_not_null{count}
+
+@sa lttng_data_stream_info_set_get_at_index() --
+    Get a data stream info from a data stream info set by index.
+*/
 extern LTTNG_EXPORT enum lttng_data_stream_info_status
 lttng_data_stream_info_set_get_count(const struct lttng_data_stream_info_set *set,
 				     unsigned int *count);
 
+/*!
+@brief
+    Sets \lt_p{*stream_info} to the data stream info at the index
+    \lt_p{index} in the data stream info set \lt_p{set}.
+
+@param[in] set
+    Data stream info set from which to get the data stream info
+    at the index \lt_p{index}.
+@param[in] index
+    Index of the data stream info to get from \lt_p{set}.
+@param[out] stream_info
+    @parblock
+    <strong>On success</strong>, this function sets \lt_p{*stream_info}
+    to the data stream info at the index \lt_p{index} in \lt_p{set}.
+
+    \lt_p{*stream_info} remains valid as long as \lt_p{set} exists.
+    @endparblock
+
+@retval #LTTNG_DATA_STREAM_INFO_STATUS_OK
+    Success.
+@retval #LTTNG_DATA_STREAM_INFO_STATUS_INVALID_PARAMETER
+    Unsatisfied precondition.
+
+@pre
+    @lt_pre_not_null{set}
+    - \lt_p{index} is less than the number of data stream infos
+      in \lt_p{set} (as given by
+      lttng_data_stream_info_set_get_count()).
+    @lt_pre_not_null{stream_info}
+*/
 extern LTTNG_EXPORT enum lttng_data_stream_info_status
 lttng_data_stream_info_set_get_at_index(const struct lttng_data_stream_info_set *set,
 					unsigned int index,
 					const struct lttng_data_stream_info **stream_info);
 
+/*!
+@brief
+    Sets \lt_p{*uid} to the Unix user ID of the owner of the data
+    streams of the data stream info set \lt_p{set}.
+
+If \lt_p{set} represents
+\ref api-channel-per-proc-buf "per-process" data streams, then the
+owner is a process and this function returns
+#LTTNG_DATA_STREAM_INFO_STATUS_NONE.
+
+@param[in] set
+    Data stream info set of which to get the ID of
+    the owning Unix user.
+@param[out] uid
+    <strong>On success</strong>, this function sets \lt_p{*uid} to the
+    ID of the Unix user owning the data streams of \lt_p{set}.
+
+@retval #LTTNG_DATA_STREAM_INFO_STATUS_OK
+    Success.
+@retval #LTTNG_DATA_STREAM_INFO_STATUS_NONE
+    \lt_p{set} isn't owned by a Unix user.
+@retval #LTTNG_DATA_STREAM_INFO_STATUS_INVALID_PARAMETER
+    Unsatisfied precondition.
+
+@pre
+    @lt_pre_not_null{set}
+    @lt_pre_not_null{uid}
+*/
 extern LTTNG_EXPORT enum lttng_data_stream_info_status
 lttng_data_stream_info_set_get_uid(const struct lttng_data_stream_info_set *set, uid_t *uid);
 
+/*!
+@brief
+    Sets \lt_p{*bitness} to the application bitness of the data
+    streams of the data stream set \lt_p{set}.
+
+@param[in] set
+    Data stream set for which to get the application bitness.
+@param[out] bitness
+    <strong>On success</strong>, this function sets \lt_p{*bitness} to
+    the application bitness of the data streams of \lt_p{set}.
+
+@retval #LTTNG_DATA_STREAM_INFO_STATUS_OK
+    Success.
+@retval #LTTNG_DATA_STREAM_INFO_STATUS_INVALID_PARAMETER
+    Unsatisfied precondition.
+
+@pre
+    @lt_pre_not_null{set}
+    @lt_pre_not_null{bitness}
+*/
 extern LTTNG_EXPORT enum lttng_data_stream_info_status
 lttng_data_stream_info_set_get_app_bitness(const struct lttng_data_stream_info_set *set,
 					   enum lttng_app_bitness *bitness);
 
+/*!
+@brief
+    Sets \lt_p{*pid} to the process ID of the owner of the data
+    streams of the data stream info set \lt_p{set}.
+
+If \lt_p{set} represents
+\ref api-channel-per-user-buf "per-user" data streams, then the
+owner is a Unix user and this function returns
+#LTTNG_DATA_STREAM_INFO_STATUS_NONE.
+
+@param[in] set
+    Data stream info set of which to get the ID of the owning process.
+@param[out] pid
+    <strong>On success</strong>, this function sets \lt_p{*pid} to the
+    ID of the owning process of the data streams of \lt_p{set}.
+
+@retval #LTTNG_DATA_STREAM_INFO_STATUS_OK
+    Success.
+@retval #LTTNG_DATA_STREAM_INFO_STATUS_NONE
+    \lt_p{set} isn't owned by a process.
+@retval #LTTNG_DATA_STREAM_INFO_STATUS_INVALID_PARAMETER
+    Unsatisfied precondition.
+
+@pre
+    @lt_pre_not_null{set}
+    @lt_pre_not_null{pid}
+*/
 extern LTTNG_EXPORT enum lttng_data_stream_info_status
 lttng_data_stream_info_set_get_pid(const struct lttng_data_stream_info_set *set, pid_t *pid);
 
+/*!
+@struct lttng_data_stream_info_sets
+
+@brief
+    Set of data stream info sets (opaque type).
+*/
 struct lttng_data_stream_info_sets;
 
+/*!
+@brief
+    Sets \lt_p{*count} to the number of data stream info sets in the
+    set of data stream info sets \lt_p{sets}.
+
+@param[in] sets
+    Set of data stream info sets of which to get the count.
+@param[out] count
+    <strong>On success</strong>, this function sets \lt_p{*count} to the
+    number of data stream info sets in \lt_p{sets}.
+
+@retval #LTTNG_DATA_STREAM_INFO_STATUS_OK
+    Success.
+@retval #LTTNG_DATA_STREAM_INFO_STATUS_INVALID_PARAMETER
+    Unsatisfied precondition.
+
+@pre
+    @lt_pre_not_null{sets}
+    @lt_pre_not_null{count}
+
+@sa lttng_data_stream_info_sets_get_at_index() --
+    Get a data stream info set from a set of data stream info sets
+    by index.
+*/
 extern LTTNG_EXPORT enum lttng_data_stream_info_status
 lttng_data_stream_info_sets_get_count(const struct lttng_data_stream_info_sets *sets,
 				      unsigned int *count);
 
+/*!
+@brief
+    Sets \lt_p{*set} to the data stream info set at the
+    index \lt_p{index} in the set of data stream info sets \lt_p{sets}.
+
+@param[in] sets
+    Set of data stream info sets from which to get the data stream info
+    set at the index \lt_p{index}.
+@param[in] index
+    Index of the data stream info set to get from \lt_p{sets}.
+@param[out] set
+    @parblock
+    <strong>On success</strong>, this function sets \lt_p{*set}
+    to the data stream info set at the index \lt_p{index}
+    in \lt_p{sets}.
+
+    \lt_p{*set} remains valid as long as \lt_p{sets} exists.
+    @endparblock
+
+@retval #LTTNG_DATA_STREAM_INFO_STATUS_OK
+    Success.
+@retval #LTTNG_DATA_STREAM_INFO_STATUS_INVALID_PARAMETER
+    Unsatisfied precondition.
+
+@pre
+    @lt_pre_not_null{sets}
+    - \lt_p{index} is less than the number of data stream info sets
+      in \lt_p{sets} (as given by
+      lttng_data_stream_info_sets_get_count()).
+    @lt_pre_not_null{set}
+*/
 extern LTTNG_EXPORT enum lttng_data_stream_info_status
 lttng_data_stream_info_sets_get_at_index(const struct lttng_data_stream_info_sets *sets,
 					 unsigned int index,
 					 const struct lttng_data_stream_info_set **set);
 
+/*!
+@brief
+    Destroys the set of data stream info sets \lt_p{sets}.
+
+@param[in] sets
+    @parblock
+    Set of data stream info sets to destroy.
+
+    May be \c NULL.
+    @endparblock
+*/
 extern LTTNG_EXPORT void
 lttng_data_stream_info_sets_destroy(const struct lttng_data_stream_info_sets *sets);
 
+/// @}
+
+/*!
+@brief
+    Return type of lttng_channel_get_data_stream_info_sets().
+
+@ingroup api_channel
+
+Error status enumerators have a negative value.
+*/
 enum lttng_channel_get_data_stream_info_sets_status {
+	/// Success.
 	LTTNG_CHANNEL_GET_DATA_STREAM_INFO_SETS_STATUS_OK = 0,
+
+	/// Other error.
 	LTTNG_CHANNEL_GET_DATA_STREAM_INFO_SETS_STATUS_ERROR = -1,
+
+	/// Unsatisfied precondition.
 	LTTNG_CHANNEL_GET_DATA_STREAM_INFO_SETS_STATUS_INVALID_PARAMETER = -2,
 	LTTNG_CHANNEL_GET_DATA_STREAM_INFO_SETS_STATUS_UNSUPPORTED_DOMAIN = -3,
 };
 
+/*!
+@brief
+    Retrieves data stream info sets for the channel named
+    \lt_p{channel_name} in the recording session named
+    \lt_p{session_name} and tracing domain \lt_p{domain}.
+
+@ingroup api_channel
+
+Only user space domains are supported. The content and availability of
+memory usage information may vary depending on tracer support.
+
+@param[in] session_name
+    Name of the recording session which contains the targeted channel.
+@param[in] channel_name
+    Name of the channel for which to get data stream info sets.
+@param[in] domain
+    Tracing domain of the targeted channel for which to get data stream
+    info sets.
+@param[out] sets
+    @parblock
+    <strong>On success</strong>, this function sets \lt_p{*sets} to a
+    the data stream information sets for the targeted channel.
+
+    Destroy \lt_p{*sets} with lttng_data_stream_info_sets_destroy().
+    @endparblock
+
+@retval #LTTNG_CHANNEL_GET_DATA_STREAM_INFO_SETS_STATUS_OK
+    Success.
+@retval #LTTNG_CHANNEL_GET_DATA_STREAM_INFO_SETS_STATUS_INVALID_PARAMETER
+    Unsatisfied precondition.
+@retval #LTTNG_CHANNEL_GET_DATA_STREAM_INFO_SETS_STATUS_ERROR
+    Other error.
+
+@pre
+    @lt_pre_conn
+    @lt_pre_not_null{session_name}
+    @lt_pre_sess_exists{session_name}
+    @lt_pre_not_null{channel_name}
+    - \lt_p{domain} is one of #LTTNG_DOMAIN_UST, #LTTNG_DOMAIN_JUL,
+      #LTTNG_DOMAIN_LOG4J, #LTTNG_DOMAIN_LOG4J2,
+      or #LTTNG_DOMAIN_PYTHON.
+    @lt_pre_not_null{sets}
+*/
 extern LTTNG_EXPORT enum lttng_channel_get_data_stream_info_sets_status
 lttng_channel_get_data_stream_info_sets(const char *session_name,
 					const char *channel_name,
