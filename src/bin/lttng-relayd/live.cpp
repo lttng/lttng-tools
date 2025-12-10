lttng/lttng-tools

commit 7dfccd9432cfd777c4c2e20138764296b139c92a

Fix: relayd: honour live viewer attach "seek" type
When attaching to the relay daemon, a live viewer requests a "seek" type
(either beginning or last). In the commit that introduces the issue,
this seek type has been hardcoded to "beginning".

Use the seek type provided by the live viewer to seek streams on viewer
attach.

Fixes: 98b82dfa2b79 ("Fix: relayd: live: Catch short lived applications for attached viewers")
Signed-off-by: Mathieu Desnoyers <mathieu.desnoyers@efficios.com>
Change-Id: I6ef542f123862412008008d0d6c2a6dce9f3b446
Signed-off-by: Jérémie Galarneau <jeremie.galarneau@efficios.com>

src/bin/lttng-relayd/live.cpp
@@ -1483,15 +1483,6 @@ static int viewer_attach_session(struct relay_connection *conn)
 		goto send_reply;
 	}
 
-	send_streams = 1;
-	viewer_attach_status = viewer_session_attach(conn->viewer_session, session);
-	if (viewer_attach_status != LTTNG_VIEWER_ATTACH_OK) {
-		DBG("Error attaching to relay session %" PRIu64 ", returning status=%s",
-		    session_id,
-		    lttng_viewer_attach_return_code_str(viewer_attach_status));
-		goto send_reply;
-	}
-
 	switch (be32toh(request.seek)) {
 	case LTTNG_VIEWER_SEEK_BEGINNING:
 	case LTTNG_VIEWER_SEEK_LAST:
@@ -1503,7 +1494,15 @@ static int viewer_attach_session(struct relay_connection *conn)
 		    session_id,
 		    lttng_viewer_attach_return_code_str(viewer_attach_status));
 		viewer_attach_status = LTTNG_VIEWER_ATTACH_SEEK_ERR;
-		send_streams = 0;
+		goto send_reply;
+	}
+
+	send_streams = 1;
+	viewer_attach_status = viewer_session_attach(conn->viewer_session, session, seek_type);
+	if (viewer_attach_status != LTTNG_VIEWER_ATTACH_OK) {
+		DBG("Error attaching to relay session %" PRIu64 ", returning status=%s",
+		    session_id,
+		    lttng_viewer_attach_return_code_str(viewer_attach_status));
 		goto send_reply;
 	}
 

src/bin/lttng-relayd/viewer-session.cpp
@@ -73,7 +73,8 @@ end:
 
 /* The existence of session must be guaranteed by the caller. */
 enum lttng_viewer_attach_return_code viewer_session_attach(struct relay_viewer_session *vsession,
-							   struct relay_session *session)
+							   struct relay_session *session,
+							   enum lttng_viewer_seek seek_type)
 {
 	enum lttng_viewer_attach_return_code viewer_attach_status = LTTNG_VIEWER_ATTACH_OK;
 
@@ -121,13 +122,8 @@ enum lttng_viewer_attach_return_code viewer_session_attach(struct relay_viewer_s
 		uint32_t total = 0;
 		uint32_t unsent = 0;
 		bool closed = false;
-		const int make_viewer_streams_ret = make_viewer_streams(session,
-									vsession,
-									LTTNG_VIEWER_SEEK_BEGINNING,
-									&total,
-									&unsent,
-									&created,
-									&closed);
+		const int make_viewer_streams_ret = make_viewer_streams(
+			session, vsession, seek_type, &total, &unsent, &created, &closed);
 
 		if (make_viewer_streams_ret == 0) {
 			DBG("Created %d new viewer streams while attaching to relay session %" PRIu64,

src/bin/lttng-relayd/viewer-session.hpp
@@ -84,7 +84,8 @@ void viewer_session_destroy(struct relay_viewer_session *vsession);
 void viewer_session_close(struct relay_viewer_session *vsession);
 
 enum lttng_viewer_attach_return_code viewer_session_attach(struct relay_viewer_session *vsession,
-							   struct relay_session *session);
+							   struct relay_session *session,
+							   enum lttng_viewer_seek seek_type);
 int viewer_session_is_attached(struct relay_viewer_session *vsession,
 			       struct relay_session *session);
 void viewer_session_close_one_session(struct relay_viewer_session *vsession,

commit 6dcfd85621985b0eeef2da8e1e64fbca0c6d9d49

Fix: relayd live: uninitialized viewer_attach_status warning
Fix this compiler warning:

In function ‘const char* lttng_viewer_attach_return_code_str(lttng_viewer_attach_return_code)’,
    inlined from ‘int viewer_attach_session(relay_connection*)’ at live.cpp:1493:3:
live.cpp:147:9: warning: ‘viewer_attach_status’ may be used uninitialized [-Wmaybe-uninitialized]
  147 |         switch (code) {
      |         ^~~~~~
live.cpp: In function ‘int viewer_attach_session(relay_connection*)’:
live.cpp:1440:46: note: ‘viewer_attach_status’ was declared here
 1440 |         enum lttng_viewer_attach_return_code viewer_attach_status;
      |

Fixes: 7dfccd9432cf ("Fix: relayd: honour live viewer attach "seek" type")
Signed-off-by: Mathieu Desnoyers <mathieu.desnoyers@efficios.com>
Change-Id: Icb3500dbb79e5ae8c56209707646ffc4f9191ac0
Signed-off-by: Jérémie Galarneau <jeremie.galarneau@efficios.com>

src/bin/lttng-relayd/live.cpp
@@ -1486,14 +1486,13 @@ static int viewer_attach_session(struct relay_connection *conn)
 	switch (be32toh(request.seek)) {
 	case LTTNG_VIEWER_SEEK_BEGINNING:
 	case LTTNG_VIEWER_SEEK_LAST:
-		viewer_attach_status = LTTNG_VIEWER_ATTACH_OK;
 		seek_type = (lttng_viewer_seek) be32toh(request.seek);
 		break;
 	default:
+		viewer_attach_status = LTTNG_VIEWER_ATTACH_SEEK_ERR;
 		ERR("Wrong seek parameter for relay session %" PRIu64 ", returning status=%s",
 		    session_id,
 		    lttng_viewer_attach_return_code_str(viewer_attach_status));
-		viewer_attach_status = LTTNG_VIEWER_ATTACH_SEEK_ERR;
 		goto send_reply;
 	}
 