ba181d5d146d9811d31b66d9cca025f2ffeb5fe4
Kienan Stewart <kstewart@efficios.com>
2026-03-18 15:15:10 -0400

    sessiond: Remove `thread_manage_kernel` testpoint declarations
    The testpoints were removed in commit
    1f692f947f41a134a5a24c7e7c64f1b07c3886ae ("sessiond: replace legacy
    kernel management thread with hotplug handler")
    
    Change-Id: If5ff13f31604ccbd6d175b5252e80f5b4f0e3de1
    Signed-off-by: Kienan Stewart <kstewart@efficios.com>
    Signed-off-by: Jérémie Galarneau <jeremie.galarneau@efficios.com>

--- src/bin/lttng-sessiond/testpoint.hpp
+++ src/bin/lttng-sessiond/testpoint.hpp
@@ -16,8 +16,6 @@ TESTPOINT_DECL(sessiond_thread_manage_clients_before_loop);
 TESTPOINT_DECL(sessiond_thread_registration_apps);
 TESTPOINT_DECL(sessiond_thread_manage_apps);
 TESTPOINT_DECL(sessiond_thread_manage_apps_before_loop);
-TESTPOINT_DECL(sessiond_thread_manage_kernel);
-TESTPOINT_DECL(sessiond_thread_manage_kernel_before_loop);
 TESTPOINT_DECL(sessiond_thread_manage_consumer);
 TESTPOINT_DECL(sessiond_thread_app_manage_notify);
 TESTPOINT_DECL(sessiond_thread_app_reg_dispatch);
