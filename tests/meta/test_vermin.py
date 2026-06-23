08a40cd4507895e47911092d819663af2ef7627e
Kienan Stewart <kstewart@efficios.com>
2026-06-23 09:37:48 -0400

    tests: Add meta test for vermin conformance
    Change-Id: I4576c2c71e6f9629becf573d2dd9f32fd6d5cffa
    Signed-off-by: Kienan Stewart <kstewart@efficios.com>
    Signed-off-by: Jérémie Galarneau <jeremie.galarneau@efficios.com>

---
+++ tests/meta/test_vermin.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+#
+# SPDX-FileCopyrightText: 2026 Kienan Stewart <kstewart@efficios.com>
+# SPDX-License-Identifier: GPL-2.0-only
+#
+"""
+Execute vermin on the tests/ directory
+"""
+
+import logging
+import pathlib
+import sys
+import shutil
+import subprocess
+
+# Import in-tree test utils
+# Modify this depending on where the test file is in the tree
+test_utils_import_path = pathlib.Path(__file__).absolute().parents[1] / "utils"
+sys.path.insert(0, str(test_utils_import_path))
+
+import lttngtest
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO, format=lttngtest.utils.get_logging_format())
+    project_root = pathlib.Path(__file__).parents[2]
+    config_path = project_root / "pyproject.toml"
+    directories = [
+        'tests/'
+    ]
+    tap = lttngtest.TapGenerator(1)
+    if shutil.which('vermin') is None:
+        tap.skip("vermin not found in path")
+        sys.exit(0)
+
+    args = ['vermin', '-c', str(config_path)] + directories
+    p = subprocess.Popen(args, cwd=str(project_root))
+    p.wait()
+    tap.test(p.returncode == 0, "Vermin exited without error")
+    sys.exit(0 if tap.is_successful else 1)
